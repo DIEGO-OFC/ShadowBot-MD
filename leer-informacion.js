9a42615fcbe34a41db6e642f545f1486c740bde9
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-08-28 14:40:16 -0400

    Update and rename Información to leer-informacion.js

---
+++ leer-informacion.js
@@ -0,0 +1,17 @@
+//gracias por usar el bot, si quieres rescanear el código qr borra la carpeta DORRASESSION, DAR RESTAR O ESTART
+//número del bot oficial +573122695406
+//Si quieres cambiar los números owners ve a config.js
+//Creador: DIEGO-OFC número: +593959425714
+//Error en la consola? Repórtalo al número +593959425714
+//puedes editar el bot como tu quieras!
+//host recomendados; boxmineworld.com acidicnodes.com o termux 
+//si deseas ayuda o soporte unete al grupo oficial o escribe al numero del creador 
+//en este github siempre van a tener actualizaciones 
+//NO ME HAGO RESPONSABLE DEL MAL USO QUE PUEDA TENER EL BOT 
+//los numeros del bot oficial son: wa.me/573122695406 o wa.me/240555351060
+//bot creado para la diversión de usuarios 
+//DORR∆T-BOT-MD CREATE BY DIEGO-OFC 
+//GRUPO OFICIAL: 
+//https://chat.whatsapp.com/CrsOmirjZNYKrvnQNT98Oo 
+              //si necesitas ayuda unete al grupo oficial 
+              // Grax por leer
