90ae93d2ab1df0b0d6eea7e161a0cbe3616f5efe
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-08-29 17:45:04 -0400

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -386,51 +386,51 @@ let str = `
 *- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)* 
 _(𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)_
 
-° ඬ⃟▶️ _Quien es tu sempai botsito 7w7_
-° ඬ⃟▶️ _Te diagnostico con gay_
-° ඬ⃟▶️ _A nadie le importa_
-° ඬ⃟▶️ _Fiesta del admin_
-° ඬ⃟▶️ _Fiesta del administrador_ 
-° ඬ⃟▶️ _Vivan los novios_
-° ඬ⃟▶️ _Feliz cumpleaños_
-° ඬ⃟▶️ _Noche de paz_
-° ඬ⃟▶️ _Buenos dias_
-° ඬ⃟▶️ _Cambiate a Movistar 
-° ඬ⃟▶️ _Bienvenido_
-° ඬ⃟▶️ _Calla fan de bts_
-° ඬ⃟▶️ _Las reglas del grupo_
-° ඬ⃟▶️ _Buenos tardes_
-° ඬ⃟▶️ _Buenos noches_
-° ඬ⃟▶️ _Audio hentai_
-° ඬ⃟▶️ _Chica lgante_
-° ඬ⃟▶️ _Feliz navidad_
-° ඬ⃟▶️ _Vete a la vrg_
-° ඬ⃟▶️ _Pasa pack Bot_
-° ඬ⃟▶️ _Atencion grupo_
-° ඬ⃟▶️_Marica quien_
-° ඬ⃟▶️ _Murio el grupo_
-° ඬ⃟▶️ _Oh me vengo_
-° ඬ⃟▶️ _tio que rico_
-° ඬ⃟▶️ _Viernes_
-° ඬ⃟▶️ _Baneado_
-° ඬ⃟▶️ _Sexo_
-° ඬ⃟▶️ _Hola_
-° ඬ⃟▶️ _Un pato_
-° ඬ⃟▶️_Nyanpasu_
-° ඬ⃟▶️ _Te amo_
-° ඬ⃟▶️ _Yamete_
-° ඬ⃟▶️ _Bañate_
-° ඬ⃟▶️ _Es puto_
-° ඬ⃟▶️ _La biblia_
-° ඬ⃟▶️ _Onichan_
-° ඬ⃟▶️ _Mierda de Bot_
-° ඬ⃟▶️ _Siuuu_
-° ඬ⃟▶️ _Epico_
-° ඬ⃟▶️ _Shitpost_
-° ඬ⃟▶️ _Rawr_
-° ඬ⃟▶️ _UwU_
-° ඬ⃟▶️ _:c_
-° ඬ⃟▶️ _a_
+├➢▶️ _Quien es tu sempai botsito 7w7_
+├➢▶️ _Te diagnostico con gay_
+├➢▶️ _A nadie le importa_
+├➢▶️ _Fiesta del admin_
+├➢▶️ _Fiesta del administrador_ 
+├➢▶️ _Vivan los novios_
+├➢▶️ _Feliz cumpleaños_
+├➢▶️ _Noche de paz_
+├➢▶️ _Buenos dias_
+├➢▶️ _Cambiate a Movistar 
+├➢▶️ _Bienvenido_
+├➢▶️ _Calla fan de bts_
+├➢▶️ _Las reglas del grupo_
+├➢▶️ _Buenos tardes_
+├➢▶️ _Buenos noches_
+├➢▶️ _Audio hentai_
+├➢▶️ _Chica lgante_
+├➢▶️ _Feliz navidad_
+├➢▶️ _Vete a la vrg_
+├➢▶️ _Pasa pack Bot_
+├➢▶️ _Atencion grupo_
+├➢▶️_Marica quien_
+├➢▶️ _Murio el grupo_
+├➢▶️ _Oh me vengo_
+├➢▶️ _tio que rico_
+├➢▶️ _Viernes_
+├➢▶️ _Baneado_
+├➢▶️ _Sexo_
+├➢▶️ _Hola_
+├➢▶️ _Un pato_
+├➢▶️_Nyanpasu_
+├➢▶️ _Te amo_
+├➢▶️ _Yamete_
+├➢▶️ _Bañate_
+├➢▶️ _Es puto_
+├➢▶️ _La biblia_
+├➢▶️ _Onichan_
+├➢▶️ _Mierda de Bot_
+├➢▶️ _Siuuu_
+├➢▶️ _Epico_
+├➢▶️ _Shitpost_
+├➢▶️ _Rawr_
+├➢▶️ _UwU_
+├➢▶️ _:(
+├➢▶️ _a_
 
 *<ℍ𝔼ℝℝ𝔸𝕄𝕀𝔼ℕ𝕋𝔸𝕊/>*
 
