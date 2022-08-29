28ad9475cdcb8700a45d3ba89ba33494919e00ff
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-08-29 16:48:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -94,6 +94,7 @@ global.fg = '☆━━━━━━━ ⚠️ 𝙵𝙰𝙻𝙻𝙾 | 𝙵𝙰𝙻
 global.fallo = fg
 global.mg = '═══════ ≫ ❗ 𝙻𝙾 𝚄𝚂𝙾 𝙼𝙰𝙻 ❗ ≪ ════════'
 global.mal = mg
+//---------------
 global.imagen1 = fs.readFileSync('./Menu2.jpg')
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png')
