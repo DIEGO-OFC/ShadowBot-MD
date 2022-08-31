64ccd80a8c9dd7068f1a28ae8c9861d52c38b77b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-08-31 13:59:34 -0400

    Update leer-informacion.js

--- leer-informacion.js
+++ leer-informacion.js
@@ -15,3 +15,21 @@
 //https://chat.whatsapp.com/CrsOmirjZNYKrvnQNT98Oo 
               //si necesitas ayuda unete al grupo oficial 
               // Grax por leer
+
+//*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
+// > cd
+// > termux-setup-storage
+// > apt update 
+// > pkg upgrade 
+// > pkg install git -y
+// > pkg install nodejs -y
+// > pkg install ffmpeg -y
+// > pkg install imagemagick -y
+// > pkg install yarn
+// > git clone https://github.com/DIEGO-OFC/DORRAT-TERMUX
+// > cd DORRAT-TERMUX 
+// > npm install 
+// > yarn install 
+// > npm install
+// > npm update
+// > npm start
