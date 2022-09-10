14b569a0e31db12f96d8ae56a6aa8a0e1df3d335
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-10 19:31:39 -0400

    Update _antitrabas.js

--- plugins/_antitrabas.js
+++ plugins/_antitrabas.js
@@ -1,19 +1,23 @@
+
 import * as fs from 'fs'
 
 export async function before(m, { conn, isAdmin, isBotAdmin, usedPrefix }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    let name = await conn.getName(m.sender)
-    let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": '𝑫𝑶𝑹𝑹𝑯∆𝑻=𝑩𝑶𝑻', 'jpegThumbnail': null}}}
-    if (chat.antiTraba && m.text.length > 2000) { //Cantidad máxima de caracteres aceptados en un mensaje//
-    	if (isAdmin) return conn.sendMessage(m.chat, { text: `El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender] }, { quoted: fakemek })
+  if (m.isBaileys && m.fromMe)
+       return !0
+  if (!m.isGroup) return !1
+  let chat = global.db.data.chats[m.chat]
+  let bot = global.db.data.settings[this.user.jid] || {}
+  let delet = m.key.participant
+  let bang = m.key.id
+  let name = await conn.getName(m.sender)
+  let fakemek = {key: {participant: "0@s.whatsapp.net","remoteJid": "0@s.whatsapp.net"},"message": {"groupInviteMessage": {"groupJid": "51995386439-1616969743@g.us","inviteCode": "m","groupName": "P", "caption": '𝙳𝙾𝚁𝚁𝙰𝚃-𝙱𝙾𝚃', 'jpegThumbnail': null}}}
+   if (chat.antiTraba && m.text.length > 2000) { //Cantidad máxima de caracteres aceptados en un mensaje//
+    if (isAdmin) return conn.sendMessage(m.chat, { text: `El administrador @${m.sender.split("@")[0]} acaba de enviar un texto que contiene muchos caracteres -.-!`, mentions: [m.sender] }, { quoted: fakemek })
     await conn.sendButton(m.chat, `*[ ! ] Se detecto un mensaje que contiene muchos caracteres [ ! ]*\n`, `${isBotAdmin ? '' : 'No soy administrador, no puedo hacer nada :/'}`, author, ['[ DESACTIVAR ANTI TRABAS ]', usedPrefix+'apagar antitraba'], fakemek )
         if (isBotAdmin && bot.restrict) {
+        conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
         	setTimeout(() => { 
-        	conn.sendMessage(m.chat, { text: `Marcar el chat como leido ☑️\n${"\n".repeat(400)}\n=> 🔰 El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\n[ ❗ ] Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fakemek })
+        	conn.sendMessage(m.chat, { text: `Marcar el chat como leido ☑️\n${"\n".repeat(400)}\n=> El número : wa.me/${m.sender.split("@")[0]}\n=> Alias : ${name}\n[ ! ] Acaba de enviar un texto que contiene muchos caracteres que puede ocasionar fallos en los dispositivos`, mentions: [m.sender] }, { quoted: fakemek })
         }, 0)
         setTimeout(() => { 
         	conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
