2e2bf0fa82aba4582a37c3cdf5b94c9aa605b925
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-13 01:06:14 -0400

    Update leer-informacion.js

--- leer-informacion.js
+++ leer-informacion.js
@@ -1,38 +1,38 @@
-//gracias por usar el bot, si quieres rescanear el código qr borra la carpeta DORRATSESSION, DAR RESTAR O ESTART
-//número del bot oficial +573122695406
-//Si quieres cambiar los números owners ve a config.js
-//Creador: DIEGO-OFC número: +593959425714
-//Error en la consola? Repórtalo al número +593959425714
-//puedes editar el bot como tu quieras!
-//host recomendados; boxmineworld.com acidicnodes.com o termux 
-//si deseas ayuda o soporte unete al grupo oficial o escribe al numero del creador 
-//en este github siempre van a tener actualizaciones 
-//NO ME HAGO RESPONSABLE DEL MAL USO QUE PUEDA TENER EL BOT 
-//los numeros del bot oficial son: wa.me/573122695406 o wa.me/240555351060
-//bot creado para la diversión de usuarios 
-//DORR∆T-BOT-MD CREATE BY DIEGO-OFC 
-//GRUPO OFICIAL: 
-//https://chat.whatsapp.com/CrsOmirjZNYKrvnQNT98Oo 
-              //si necesitas ayuda unete al grupo oficial 
-              // Grax por leer
-//*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝚃𝙴𝚁𝙼𝚄𝚇*:
-// https://youtu.be/DKo7PO2ta4o
-//*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
-// > cd
-// > termux-setup-storage
-// > apt update 
-// > pkg upgrade 
-// > pkg install git -y
-// > pkg install nodejs -y
-// > pkg install ffmpeg -y
-// > pkg install imagemagick -y
-// > pkg install yarn
-// > git clone https://github.com/DIEGO-OFC/DORRAT-TERMUX
-// > cd DORRAT-TERMUX 
-// > npm install 
-// > yarn install 
-// > npm install
-// > npm update
-// > npm start
+/*gracias por usar el bot, si quieres rescanear el código qr borra la carpeta DORRATSESSION, DAR RESTAR O ESTART
+número del bot oficial +573122695406
+Si quieres cambiar los números owners ve a config.js
+Creador: DIEGO-OFC número: +593959425714
+Error en la consola? Repórtalo al número +593959425714
+puedes editar el bot como tu quieras!
+host recomendados; boxmineworld.com acidicnodes.com o termux 
+si deseas ayuda o soporte unete al grupo oficial o escribe al numero del creador 
+en este github siempre van a tener actualizaciones 
+NO ME HAGO RESPONSABLE DEL MAL USO QUE PUEDA TENER EL BOT 
+los numeros del bot oficial son: wa.me/573122695406 o wa.me/240555351060
+bot creado para la diversión de usuarios 
+DORR∆T-BOT-MD CREATE BY DIEGO-OFC 
+GRUPO OFICIAL: 
+https://chat.whatsapp.com/CrsOmirjZNYKrvnQNT98Oo 
+              si necesitas ayuda unete al grupo oficial 
+               Grax por leer
+*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝚃𝙴𝚁𝙼𝚄𝚇*:
+https://youtu.be/DKo7PO2ta4o
+*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
+ > cd
+ > termux-setup-storage
+ > apt update 
+ > pkg upgrade 
+ > pkg install git -y
+ > pkg install nodejs -y
+ > pkg install ffmpeg -y
+ > pkg install imagemagick -y
+ > pkg install yarn
+ > git clone https://github.com/DIEGO-OFC/DORRAT-TERMUX
+ > cd DORRAT-TERMUX 
+ > npm install 
+ > yarn install 
+ > npm install
+ > npm update
+ > npm start
 
-//Cómo cambió el bot sin perder los diamantes y xp? Descarga la database.json y instala el nuevo bot, antes de encerderlo pones la database
+Cómo cambió el bot sin perder los diamantes y xp? Descarga la database.json y instala el nuevo bot, antes de encerderlo pones la database*/
