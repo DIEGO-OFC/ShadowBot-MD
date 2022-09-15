79c190c70cc9e208893e9c2566cd51387f319a84
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-15 09:17:06 -0400

    Update random-cat.js

--- plugins/random-cat.js
+++ plugins/random-cat.js
@@ -4,7 +4,7 @@ try {
 let res = await fetch('https://cataas.com/cat')
 let img = await res.buffer()
 let caption = `
-_©The Mystic - Bot_
+_©The DorrAt - Bot_
 `.trim()
 conn.sendFile(m.chat, img, 'cat.jpg', caption, m)
 } catch (e) {
