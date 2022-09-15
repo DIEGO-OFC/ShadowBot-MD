bd5b3f775cf9af49529c7d0fc4f9b491d49d0d55
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-15 09:10:03 -0400

    Update menu-caja_fuerte.js

--- plugins/menu-caja_fuerte.js
+++ plugins/menu-caja_fuerte.js
@@ -50,7 +50,7 @@ let str = `
 ° ඬ⃟🗳️ _${usedPrefix}eliminarimg *<texto/comando/palabra clave>*_
 ° ඬ⃟🗳️ _${usedPrefix}eliminarsticker *<texto/comando/palabra clave>*_`.trim()
 
-conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/DIEGO-OFC/DORRAT-BOT-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
 ], m)
 }}
