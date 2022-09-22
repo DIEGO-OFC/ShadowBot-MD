42429aba388c452300237024b01749bd0c253584
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-22 16:24:46 -0400

    Update _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -9,65 +9,60 @@ if (isBotAdmin && chat.antifake) {
 		
 if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = false
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
 if (m.sender.startsWith('92' || '92')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('84' || '84')) {
    	global.db.data.users[m.sender].banned = true
-m.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+m.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('62' || '62')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('7' || '7')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('213' || '213')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('48' || '48')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('371' || '371')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
    if (m.sender.startsWith('98' || '98')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento mirey seras expulsado del grupo.🌠')
+conn.reply('Antifake  Activado lo siento mirey seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
    if (m.sender.startsWith('377' || '377')) {
    	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras expulsado del grupo.🌠')
-conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-   }
-if (m.sender.startsWith('234' || '234')) {
-   	global.db.data.users[m.sender].banned = true
-conn.reply('Antifake  Activado lo siento seras eliminado por spam de Bitcoins.🌠')
+conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
-
+    }
 
 export default handler
