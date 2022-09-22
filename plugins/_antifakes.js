9056fbb0c0f93f8d3796ed29a75f8a838cab2856
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-22 17:37:24 -0400

    Update _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -1,15 +1,8 @@
+/*let handler = m => m
 
-
-let handler = m => m
-
-handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
-	
-if (!m.isGroup) return !1
-let chat = global.db.data.chats[m.chat]
-
-if (isBotAdmin && chat.antifake) {
+handler.before = async function (m) {
    if (m.sender.startsWith('212' || '212')) {
-   	global.db.data.users[m.sender].banned = true
+   	global.db.data.users[m.sender].banned = false
 conn.reply('Antifake  Activado lo siento seras expulsado del grupo.😄')
 conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    }
@@ -66,4 +59,5 @@ conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
    } 
     }
 
-export default handler
+export default handler*/
+
