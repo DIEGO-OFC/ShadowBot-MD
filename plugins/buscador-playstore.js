daa00ba560ad44210e9d67a67c0fdffb6e611239
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-24 14:11:38 -0400

    Update buscador-playstore.js

--- plugins/buscador-playstore.js
+++ plugins/buscador-playstore.js
@@ -1,30 +1,25 @@
 import fetch from 'node-fetch'
-
 let handler = async (m, { conn, text, args }) => {
-	if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
-	let enc = encodeURIComponent(text)
+if (!args[0]) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙰𝙿𝙺 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
+let enc = encodeURIComponent(text)
 try {
-let json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=DIEGO-OFC&q=${enc}`)
+let json = await fetch(`https://latam-api.vercel.app/api/playstore?apikey=brunosobrino&q=${enc}`)
 let gPlay = await json.json()
 if (!gPlay.titulo) return m.reply(`[ ! ] Sin resultados`)
 conn.sendMessage(m.chat,{image:{url: gPlay.imagen},caption:`🔍 Resultado: ${gPlay.titulo}
-🧬 Identificador: ${gPlay.id}
-⛓️ Link: ${gPlay.link}
+📦 Identificador: ${gPlay.id}
+📎 Link: ${gPlay.link}
 🖼️ Imagen: ${gPlay.imagen}
 ✍️ Desarrollador: ${gPlay.desarrollador}
-📜 Descripcion: ${gPlay.descripcion}
+🛡️ Descripcion: ${gPlay.descripcion}
 💲 Moneda: ${gPlay.moneda}
 🎭 Gratis?: ${gPlay.gratis}
 💸 Precio: ${gPlay.precio}
 📈 Puntuacion: ${gPlay.puntuacion}`},{quoted:m})
-} catch (e) {
+} catch {
 m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')
-console.log(e)
-}
-}
-
+}}
 handler.help = ['playstore <aplicacion>']
-handler.tags = ['internet']
+handler.tags = ['buscador']
 handler.command = /^(playstore)$/i
-
 export default handler
