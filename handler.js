99bb4515ec9d97f792b1cc4790e9228e85f896bf
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-24 01:43:47 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1301,7 +1301,7 @@ export async function participantsUpdate({ id, participants, action }) {
             if (chat.welcome) {
                 let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
                 for (let user of participants) {
-                    let pp = './src/sinfoto.jpg'
+                    let pp = './galeria/sinfoto.jpg'
                     try {
                         pp = await this.profilePictureUrl(user, 'image')
                     } catch (e) {
