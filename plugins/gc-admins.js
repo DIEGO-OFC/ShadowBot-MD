5ec5c7270b9a1c619ab6acf13334a0593518e973
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-24 01:59:24 -0400

    Update gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './galeria/admins.jpg'
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
