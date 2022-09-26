fe4b5ee2a526beae6e676940aea14090114944e7
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-26 02:40:15 -0400

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -26,7 +26,6 @@ let str = `
 *║ ▢ 𝐅𝐄𝐂𝐇𝐀 » ${week}, ${date}*
 *║ ▢ 𝐓𝐈𝐄𝐌𝐏𝐎 𝐀𝐂𝐓𝐈𝐕𝐎 » ${uptime}*
 *║ ▢ 𝐑𝐀𝐌 » *⮕ RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
-*⮕ FreeRAM:* ${format(freemem())}
 *║ ▢ 𝐋𝐈𝐁𝐑𝐀𝐑𝐘 » 𝐁𝐀𝐈𝐋𝐄𝐘𝐒-𝐌𝐃
 *║ ▢ 𝐋𝐄𝐍𝐆𝐔𝐀𝐉𝐄 » 𝐄𝐒𝐏𝐀𝐍̃𝐎𝐋
 *║ ▢ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒 » ${Object.keys(global.db.data.users).length}*
