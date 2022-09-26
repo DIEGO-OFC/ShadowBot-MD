ea96e2a66d79d854bd1bdbc400cf099f2d8d886a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-26 18:35:07 -0400

    Create owner-backup.js

---
+++ plugins/owner-backup.js
@@ -0,0 +1,32 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
+	let fdoc = {
+  key : {
+  remoteJid: 'status@broadcast',
+  participant : '0@s.whatsapp.net'
+  },
+  message: {
+  documentMessage: {
+  title: '𝙳 𝙰 𝚃 𝙰 𝙱 𝙰 𝚂 𝙴', 
+  jpegThumbnail: fs.readFileSync('./Menu2.jpg'),
+                            }
+                          }
+                        }
+	let d = new Date
+            let date = d.toLocaleDateString('id', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            })
+conn.reply(m.chat, '*Succes*', m)
+conn.reply('593959425714' + '@s.whatsapp.net', `*🗓️ Database:* ${date}`, null)
+          conn.sendFile('6288215689772' + '@s.whatsapp.net', fs.readFileSync('./index.js_database.json'), 'index.js_database.json', '', 0, 0, { mimetype: 'application/json', quoted: fdoc})
+ }
+ 
+ handler.help = ['backup']
+handler.tags = ['owner']
+handler.command = /^(backup)$/i
+handler.rowner = true
+
+export default handler
