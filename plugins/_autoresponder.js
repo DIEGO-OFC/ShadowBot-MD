7c9a37219a954d19451e63c57bca887096918ab9
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-09-29 21:52:03 -0400

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -5,9 +5,9 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
     
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-let stiker = await sticker(imagen1, false, global.packname, global.author)  
+let stiker = await sticker(imagen4, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝘿𝙊𝙍𝙍𝘼𝙏-𝘽𝙊𝙏-𝙈𝘿', body: '𝑫𝑰𝑬𝑮𝑶-𝑶𝑭𝑪', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen4}}})}
+contextInfo: { externalAdReply: { title: '𝘿𝙊𝙍𝙍𝘼𝙏-𝘽𝙊𝙏-𝙈𝘿', body: '𝑫𝑰𝑬𝑮𝑶-𝑶𝑭𝑪', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen1}}})}
     
 return !0 }
 export default handler
