82fc3e27ff8e7d6d958e8447c34aeb78e7caf94a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-08 18:02:35 -0400

    Update virus-1.js

--- plugins/virus-1.js
+++ plugins/virus-1.js
@@ -2,7 +2,7 @@ import fecth from 'node-fetch'
 let handler  = async (m, { conn }) => {
 let info = await fetch('https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/master/galeria/crash/traba5.txt').then(v => v.text());
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by ĐłɆ₲Ø-Ø₣₵ 🔥', 'status@broadcast')}
-handler.command = ['virus10', 'c10', 'binario10', 'traba10', 'crash10'] 
+handler.command = ['virus1', 'c1', 'binario1', 'traba1', 'crash1'] 
 handler.rowner = true
 handler.fail = null
 export default handler
