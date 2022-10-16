fde76f2f7e8eb44529ea3421300db218308d16ae
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-16 16:52:34 -0400

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -20,3 +20,12 @@ handler.help = ['join [chat.whatsapp.com]']
 handler.tags = ['premium']
 handler.command = /^join|unete|nuevogrupo$/i
 export default handler
+
+let handler = async (m, { conn }) => {
+m.reply(global.join)
+
+}
+handler.command = /^(join|unete|nuevogrupo)$/i
+export default handler
+
+global.join =  `*LO SIENTO, EL BOT OFICIAL NO SE UNE A GRUPOS, PARA EVITAR SATURAS, PUEDES BUSCAR OTRO BOT OFICIAL, O BUSCANDO UN SUBBOT CON EL COMANDO #listjadibot o unete al grupo oficial ${md}*`
