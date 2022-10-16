15be3f9d6cf4377d16c347169f566c58910e8376
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-16 16:53:10 -0400

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -21,11 +21,11 @@ handler.tags = ['premium']
 handler.command = /^join|unete|nuevogrupo$/i
 export default handler
 
-let handler = async (m, { conn }) => {
+/*let handler = async (m, { conn }) => {
 m.reply(global.join)
 
 }
 handler.command = /^(join|unete|nuevogrupo)$/i
 export default handler
 
-global.join =  `*LO SIENTO, EL BOT OFICIAL NO SE UNE A GRUPOS, PARA EVITAR SATURAS, PUEDES BUSCAR OTRO BOT OFICIAL, O BUSCANDO UN SUBBOT CON EL COMANDO #listjadibot o unete al grupo oficial ${md}*`
+global.join =  `*LO SIENTO, EL BOT OFICIAL NO SE UNE A GRUPOS, PARA EVITAR SATURAS, PUEDES BUSCAR OTRO BOT OFICIAL, O BUSCANDO UN SUBBOT CON EL COMANDO #listjadibot o unete al grupo oficial ${md}*`*/
