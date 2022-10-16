deaf039503c5899670f9bef8d55cdf42ddde635c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-15 20:45:23 -0400

    Update rpg-verificar.js

--- plugins/rpg-verificar.js
+++ plugins/rpg-verificar.js
@@ -18,15 +18,17 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
-  m.reply(`
+  let caption = `
 ┌─「 *REGISTRADO* 」─
 ▢ *Nombre:* ${name}
 ▢ *Edad* : ${age} años
 ▢ *Numero de serie* :
 ${sn}
 └──────────────
- *${usedPrefix}help* para ver el Menu
-`.trim())
+let author = global.author
+conn.sendButton(m.chat, caption, `¡𝚃𝚄 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴 𝚃𝙴 𝚂𝙴𝚁𝚅𝙸𝚁𝙰 𝙿𝙾𝚁 𝚂𝙸 𝙳𝙴𝚂𝙴𝙰𝚂 𝙱𝙾𝚁𝚁𝙰𝚁 𝚃𝚄 𝚁𝙴𝙶𝙸𝚂𝚃𝚁𝙾 𝙴𝙽 𝙴𝙻 𝙱𝙾𝚃!\n${author}`, [['¡¡𝙰𝙷𝙾𝚁𝙰 𝚂𝙾𝚈 𝚄𝙽 𝚅𝙴𝚁𝙸𝙵𝙸𝙲𝙰𝙳𝙾/𝙰!!', '/profile']], m)
+global.db.data.users[m.sender].money += 10000
+global.db.data.users[m.sender].exp += 10000
 }
 handler.help = ['reg'].map(v => v + ' <nombre.edad>')
 handler.tags = ['rg']
