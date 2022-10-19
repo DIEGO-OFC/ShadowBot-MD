4e118aacc862588350fa3c51bd8796143ca5f662
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-19 17:39:06 -0400

    Update sticker-ttp_attp.js

--- plugins/sticker-ttp_attp.js
+++ plugins/sticker-ttp_attp.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*◉ ${usedPrefix + command} Mystic-Bot*`
+if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*◉ ${usedPrefix + command} Dorrat-Bot*`
 let teks = encodeURI(text)
 
 if (command == 'attp') {
