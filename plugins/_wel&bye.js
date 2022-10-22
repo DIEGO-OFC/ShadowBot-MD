ab7a8dbeb9443b41ee1f603b275d697d9e52c59d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-22 14:29:32 -0400

    Update _wel&bye.js

--- plugins/_wel&bye.js
+++ plugins/_wel&bye.js
@@ -1,11 +1,13 @@
 let handler = async (m, { conn, text, usedPrefix, command, customPrefix }) => {
-let stikerwelgc = "./galeria/welgc.webp"
-let stikerbyegc = "./galeria/byegc.webp"
+let stikerwelgc = "./src/welgc.webp"
+let stikerbyegc = "./src/byegc.webp"
 if (command == 'welcomegc') {
-conn.sendFile(m.chat, stikerwelgc, null, { asSticker: true })
+conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
+contextInfo: { externalAdReply: { title: 'ᴛʜᴇ 𝙳𝙾𝚁𝚁𝙰𝚃-𝙱𝙾𝚃-𝙼𝙳', body: '©𝙳𝚒𝚎𝚐𝚘𝙾𝚏𝚌', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen1}}})  
 }
 if (command == 'byegc') {
-conn.sendFile(m.chat, stikerbyegc, null, { asSticker: true })
+conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
+contextInfo: { externalAdReply: { title: 'ᴛʜᴇ 𝙳𝙾𝚁𝚁𝙰𝚃-𝙱𝙾𝚃-𝙼𝙳', body: '©𝙳𝚒𝚎𝚐𝚘𝙾𝚏𝚌', sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`, thumbnail: imagen1}}})
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
