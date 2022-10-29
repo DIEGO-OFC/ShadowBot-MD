5c70ae27231542567f4c70703dc3e537a6e3093f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-29 07:50:18 -0400

    Update menu-lista.js

--- comandos/menu-lista.js
+++ comandos/menu-lista.js
@@ -41,7 +41,7 @@ rows: [
 {title: lenguajeGB['smsLista9'](), rowId: `${usedPrefix}lb`},
 {title: lenguajeGB['smsLista10'](), rowId: ${usedPrefix}minar},
 {title: lenguajeGB['smsLista11'](), `${usedPrefix}work`},
-{title: "👜 𝐁𝐀𝐋𝐀𝐍𝐂𝐄", rowId: `${usedPrefix}bal`},
+{title: lenguajeGB['smsLista12'](),`${usedPrefix}bal`},
 {title: "💰 𝐌𝐄𝐍𝐔 𝐑𝐏𝐆", rowId: `${usedPrefix}rpgmenu`},
 {title: "💎 𝐂𝐎𝐌𝐏𝐑𝐀𝐑 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄𝐒", rowId: `${usedPrefix}buyall`}
 ]},{	
