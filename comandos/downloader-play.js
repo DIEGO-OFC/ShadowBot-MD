a46c2dfbb5afeea072c0a4ddda380f8ef6a0b922
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-10-30 13:57:16 -0400

    Update downloader-play.js

--- comandos/downloader-play.js
+++ comandos/downloader-play.js
@@ -63,7 +63,7 @@ handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^play2?$/i
 handler.exp = 20
-handler.register = true
+// handler.register = true
 handler.dorracoins = 1
 //handler.limit = 1
 export default handler
