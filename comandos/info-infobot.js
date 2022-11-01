388d975699b2af8ff3b098f895eb7e55c6933709
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-01 00:23:49 -0400

    Update info-infobot.js

--- comandos/info-infobot.js
+++ comandos/info-infobot.js
@@ -1,4 +1,3 @@
-import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 import os from 'os'
 import util from 'util'
 import sizeFormatter from 'human-readable'
@@ -13,11 +12,13 @@ const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isCha
 const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
 const groups = chats.filter(([id]) => id.endsWith('@g.us'))
 const used = process.memoryUsage()
-const { restrict, antiCall, antiprivado } = global.db.data.settings[conn.user.jid] || {}
-const { autoread, gconly, pconly, self } = global.opts || {}
+const { restrict } = global.db.data.settings[conn.user.jid] || {}
+const { autoread } = global.opts
+let pp = './Menu2.jpg'
 let old = performance.now()
 let neww = performance.now()
 let speed = neww - old
+
 let info = `
 ╠═〘 𝐈𝐍𝐅𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 〙 ═
 ╠
@@ -41,13 +42,17 @@ let info = `
 ╠
 ╠═〘 𝐃⃟𝕺𝐑⃯𝐑𝐇⃯𝚵𝐓᪣𝕭⃯𝚹⃯𝐓⃤ 〙 ═
 `.trim() 
-function _0x29f5(){const _0x566d80=['chat','10zQulaE','1128940ganLMA','relayMessage','2992230eQzNdo','161nvJXUq','25962YOlrjt','179545SeklXD','34167910xOqfEH','10IIKotd','message','878088DXpOHr','9083456bTUBuy'];_0x29f5=function(){return _0x566d80;};return _0x29f5();}const _0x4683a7=_0x27ac;function _0x27ac(_0x4fd3ff,_0x2f7c1a){const _0x29f56b=_0x29f5();return _0x27ac=function(_0x27ac09,_0x443f1e){_0x27ac09=_0x27ac09-0xda;let _0x324a1d=_0x29f56b[_0x27ac09];return _0x324a1d;},_0x27ac(_0x4fd3ff,_0x2f7c1a);}(function(_0x3b8202,_0x534d5b){const _0x37ffd2=_0x27ac,_0x30c97b=_0x3b8202();while(!![]){try{const _0x12b804=parseInt(_0x37ffd2(0xe4))/0x1*(-parseInt(_0x37ffd2(0xde))/0x2)+-parseInt(_0x37ffd2(0xdb))/0x3+parseInt(_0x37ffd2(0xdf))/0x4*(-parseInt(_0x37ffd2(0xe6))/0x5)+-parseInt(_0x37ffd2(0xe3))/0x6*(parseInt(_0x37ffd2(0xe2))/0x7)+-parseInt(_0x37ffd2(0xdc))/0x8+parseInt(_0x37ffd2(0xe1))/0x9+parseInt(_0x37ffd2(0xe5))/0xa;if(_0x12b804===_0x534d5b)break;else _0x30c97b['push'](_0x30c97b['shift']());}catch(_0x9d0633){_0x30c97b['push'](_0x30c97b['shift']());}}}(_0x29f5,0xb9679));let res=generateWAMessageFromContent(m[_0x4683a7(0xdd)],{'liveLocationMessage':{'degreesLatitude':0x0,'degreesLongitude':0x0,'caption':info,'sequenceNumber':'0','contextInfo':{'mentionedJid':conn['parseMention'](info)}}},{'quoted':m});conn[_0x4683a7(0xe0)](m[_0x4683a7(0xdd)],res[_0x4683a7(0xda)],{});
-}
-handler.help = ['infobot', 'speed']
-handler.tags = ['info', 'tools']
-handler.command = /^(ping|speed|infobot)$/i
-export default handler
+conn.sendHydrated(m.chat, info, wm, pp, 'https://github.com/DIEGO-OFC/DORRAT-BOT-MD', '𝘿𝙤𝙧𝙧𝙖𝙩𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['📦 𝙂𝙍𝙐𝙋𝙊𝙎', '#grupos'],
+['🛡️ 𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́', '.menu']
+], m,)
 
+//conn.reply(m.chat, info, m)
+}
+handler.help = ['infobot']
+handler.tags = ['info', 'tools']
+handler.command = /^(infobot|informacionbot|infozirax|informaciónzirax|informacionzirax)$/i
+export default handler
 function clockString(ms) {
 let h = Math.floor(ms / 3600000)
 let m = Math.floor(ms / 60000) % 60
