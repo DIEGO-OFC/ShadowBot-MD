f693c4d1b08b5f5465a6e0d917c23211a38e9227
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-06 19:39:05 -0400

    Update rpg-bot_temporal.js

--- comandos/rpg-bot_temporal.js
+++ comandos/rpg-bot_temporal.js
@@ -5,7 +5,6 @@ let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner, isR
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let grupos = [md]
 let gata = [imagen1, imagen4]
-let enlace = { contextInfo: { externalAdReply: {title: wm + ' 😎', body: 'support group' , sourceUrl: grupos.getRandom(), thumbnail: await(await fetch(gata.getRandom())).buffer() }}}
 let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '👑 𝗦𝘂𝗽𝗲𝗿 𝘿𝙤𝙧𝙧𝙖𝙩𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', thumbnailUrl: await(await fetch(img)).buffer(), sourceUrl: yt }}}
 let dos = [enlace, enlace2]  
 
