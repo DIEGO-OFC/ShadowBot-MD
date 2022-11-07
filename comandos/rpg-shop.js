05ade821ce0373458a905504b23de07c7ab3744b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-07 14:53:44 -0400

    Update rpg-shop.js

--- comandos/rpg-shop.js
+++ comandos/rpg-shop.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+/*import fetch from 'node-fetch'
 let handler = async (m, { command, conn, usedPrefix, args }) => {
 let user = global.db.data.users[m.sender]
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" 
@@ -290,4 +290,4 @@ function isNumber(number) {
     if (!number) return number
     number = parseInt(number)
     return typeof number == 'number' && !isNaN(number)
-}
+}*/
