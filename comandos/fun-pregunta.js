3730a824fbb7cc55d029194d7ef332d48fb37100
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-14 01:22:44 -0400

    Update fun-pregunta.js

--- comandos/fun-pregunta.js
+++ comandos/fun-pregunta.js
@@ -8,10 +8,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
   let json = await res.json()
   if (json.success) 
-m.reply(`≡ *❗ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ❗*
- 
-➢ *𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
-➢ *𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux')}`) 
+m.reply(`
+╔═══════════════════
+║≡ *❗ 𝐏𝐑𝐄𝐆𝐔𝐍𝐓𝐀𝐒 ❗*
+║-----------------------
+║➢ *𝙿𝚁𝙴𝙶𝚄𝙽𝚃𝙰:* ${text}
+║-----------------------
+║➢ *𝚁𝙴𝚂𝙿𝚄𝙴𝚂𝚃𝙰:* ${json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux')}`) 
+╚═══════════════════
   else throw json
 }
 
