3342437959409e686f41c6f3450975a68263dd7b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-14 01:47:51 -0400

    Update owner-actualizacion.js

--- comandos/owner-actualizacion.js
+++ comandos/owner-actualizacion.js
@@ -2,17 +2,26 @@ import fs from 'fs'
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 const grupo = 'https://chat.whatsapp.com/LFi7P4vAQJW0qlYERTdLGn'
 
-let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => { 
-let fkontak2 = { key: { participant: '0@s.whatsapp.net' }, message: { contactMessage: { displayName: `~ 𝐃𝐈𝐄𝐆𝐎-𝐎𝐅𝐂`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${author},;;;\nFN:${author},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, jpegThumbnail: fs.readFileSync('./galería/menudorra3.jpg'), thumbnail: fs.readFileSync('./galería/menudorra3.jpg'), sendEphemeral: true }}}
-if (!text) throw `*Falta Texto*`
+let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
+let users = m.sender.split`@`[0]
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let [_, code] = grupo.match(linkRegex) || []
 
-try{
+if ( users == 584125778026 || users == 584125778026 ) try {
+if (!text) return m.reply(`*Falta Texto*`) 
 let res = await conn.groupAcceptInvite(code)
-await conn.sendMessage(res, { text: text, mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak2 })
+await conn.sendMessage(res, { text: text + '\n\n_atte. 𝐃𝐈𝐄𝐆𝐎-𝐎𝐅𝐂_', mentions: (await conn.groupMetadata(`${res}`)).participants.map(v => v.id) }, { quoted: fkontak })
 await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO* `)
+
+} catch (e) {
+await conn.sendButton(m.chat, `\n${wm}`, lenguajeGB['smsMalError3']() + '#report ' + usedPrefix + command, null, [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB['smsMensError2']()} *${usedPrefix + command}*`]], m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+
+} else {
+await m.reply('```USTED NO TIENE AUTORIZACIÓN PARA USAR ESTE COMANDO.```')}
 }
-handler.command = ['actualizacion']
+handler.command = ['mensajeoficial']
 handler.owner = true
 
 export default handler
