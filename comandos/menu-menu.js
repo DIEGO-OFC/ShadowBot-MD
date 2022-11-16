894a78704b2083687310a7f4b85f1328aebe8480
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-16 00:58:49 -0400

    Update menu-menu.js

--- comandos/menu-menu.js
+++ comandos/menu-menu.js
@@ -10,7 +10,7 @@ import PhoneNumber from 'awesome-phonenumber'
 import { promises } from 'fs'
 import { join } from 'path'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
-try {
+//try {
 let vn = './media/menu.mp3'
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
 let { exp, limit, level, role } = global.db.data.users[m.sender]
