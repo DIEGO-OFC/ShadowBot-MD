e6004e0421db1fdc4c62c50764dafd635576a145
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-18 18:25:35 -0400

    Update gc-config_time.js

--- comandos/gc-config_time.js
+++ comandos/gc-config_time.js
@@ -1,6 +1,4 @@
 
-  /* Creditos a https://github.com/ALBERTO9883/NyanCatBot-MD */
-
 let handler = async (m, { conn, isAdmin, isOwner, args, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
