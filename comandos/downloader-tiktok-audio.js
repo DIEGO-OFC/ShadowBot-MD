9349ff0e2e573f69a10e739054d301e128dea95a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-18 18:00:41 -0400

    Create downloader-tiktok-audio.js

---
+++ comandos/downloader-tiktok-audio.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+import axios from "axios"
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+ let chat = global.db.data.chats[m.chat]
+    let url = `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`
+let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
+    await conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`, 'tiktokaudio.mp3', `
+┏┉━━━━━━━━━━━❏
+┆ *YOUTUBE MP3*
+├┈┈┈┈┈┈┈┈┈┈┈
+┆• *Judul:* 
+│• *Type:* MP3
+┆• *📥 Ukuran File:* 
+└❏
+`.trim(), m, null, {
+   document: { url: `https://api.lolhuman.xyz/api/tiktokwm?apikey=${global.lolkey}&url=${args[0]}`}, mimetype: 'audio/mpeg', fileName: 'tiktok.mp3', conntextInfo: {
+        externalAdReply: {
+            title: '▶︎ ━━━━━━━•────────────────── ', 
+            body: 'Now Playing...',
+            description: 'Now Playing...',
+            mediaType: 2,
+          thumbnail: await (await fetch('https://telegra.ph/file/9e323ad1f4b2d52579416.jpg')).buffer(),
+         mediaUrl: sig
+        }
+     }
+  })
+}
+handler.tags = ['downloader']
+handler.command = /^(tt|tiktok)(a(udio)?|mp3|sound)(dl)?(download(er)?)?$/i
+
+handler.limit = true
+
+export default handler
