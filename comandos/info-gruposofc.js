d6252cdedc8341cff4941d39d2735c656c9a269c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-20 21:21:37 -0400

    Update info-gruposofc.js

--- comandos/info-gruposofc.js
+++ comandos/info-gruposofc.js
@@ -1,4 +1,4 @@
-let media = './galería/grupos-oficiales.jpg'
+let media = './galería/menudorra3.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 †𝐃⃟𝕺𝐑⃯𝐑𝐇⃯𝚵𝐓᪣𝕭⃯𝚹⃯𝐓⃤ 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
