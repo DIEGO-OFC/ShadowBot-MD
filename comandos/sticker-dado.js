69ff6c4a637d40fe5e356dc8651185b45a57447c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-22 13:04:02 -0400

    Update sticker-dado.js

--- comandos/sticker-dado.js
+++ comandos/sticker-dado.js
@@ -1,4 +1,23 @@
-/* CREDITOS A https://github.com/FG98F */
+const dir = [
+  'https://tinyurl.com/dados01',
+  'https://tinyurl.com/dados02',
+  'https://tinyurl.com/dados03',
+  'https://tinyurl.com/dados04',
+  'https://tinyurl.com/dados05',
+  'https://tinyurl.com/dados06'
+];
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
+}
+handler.help = ['dado']
+handler.tags = ['game']
+handler.command = ['dado', 'dados'] 
+
+export default handler
+
+
+
+/* CREDITOS A https://github.com/FG98F
 const dir = [
   'https://tinyurl.com/ygms8wvy',
   'https://tinyurl.com/yhdyhnap',
@@ -13,4 +32,4 @@ conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dadu.webp', 
 handler.help = ['dado']
 handler.tags = ['game']
 handler.command = ['dado', 'dados'] 
-export default handler
+export default handler*/
