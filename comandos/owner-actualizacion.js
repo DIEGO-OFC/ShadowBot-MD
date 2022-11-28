9ffa242973d2e1b44853a5449c71deb151c05aad
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-11-28 15:08:31 -0400

    Update owner-actualizacion.js

--- comandos/owner-actualizacion.js
+++ comandos/owner-actualizacion.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
-const grupo = 'https://chat.whatsapp.com/Iue7oD6A02h39xMh4bfR1s'
+const grupo = 'https://chat.whatsapp.com/DVriK22B6hgI4EPNFqGajb'
 
 let handler = async (m, { conn, text, usedPrefix, command, participants, groupMetadata }) => {
 let users = m.sender.split`@`[0]
