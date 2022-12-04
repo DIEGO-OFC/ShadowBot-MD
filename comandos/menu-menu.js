1a8af911d54714c9753b2802cb2410a2c283e1df
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-04 00:17:12 -0400

    Update menu-menu.js

--- comandos/menu-menu.js
+++ comandos/menu-menu.js
@@ -6,7 +6,6 @@ import moment from 'moment-timezone'
 import fs, { promises } from 'fs'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
-//try {
 let vn = './media/menu.mp3'
 let d = new Date(new Date + 3600000)
 let locale = 'es'
@@ -547,10 +546,9 @@ ${wm}`,
 title: null,
 buttonText: "prueba.", 
 sections }
-conn.sendMessage(m.chat, listMessage)
 
-//catch {
-//conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
+await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})	
+
 
 handler.command = /^(menucompleto|menu completo|allmenu)$/i
 handler.exp = 50
