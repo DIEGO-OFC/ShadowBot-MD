32d8fc937412f1871f638aab6b8d0d34f76c05be
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-04 00:14:31 -0400

    Update menu-menu.js

--- comandos/menu-menu.js
+++ comandos/menu-menu.js
@@ -551,7 +551,7 @@ conn.sendMessage(m.chat, listMessage)
 
 //catch {
 //conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
-}
+
 handler.command = /^(menucompleto|menu completo|allmenu)$/i
 handler.exp = 50
 handler.fail = null
