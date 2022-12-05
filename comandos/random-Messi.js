6e2979439d7920a2af7d7f273bdad89148777133
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-05 14:41:57 -0400

    Update random-Messi.js

--- comandos/random-Messi.js
+++ comandos/random-Messi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/master/galeria/JSON/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+conn.sendButton(m.chat, "*Messi*", wm, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['messi']
 handler.tags = ['internet']
 handler.command = /^(messi)$/i
