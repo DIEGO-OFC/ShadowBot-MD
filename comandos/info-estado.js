0da51472ab128b0b004d2c6014d9c12b32ef152d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-11 23:40:42 -0400

    Update info-estado.js

--- comandos/info-estado.js
+++ comandos/info-estado.js
@@ -1,36 +1,32 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
-let picture = './Menu2.jpg'
-let name = await conn.getName(m.sender)
+let handler = async (m, { conn }) => {
+try {
+let pp = imagen4
+let img = await(await fetch('https://github.com/DIEGO-OFC.png')).buffer()
 let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
-let estado =`
-╔═[ *∆.𝑫𝑶𝑹𝑹𝑯∆𝑻-𝑩𝑶𝑻.∆* ]
-║ *➤ 𝙷𝙾𝙻𝙰 ${name}*
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `╔═[ *∆.𝑫𝑶𝑹𝑹𝑯∆𝑻-𝑩𝑶𝑻.∆* ]
+║ *➤ 𝙷𝙾𝙻𝙰 ${taguser}*
 ║
 ║ *=➢ミ🤖 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🤖彡*
 ║ *=➢ 𝙱𝙾𝚃 𝙰𝙲𝚃𝙸𝚅𝙾 ✅*
 ║ *=➢ 𝙱𝙾𝚃 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 ✅*
 ║ *=➢ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
-╚════════════════
-`.trim()
-
-conn.sendHydrated(m.chat, estado, wm, picture, 'https://github.com/DIEGO-OFC/DORRAT-BOT-MD', '📎𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
-], m)}
-
+╚════════════════`.trim()
+let buttons = [{ buttonId: '#menu', buttonText: { displayText: ' 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 ✅' }, type: 1 }]
+let buttonMessage = { image: pp, caption: str.trim(), mentions: [m.sender], footer: global.wm, buttons: buttons, headerType: 4, contextInfo: { mentionedJid: [m.sender], externalAdReply: { showAdAttribution: true, mediaType: 'VIDEO', mediaUrl: null, title: '*PÁGINA OFICIAL*', body: '*BY @DIEGO-OFC*', thumbnail: img, sourceUrl: `https://www.atom.bio/theshadowbrokers-team`}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+} catch {
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)    
+throw `*🤖 TIEMPO ACTIVO: ${uptime} ┃ 👑 BY DIEGO-OFC ┃ 🔗 PAGINA OFC: https://sites.google.com/view/dorratbotmd/inicio*`}}
 handler.help = ['estado']
-handler.tags = ['info']
-handler.command = /^(estado|status|estate|state|stado|stats)$/i
+handler.tags = ['main']
+handler.command = /^(estado|status|estate|state|stado|stats|runtime|uptime)$/i
 export default handler
-
 function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
-
-
+return [`\n║ *=➢ 💥 ` + d, ' Día(s)* ', `\n║ *=> 💫 ` + h, ' Hora(s)* ', `\n║ *=➢ 💠 ` + m, ' Minuto(s)* ', `\n║ *=➢ ♦ ` + s, ' Segundo(s)* '].map(v =➢ v.toString().padStart(2, 0)).join('')}
