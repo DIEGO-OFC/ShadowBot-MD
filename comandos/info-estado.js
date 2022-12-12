293f270e47dd735b26c30ffdd783963d82df5e38
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-12 00:02:47 -0400

    Update info-estado.js

--- comandos/info-estado.js
+++ comandos/info-estado.js
@@ -11,7 +11,7 @@ let str = `╔═[ *∆.𝑫𝑶𝑹𝑹𝑯∆𝑻-𝑩𝑶𝑻.∆* ]
 ║ *=➢ミ🤖 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🤖彡*
 ║ *=➢ 𝙱𝙾𝚃 𝙰𝙲𝚃𝙸𝚅𝙾 ✅*
 ║ *=➢ 𝙱𝙾𝚃 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 ✅*
-║ *=➢ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
+║ *=➢ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾:* ${uptime}
 ╚════════════════`.trim()
 let buttons = [{ buttonId: '#menu', buttonText: { displayText: ' 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 ✅' }, type: 1 }]
 let buttonMessage = { image: pp, caption: str.trim(), mentions: [m.sender], footer: global.wm, buttons: buttons, headerType: 4, contextInfo: { mentionedJid: [m.sender], externalAdReply: { showAdAttribution: true, mediaType: 'VIDEO', mediaUrl: null, title: 'PÁGINA OFICIAL', body: 'BY @DIEGO-OFC', thumbnail: img, sourceUrl: `https://sites.google.com/view/dorratbotmd/inicio`}}}
@@ -29,4 +29,4 @@ let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [`\n║ *=> 💥 ` + d, ' Día(s)* ', `\n║ *=> 💫 ` + h, ' Hora(s)* ', `\n║ *=> 💠 ` + m, ' Minuto(s)* ', `\n║ *=> ♦ ` + s, ' Segundo(s)* '].map(v => v.toString().padStart(2, 0)).join('')}
+return [`\n║ *=➢ 💥 ` + d, ' Día(s)* ', `\n║ *=➢ 💫 ` + h, ' Hora(s)* ', `\n║ *=➢ 💠 ` + m, ' Minuto(s)* ', `\n║ *=➢ ♦ ` + s, ' Segundo(s)* '].map(v => v.toString().padStart(2, 0)).join('')}
