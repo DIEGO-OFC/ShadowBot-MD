a1a8d7ff85ac15707b9acff4e617d0de93451e4b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-12 00:00:17 -0400

    Update info-estado.js

--- comandos/info-estado.js
+++ comandos/info-estado.js
@@ -29,4 +29,4 @@ let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [`\n│ *=> 💥 ` + d, ' Día(s)* ', `\n│ *=> 💫 ` + h, ' Hora(s)* ', `\n│ *=> 💠 ` + m, ' Minuto(s)* ', `\n│ *=> ♦ ` + s, ' Segundo(s)* '].map(v => v.toString().padStart(2, 0)).join('')}
+return [`\n║ *=> 💥 ` + d, ' Día(s)* ', `\n║ *=> 💫 ` + h, ' Hora(s)* ', `\n║ *=> 💠 ` + m, ' Minuto(s)* ', `\n║ *=> ♦ ` + s, ' Segundo(s)* '].map(v => v.toString().padStart(2, 0)).join('')}
