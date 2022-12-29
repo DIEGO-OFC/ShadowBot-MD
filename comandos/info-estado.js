8e4b0db9178c20d24e3d41c2ffe8c9e5f193cb57
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2022-12-28 23:42:59 -0400

    Update info-estado.js

--- comandos/info-estado.js
+++ comandos/info-estado.js
@@ -10,10 +10,10 @@ let str = `╔═[ *∆.𝑫𝑶𝑹𝑹𝑯∆𝑻-𝑩𝑶𝑻.∆* ]
 ║
 ║ *=➢ 🤖 TIEMPO ACTIVO:* ${uptime}
 ║ *=➢ 👑 CREADOR: DIEGO-OFC*
-║ *=➢ 🔗 PAGINA OFICIAL:* https://sites.google.com/view/dorratbotmd/inicio
+║ *=➢ 🔗 PAGINA OFICIAL:* dorrat-bot-oficial.ml
 ╚════════════════`.trim()
 let buttons = [{ buttonId: '#menu', buttonText: { displayText: ' 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 ✅' }, type: 1 }]
-let buttonMessage = { image: pp, caption: str.trim(), mentions: [m.sender], footer: global.wm, buttons: buttons, headerType: 4, contextInfo: { mentionedJid: [m.sender], externalAdReply: { showAdAttribution: true, mediaType: 'VIDEO', mediaUrl: null, title: 'PÁGINA OFICIAL', body: 'BY @DIEGO-OFC', thumbnail: img, sourceUrl: `https://sites.google.com/view/dorratbotmd/inicio`}}}
+let buttonMessage = { image: pp, caption: str.trim(), mentions: [m.sender], footer: global.wm, buttons: buttons, headerType: 4, contextInfo: { mentionedJid: [m.sender], externalAdReply: { showAdAttribution: true, mediaType: 'VIDEO', mediaUrl: null, title: 'PÁGINA OFICIAL', body: 'BY @DIEGO-OFC', thumbnail: img, sourceUrl: `https://dorrat-bot-oficial.ml/`}}}
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 } catch {
 let _uptime = process.uptime() * 1000
