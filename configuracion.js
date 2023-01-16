333c6a3e42dd9930906465aee7cea57742aa77d8
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-16 16:32:06 -0400

    Update configuracion.js

--- configuracion.js
+++ configuracion.js
@@ -140,7 +140,7 @@ global.lolkeysapi = ['85faf717d0545d14074659ad']
 //__________________
 global.lolkeys = ['2e0da1f78d1721134b21816d', '902c3bc9d8c08b0dcf8f5373', '808693688ecc695293359089', '85faf717d0545d14074659ad']
 //global.lolkeysapi = lolkeys[Math.floor(lolkeys.length * Math.random())]
-global.lolkeysapi = ['85faf717d0545d14074659ad']
+global.lolkeysapi = ['BrunoSobrino']
               
 /* ⊱__________________.⋅ RPG ⋅.________ ⊰ */
 
