10f601557b5b9611fc8b0d0a578c5c43b4cecdbd
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-17 18:20:31 -0400

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -6,10 +6,10 @@ try {
 /*m.reply(`*⏰ Cargando, espere un momento*`)*/
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await tiores.json()
-handler.dorracoins = 1
 conn.fakeReply(`${hasil.result}`,'0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast'.trim())
 } catch {
 throw `*[❕] ERROR, INTENTA DE NUEVO*`
 }}
+handler.limit = 1
 handler.command = ['openai', 'chatgpt', 'ia', 'robot']
 export default handler
