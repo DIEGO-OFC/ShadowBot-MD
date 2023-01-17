561dc9fce8a385e9724bc109e8a6282fb88bba47
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-17 18:16:35 -0400

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -1,8 +1,9 @@
-handler.dorracoins = 1
+
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*`
 try {
+m.reply(`handler.dorracoins = 1`)
 let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await tiores.json()
 conn.fakeReply(`${hasil.result}`,'0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast'.trim())
