a53379cb478690eb33f26ed9bca0683633353213
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-18 02:28:18 -0400

    Update rpg-perfil.js

--- comandos/rpg-perfil.js
+++ comandos/rpg-perfil.js
@@ -30,7 +30,7 @@ let str = `
 *PREMIUM:* ${prem ? 'Si' : 'No'}
 *NUMERO DE SERIE:* 
 ${sn}`
-con.reply(m.chat, str, author, pp, m)
+conn.reply(m.chat, str, author, pp, m)
 //conn.sendButton(m.chat, str, author, pp, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)
 }}
 handler.help = ['profile [@user]']
