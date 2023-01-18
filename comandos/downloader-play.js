125ce049b59a05603c76db0a21d1282250786c5b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-18 16:24:33 -0400

    Update downloader-play.js

--- comandos/downloader-play.js
+++ comandos/downloader-play.js
@@ -48,14 +48,14 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 ║ ${vid.url}
 ╚═══════❰ *${vs}* ❱══════⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['📽VIDEO', `${usedPrefix}getvid ${url} 360`], false, { quoted: m, 'document': { 'wa.me/111114' },
 'mimetype': global.dpdf,
-'fileName': `𝕐𝕠𝕦𝕋𝕦𝕓𝕖`,
+'fileName': `𝕐𝕠𝕦𝕋𝕦𝕓𝕖 ℙ𝕝𝕒𝕪`,
 'fileLength': 666666666666666,
 'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
 mediaType:  2,
 mediaUrl: `${url}`,
 title: `⏰ Enviando audio`,
 body: wm,
-sourceUrl: 'https://github.com/DIEGO-OFC/DORRAT-BOT-MD', thumbnail: await ( await conn.getFile(thumbnail)).data
+sourceUrl: 'http://wa.me/12522518391', thumbnail: await ( await conn.getFile(thumbnail)).data
   }
  } 
 })
@@ -84,78 +84,12 @@ thumbnail: await(await conn.getFile(thumbnail)).data
 handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
 handler.command = /^play$/i
-handler.dorracoins = 5
-handler.exp = 0
 
+handler.exp = 0
+handler.limit = true
 
 export default handler
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
-
-
-
-
-
-/*import { youtubeSearch } from '@bochilteam/scraper'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) throw `*[❕𝐈𝐍𝐅𝐎❕] NOMBRE DE LA CANCION FALTANTE, POR FAVOR INGRESE EL COMANDO MAS EL NOMBRE/TITULO DE UNA CANCIÓN*\n\n*➢ EJEMPLO:*\n*${usedPrefix + command} Phonk*`
-try {
-let vid = (await youtubeSearch(text)).video[0]
-let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-const urll = 'https://www.youtube.com/watch?v=' + videoId
-var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
-var document = doc[Math.floor(Math.random() * doc.length)]
-const buttons = [
-{ buttonId: `#ytmp3 ${urll}`, buttonText: { displayText: '🎶 𝗔𝗨𝗗𝗜𝗢 🎶' }, type: 1 },
-{ buttonId: `#ytmp4 ${urll}`, buttonText: { displayText: '🎦 𝗩𝗜𝗗𝗘𝗢 🎦' }, type: 1 },
-{ buttonId: `#playlist ${text}`, buttonText: { displayText: '🔍 𝗠𝗔𝗦 𝗥𝗘𝗦𝗨𝗟𝗧𝗔𝗗𝗢𝗦  🔍' }, type: 1 }, ]    
-let texto1 = `
-╔═══════❰  *🔰*  ❱══════⬣
-║ *TITULO | TITLE*
-║ ${title}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *DESCRIPCIÓN  | DESCRIPTION*
-║ ${description}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *PUBLICADO | PUBLISHE*
-║ ${publishedTime}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *DURACION | DURATION*
-║ ${durationH}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *VISTAS | VIEWS*
-║ ${viewH}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *URL*
-║ ${urll}
-╚═══════❰ *${vs}* ❱══════⬣`.trim()
-let buttonMessage = { "document": { url: "https://wa.me/593959425715" }, "fileName": '➢ 🗂️ ʀᴇᴘʀᴏᴅᴜᴄᴛᴏʀ ᴅᴇ ʏᴏᴜᴛᴜʙᴇ', "mimetype": 'application/vnd.ms-excel', "caption": texto1, "fileLength": '99999999999999', "mentions": [m.sender], "footer": wm, "buttons": buttons, "headerType": 4, contextInfo: { "mentionedJid": [m.sender], "externalAdReply": { "showAdAttribution": true, "title": `${title}`, "mediaType": 2, "previewType": "VIDEO", "thumbnail": await (await fetch(thumbnail)).buffer(), "mediaUrl": `${urll}`, "sourceUrl": `https://github.com/DIEGO-OFC/DORRAT-BOT-MD` }}} 
-conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-} catch {
-try {
-let vid2 = await (await fetch(`https://api.lolhuman.xyz/api/ytsearch?apikey=${lolkeysapi}&query=${text}`)).json()
-let { videoId, title, views, published, thumbnail } = await vid2.result[0]
-const url = 'https://www.youtube.com/watch?v=' + videoId
-let ytLink = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`)
-let jsonn = await ytLink.json()
-let aud = await jsonn.result.audio
-let capt = `➢ *TITULO:* ${title}\n➢ *PUBLICADO:* ${published}\n➢ *VISTAS:* ${views}`
-const buttons = [{buttonId: `#playlist ${title}`, buttonText: {displayText: '⏱️ 𝗠𝗔𝗦 𝗥𝗘𝗦𝗨𝗟𝗧𝗔𝗗𝗢𝗦'}, type: 1}]
-const buttonMessage = { image: {url: thumbnail}, caption: capt, footer: '*ᴇɴᴠɪᴀɴᴅᴏ ᴀᴜᴅɪᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*', buttons: buttons, headerType: 4 }
-let msg = await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-conn.sendMessage(m.chat, { audio: { url: aud }, mimetype: 'audio/mp4', fileName: `${title}.mp3`}, {quoted: msg})
-} catch {  
-throw '*[❕] ERROR, POR FAVOR INTÉNTELO DE NUEVO*'}}}
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = /^play2?$/i
-handler.exp = 20
-// handler.register = true
-handler.dorracoins = 5
-//handler.limit = 5
-export default handler*/
-
-
