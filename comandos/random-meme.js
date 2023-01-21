98a56ab67ca9a597a1ef1adbbcda2d4a2fc57f96
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-21 14:27:35 -0400

    Update random-meme.js

--- comandos/random-meme.js
+++ comandos/random-meme.js
@@ -1,24 +1,4 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let res = await fetch('https://api.imgflip.com/get_memes')
-if (!res.ok) throw await res.text()
-let json = await res.json()
-if (!json.url) throw 'Error!'
-conn.sendButton(m.chat, `_${command}_`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
-}
-handler.help = ['meme']
-handler.tags = ['random']
-handler.command = /^(meme|memes)$/i
-handler.limit = 3
-
-export default handler 
-
-
-
-
-
-
-/*import hispamemes from 'hispamemes'
+import hispamemes from 'hispamemes'
 let handler = async (m, {command, conn}) => {
 const url = await hispamemes.meme()  
 conn.sendButton(m.chat, `_${command}_`.trim(), wm, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
@@ -26,4 +6,4 @@ handler.help = ['meme']
 handler.tags = ['random']
 handler.command = /^(meme|memes)$/i
 handler.limit = 3
-export default handler*/
+export default handler
