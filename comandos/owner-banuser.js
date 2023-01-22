a7492e7cba4a554ef2b44a763e083449d8440bac
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-22 12:50:41 -0400

    Update owner-banuser.js

--- comandos/owner-banuser.js
+++ comandos/owner-banuser.js
@@ -1,5 +1,6 @@
 let handler = async (m, { conn, text}) => {
-if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
+let txt = text.replace('@' + who.split`@`[0], '').trim()
+if (!txt) return conn.reply(m.chat, `[❕] ESCRIBA LA RAZÓN DEL BANEO`)
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
