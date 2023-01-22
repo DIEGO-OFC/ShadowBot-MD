da05c5b2e3ddc704a4859e6e6c6c27a6feb2544b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-22 14:27:49 -0400

    Update owner-supprt.js

--- comandos/owner-supprt.js
+++ comandos/owner-supprt.js
@@ -5,7 +5,7 @@ import MessageType from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, text, usedPrefix }) => {
     let [n, mes] = text.split('|')
-            no = m.sender.split("@s.whatsapp.net")
+            m = m.sender.split("@s.whatsapp.net")
             let teks = `📩 *SOPORTE*\n\nTienes un nuevo mensaje por parte del equipo de administración del bot, por favor léelo!\n\n📝 *Mensaje:* ${mes}\n*Ticket ID:* #702\n\n_Att: DORRAT | Soporte_`
             conn.reply('56964787183@s.whatsapp.net',teks, MessageType.text,  {
            contextInfo: {
