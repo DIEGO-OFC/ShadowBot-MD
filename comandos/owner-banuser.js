dff25924bc57042bf658f299c8a1cbf7bc4315cf
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-22 13:16:31 -0400

    Update owner-banuser.js

--- comandos/owner-banuser.js
+++ comandos/owner-banuser.js
@@ -1,7 +1,6 @@
 let handler = async (m, { conn, text}) => {
 let who
 let pp = './Menu2.jpg'
-let ban = '✅ USUARIO BANEADO\nBANEADO @${who.split`@`[0]}*`} RAZON ${text}'.trim()
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 else who = m.chat
 let user = global.db.data.users[who]
@@ -10,8 +9,9 @@ let txt = text.replace('@' + who.split`@`[0], '').trim()
 if (!txt) return conn.reply(m.chat, `[❕] Ingresé la razón del baneo`, m)
 let users = global.db.data.users
 users[who].banned = true
-conn.reply(m.chat, `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙵𝚄𝙴 𝙱𝙰𝙽𝙴𝙰𝙳𝙾 𝙲𝙾𝙽 𝙴𝚇𝙸𝚃𝙾*\n*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙿𝙾𝙳𝚁𝙰 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃 𝙷𝙰𝚂𝚃𝙰 𝚀𝚄𝙴 𝚂𝙴𝙰 𝙳𝙴𝚂𝙱𝙰𝙽𝙴𝙰𝙳𝙾*`,pp, m)
-}
+conn.sendButton(m.chat `✅ USUARIO BANEADO\nBANEADO @${who.split`@`[0]}*`} RAZON ${text}`, wm,  pp,
+[
+['𝙼𝙴𝙽𝚄', `#menusimple`]], m)}}
 handler.help = ['banuser']
 handler.tags = ['owner']
 handler.command = /^banuser$/i
