a5fea4968ada439d80972fa3111d8a1ea84ec401
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-23 16:14:32 -0400

    Update owner-getfile.js

--- comandos/owner-getfile.js
+++ comandos/owner-getfile.js
@@ -1,4 +1,4 @@
-import cp from 'child_process'
+/"import cp from 'child_process'
 import { promisify } from 'util'
 let exec = promisify(cp.exec).bind(cp)
 let handler = async (m, { conn, isROwner, usedPrefix, command, text }) => {
@@ -22,4 +22,4 @@ handler.command = /^(getfile|gf)$/i
 
 handler.rowner = true
 
-export default handler
+export default handler*/
