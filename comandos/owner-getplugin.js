1b8d703fcb1a7f7138bb816f94ffef5cd9128902
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-23 16:46:43 -0400

    Update and rename PRUEBA6.js to owner-getplugin.js

--- comandos/owner-getplugin.js
+++ comandos/owner-getplugin.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 let handler = async (m, { usedPrefix, command, text }) => {
     let ar = Object.keys(comandos)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} menu`
+    if (!text) throw `*[❕] INGRESA EL TEXTO DEL PLUGIN\nejemplo:\n${usedPrefix + command} menu`
     if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
     m.reply(fs.readFileSync('./comandos/' + text + '.js', 'utf-8'))
 }
