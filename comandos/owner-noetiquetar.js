7d44ac0d575809eb823975f276912e9c4acfa481
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-23 01:59:01 -0400

    Create owner-noetiquetar.js

---
+++ comandos/owner-noetiquetar.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, text }) => {
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
+
+  conn.reply(m.chat, `
+*[ ⚠ ️] No etiquetes a mi creador, si es algo urgente contacta con el a su chat privado*
+`.trim(), m)
+    let mentionedJid = [m.sender]
+}
+handler.customPrefix = /@584125778026/i
+handler.command = new RegExp
+
+export default handler
