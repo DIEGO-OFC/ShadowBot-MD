14d5aaa8714a44933707e1cc605f81b5ee96fb73
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-24 12:33:35 -0400

    Update rpg-minar.dorracoins.js

--- comandos/rpg-minar.dorracoins.js
+++ comandos/rpg-minar.dorracoins.js
@@ -7,7 +7,7 @@ global.db.data.users[m.sender].dorracoins += d * 1
 let time = global.db.data.users[m.sender].lastdiamantes + 600000
 if (new Date - global.db.data.users[m.sender].lastdiamantes < 600000) throw `*⏱️ Vuelva en ${msToTime(time - new Date())} para continuar minando DorratCoins ⛏️*`  
 
-conn.sendButton(m.chat, `*${minar} ${d} DORRAT-COINS*`, dorrat3, pp, 
+conn.sendButton(m.chat, `*${minar} ${d} DORRAT-COINS*`, wm3, pp, 
 [
 ['𝗠𝗜𝗡𝗔𝗥 𝗘𝗫𝗣 ⚡', `.minar`],
 ['𝗚𝗥𝗨𝗣𝗢 𝗢𝗙𝗜𝗖𝗜𝗔𝗟𝗘𝗦 👑', `.grupos`],
