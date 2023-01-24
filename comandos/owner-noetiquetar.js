9fb1de78c23d4228f5f58fe607c705a4e9df8219
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-24 12:32:41 -0400

    Update owner-noetiquetar.js

--- comandos/owner-noetiquetar.js
+++ comandos/owner-noetiquetar.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
     let name = await conn.getName(m.sender)
 
-conn.sendButton(m.chat, '*[ ⚠ ️] No etiquetes a mi creador, si es algo urgente contacta con el a su chat privado*', dorrat3, [['📋 TERMINOS Y CONDICIONES', `#terminos`]], m)
+conn.sendButton(m.chat, '*[ ⚠ ️] No etiquetes a mi creador, si es algo urgente contacta con el a su chat privado*', wm3, [['📋 TERMINOS Y CONDICIONES', `#terminos`]], m)
     let mentionedJid = [m.sender]
 }
 handler.customPrefix = /@584125778026/i
