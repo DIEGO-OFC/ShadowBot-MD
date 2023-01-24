2908d03ac13c3c84ea2c32bc801476712a6ace15
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-24 13:47:26 -0400

    Update owner-balas.js

--- comandos/owner-balas.js
+++ comandos/owner-balas.js
@@ -1,16 +1,17 @@
 import MessageType from '@adiwajshing/baileys'
 
+
 let handler = async (m, { conn, args, text }) => {
     conn.req = conn.req ? conn.req : {}
     if (!args || !text) return m.reply('Silahkan Masukan Teksnya')
     let lmfao = args[0]
     let bruh = (lmfao + '@s.whatsapp.net')
     let tex = args.slice(1).join(' ')
-    let text = conn.req[bruh].text || m.quoted ? m.quoted.text ? m.quoted.text : text ? text : m.text : text ? text : m.text
+    let txt = conn.req[bruh].text || m.quoted ? m.quoted.text ? m.quoted.text : text ? text : m.text : text ? text : m.text
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
- 
+    let _text = (txt)
     conn.reply(m.chat, 'Pesan Anda sudah terkirim', m)
-    conn.sendMessage(bruh, text, MessageType.text)
+    conn.sendMessage(bruh, _text, MessageType.text)
     delete conn.req[bruh]
     
 }
