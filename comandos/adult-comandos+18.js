d3569f05ab48a4d4a460faa487241b2cbd21493f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-25 14:39:18 -0400

    Update adult-comandos+18.js

--- comandos/adult-comandos+18.js
+++ comandos/adult-comandos+18.js
@@ -42,7 +42,8 @@ conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['🔄 𝚂�
 if (command == 'nsfworgy') {
 let haha = await conn.getFile(`https://api.xteam.xyz/randomimage/orgy?APIKEY=29d4b59a4aa687ca`)
 conn.sendButton(m.chat, `_${command}_`.trim(), author, haha.data, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
-  
+ 
+if (command == 'tetas') { 
 let resError = (await axios.get(`https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/master/src/JSON/tetas.json`)).data   
 let res = await conn.getFile(`https://api-fgmods.ddns.net/api/nsfw/boobs?apikey=fg-dylux`).data
 if (res == '' || !res || res == null) res = await resError[Math.floor(resError.length * Math.random())]  
