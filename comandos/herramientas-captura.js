033e78299de98606787f2a5b471ec19526580bd7
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-25 16:41:35 -0400

    Update herramientas-captura.js

--- comandos/herramientas-captura.js
+++ comandos/herramientas-captura.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args, text }) => {
       
     if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
-    m.react(rwait) 
+     
 	let full = /f$/i.test(command)
     let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
     let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
