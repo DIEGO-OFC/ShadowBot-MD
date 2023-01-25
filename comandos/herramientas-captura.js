9a848bdba3197ea0d37148362a0c441fbf6c1285
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-25 16:15:47 -0400

    Update herramientas-captura.js

--- comandos/herramientas-captura.js
+++ comandos/herramientas-captura.js
@@ -1,4 +1,27 @@
+import https from 'https'
 import fetch from 'node-fetch'
+
+let handler = async (m, { args, usedPrefix, command }) => {
+	if (!args[0]) throw `*[❕] INGRESE EL LINK DE UNA PAGINA EJEMPLO: ${usedPrefix + command} https://github.com/DIEGO-OFC/DORRAT-BOT-MD*`
+	let res = await checkWeb(args)
+	m.reply(res.map(v => `*• Domain:* ${v.Domain}\n*• Status:* ${v.Status}`).join('\n\n'))
+}
+handler.command = /^web(check|cek)|(check|cek)web$/i
+
+export default handler
+
+async function checkWeb(url) {
+	let res = await (await fetch('https://trustpositif.kominfo.go.id/Rest_server/getrecordsname_home', {
+		agent: new https.Agent({ rejectUnauthorized: false }),
+		method: 'post',
+		body: new URLSearchParams(Object.entries({ name: url.join('%0A') }))
+	})).json()
+	return res.values
+}
+
+
+
+/*import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command, args }) => {
   let full = /f$/i.test(command)
@@ -13,4 +36,4 @@ handler.help = ['captura']
 handler.tags = ['herramientas']
 handler.command = /^(ssweb|ss|cap|screenshot|captura)$/i
 
-export default handler
+export default handler*/
