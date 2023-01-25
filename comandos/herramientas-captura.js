1717a3655f97e48651ca0761096e3e69eb5ee31e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-25 16:40:51 -0400

    Update herramientas-captura.js

--- comandos/herramientas-captura.js
+++ comandos/herramientas-captura.js
@@ -1,47 +1,17 @@
 import fetch from 'node-fetch'
-let handler = async(m, { conn, usedPrefix, text, args, command }) => {
-if (!args[0]) throw `Contoh  ${usedPrefix + command} https://s.id`
-
-	let lis = [
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=tablet&full=on',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=tablet&full=off',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=desktop&full=on',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=desktop&full=off',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=phone&full=on',
-'https://hadi-api.herokuapp.com/api/ssweb?url=' + args[0] + '&device=phone&full=off',
-'https://hadi-api.herokuapp.com/api/ssweb2?url=' + args[0],
-'https://nurutomo.herokuapp.com/api/ssweb?url=' + args[0],
-'https://shot.screenshotapi.net/screenshot?token=WCCYKR0-X5CMMV0-JB4G5Z5-P6SPC8R&url=' + args[0] + '&full_page=true&fresh=true&output=image&file_type=jpg',
-'https://api.popcat.xyz/screenshot?url=' + args[0],
-'https://api.apiflash.com/v1/urltoimage?access_key=7eea5c14db5041ecb528f68062a7ab5d&wait_until=page_loaded&url=' + args[0]
-]
-let liss = [
-'hadi-api tablet full on',
-'hadi-api tablet full off',
-'hadi-api desktop full on',
-'hadi-api desktop full off',
-'hadi-api phone full on',
-'hadi-api phone full off',
-'hadi-api original',
-'nurutomo',
-'shot.screenshotapi',
-'api.popcat',
-'api.apiflash'
-]
-let row = Object.keys(lis, liss).map((v, index) => ({
-		title: 'By ' + liss[v],
-		description: 'Bot ' + author,
-		rowId: usedPrefix + 'get ' + lis[v]
-	}))
-	let button = {
-		buttonText: `☂️ Tema Disini ☂️`,
-		description: `⚡ Silakan pilih tema di tombol di bawah...\n*Teks yang anda kirim:* ${text}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`,
-		footerText: wm
-	}
-	return conn.sendListM(m.chat, button, row, m)
+let handler = async (m, { conn, command, args, text }) => {
+      
+    if (!args[0]) return m.reply('✳️ Ingrese la Url de una página')
+    m.react(rwait) 
+	let full = /f$/i.test(command)
+    let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
+    conn.sendFile(m.chat, ss, 'ssweb.png', '✅ Captura de la Pagina', m) 
+   m.react(done) 
 }
-handler.help = ['ssweb']
-handler.tags = ['internet']
-handler.command = /^ss(web)?|scre?e?nshu?o?t|sswebdown$/i
+handler.help = ['ssweb <url>']
+handler.tags = ['tools']
+handler.command = ['ssweb', 'ss', 'captura'] 
+handler.diamond = true
 
 export default handler
