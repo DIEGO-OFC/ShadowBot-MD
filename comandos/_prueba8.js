4b6af22e0db0763032e8636d6291c6f417aa7a8b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-26 00:20:51 -0400

    Update _prueba8.js

--- comandos/_prueba8.js
+++ comandos/_prueba8.js
@@ -5,7 +5,7 @@ let handler = async (m, {text, usedPrefix, command, conn}) => {
 if (command == 'prueba8') {
 let json = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=your_api_key`)
 let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
-for (let response = JSON.parse(xhr.responseText);) {
+for (let response = JSON.parse(json.responseText);) {
 caption += `
 *TITULO: ${response.Title}*
 *AÑO: ${response.Year}*
