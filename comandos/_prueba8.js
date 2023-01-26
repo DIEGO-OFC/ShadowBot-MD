fe97a740e46fd354e4db2d559243e181c19917e4
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-26 00:49:17 -0400

    Update _prueba8.js

--- comandos/_prueba8.js
+++ comandos/_prueba8.js
@@ -1,18 +1,12 @@
 import fetch from 'node-fetch'
-import fs from 'fs'
 let handler = async (m, {text, usedPrefix, command, conn}) => {
-if (command == 'prueba8') {
-let json = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`)
-let jsons = await json.json()
-let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
-for (let x of jsons) {
-caption += `
-*TITULO: ${x.Title}*
-*AÑO: ${x.Year}*
-*Duración: ${x.Runtime}*
-`}}   
-`}}}
-
-
+let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`)
+let x = await a.json()
+let caption = `*⎔┉━「 ${command} 」━┉⎔*\n
+*TITULO: ${x.Title || ''}*
+*AÑO: ${x.Year || ''}*
+*Duración: ${x.Runtime || ''}*`
+m.reply(caption) 
+}
 handler.command = ['prueba8']
 export default handler
