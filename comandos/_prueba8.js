fd31d722e93ad34452592dd8e5f3aad4c1aef853
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-26 00:35:31 -0400

    Create _prueba8.js

---
+++ comandos/_prueba8.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+let handler = async (m, {text, usedPrefix, command, conn}) => {
+if (command == 'prueba8') {
+let json = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`)
+let jsons = await json.json()
+let caption = `*⎔┉━「 ${command} 」━┉⎔*\n`
+for (let x of jsons.result) {
+caption += `
+*TITULO: ${response.Title}*
+*AÑO: ${response.Year}*
+*Duración: ${response.Runtime}*
+`}}}
+
+
+handler.command = ['prueba8']
+export default handler
