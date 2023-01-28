f21aa11b842cee730b7403358d58ad90700b8910
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 14:32:59 -0400

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -1,14 +1,10 @@
 
-import fetch from 'node-fetch'
-let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*`
-try {
-m.reply(`*⏰ Cargando, espere un momento*`)
-let tiores = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
-let hasil = await tiores.json()
-m.reply(`${hasil.result}`.trim())
-} catch {
-throw `*[❕] ERROR, INTENTA DE NUEVO*`
-}}
-handler.command = ['openai', 'chatgpt', 'ia', 'robot']
+import { openAi } from "./lib/chatgpt.js";
+let handler = async(m, { conn, text }) => {
+if (!text) return m.reply("Ingresa un texto.")
+ const { openAi } = require("./lib/chatgpt.js");
+ let result_chatgpt = await openAi(text);
+ m.reply(result_chatgpt.dorrat)
+}
+handler.command = /^(chatgpt|iatext)$/i
 export default handler
