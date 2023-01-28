8b4691d60438f7cfbfcd040ea540c1709b17b6bf
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 10:39:00 -0400

    Update herramientas-ssweb.js

--- comandos/herramientas-ssweb.js
+++ comandos/herramientas-ssweb.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
 if (!args[0]) return conn.reply(m.chat, '*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*', m)
-let ss = await (await fetch(`https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`)).buffer()
-conn.sendFile(m.chat, ss, 'error.png', args[0], m)}
+let link = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`
+conn.sendFile(m.chat, link, 'error.png', args[0], m)}
 handler.help = ['ss', 'ssf'].map(v => v + ' <url>')
 handler.tags = ['internet']
 handler.command = /^ss(web)?f?$/i
