d42efb3146f7c18dc4d0ae699191e087a2274c0e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 19:47:19 -0400

    Update buscador-yts.js

--- comandos/buscador-yts.js
+++ comandos/buscador-yts.js
@@ -2,13 +2,14 @@ import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { text }) => {
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
 const { video, channel } = await youtubeSearch(text)
+let pp = './galeria/menudorrat3.jpg'
 let teks = [...video, ...channel].map(v => {
 switch (v.type) {
 case 'video': return `
-🔍 *${v.title}* (${v.url})
-⏱️ 𝙳𝚞𝚛𝚊𝚌𝚒𝚘𝚗: ${v.durationH}
-📗 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘 ${v.publishedTime}
-🗒️ ${v.view} 𝚟𝚒𝚜𝚝𝚊𝚜
+*🔍 ${v.title} (${v.url})*
+*⏱️ Duración: ${v.durationH}*
+*📗 Publicado ${v.publishedTime}*
+*🗒️ ${v.view} Vistas*
 `.trim()
 case 'channel': return `
 🔍 *${v.channelName}* (${v.url})
@@ -17,8 +18,10 @@ case 'channel': return `
 `.trim()
 }
 }).filter(v => v).join('\n\n• • ◕◕═════════════════◕◕ • •\n\n')
-m.reply(teks)
-}
+conn.sendButton(m.chat, teks, wm, pp,
+[
+['null', `null`]], m)
+     }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
 handler.command = /^yts(earch)?$/i
