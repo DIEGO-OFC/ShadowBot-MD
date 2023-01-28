2cb6f5bdf72d64f4442f04746b3858cda7d0b3d2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-27 23:03:01 -0400

    Update downloader-playlist.js

--- comandos/downloader-playlist.js
+++ comandos/downloader-playlist.js
@@ -1,3 +1,4 @@
+handler.dorracoins = 5
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Begin you*`
