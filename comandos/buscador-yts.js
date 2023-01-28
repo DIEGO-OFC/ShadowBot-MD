afdba5d68564c8e467e8768070e47927b0ca059e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 19:57:11 -0400

    Update buscador-yts.js

--- comandos/buscador-yts.js
+++ comandos/buscador-yts.js
@@ -3,7 +3,7 @@ let handler = async (m, { text }) => {
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙾 𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*'
 const { video, channel } = await youtubeSearch(text)
 let pp = './galeria/menudorrat3.jpg'
-let db2 = `*${htki} YOUTUBE SEARCH* ${htka}`
+global.xd = '*${htki} YOUTUBE SEARCH* ${htka}'
 let teks = [...video, ...channel].map(v => {
 switch (v.type) {
 case 'video': return `
@@ -19,7 +19,7 @@ case 'channel': return `
 `.trim()
 }
 }).filter(v => v).join('\n\n• • ◕◕══════════════◕◕ • •\n\n')
-conn.sendButton(m.chat, db2, teks, wm, pp,
+conn.sendButton(m.chat, xd, teks, wm, pp,
 [
 ['gracias 🌹', `.sc`]], m)
      }
