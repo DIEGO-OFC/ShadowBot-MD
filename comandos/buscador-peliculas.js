0e5c25163dbb6bdd3db7e09c0888ebe1400e60dc
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 16:31:11 -0400

    Update buscador-peliculas.js

--- comandos/buscador-peliculas.js
+++ comandos/buscador-peliculas.js
@@ -2,6 +2,7 @@
 
 import fetch from 'node-fetch'
 let handler = async (m, {text, usedPrefix, command, conn}) => {
+try {
 if (!text) throw `*[❕] Ingrese el nombre de una película*\n\n*❍ EJEMPLO: ${usedPrefix + command} Batman*`
 let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`)
 let x = await a.json()
@@ -12,7 +13,9 @@ let caption = `*⎔┉━「 ${command} 」━┉⎔*\n
 *Género: ${x.Genre || ''}*
 *Actores: ${x.Actors || ''}*`
 m.reply(caption) 
-}
+} catch {
+await conn.reply(m.chat, '*⚠️ Ocurrio un error, inténtelo de nuevo*', m)}
+
 handler.command = /^(película|pelicula|peli)$/i
 
 export default handler
