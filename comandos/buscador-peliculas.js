dc8c9515628394639db2a0bae51831c32c4e8032
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 16:38:53 -0400

    Update buscador-peliculas.js

--- comandos/buscador-peliculas.js
+++ comandos/buscador-peliculas.js
@@ -2,16 +2,18 @@
 
 import fetch from 'node-fetch'
 let handler = async (m, {text, usedPrefix, command, conn}) => {
-
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 if (!text) throw `*[❕] Ingrese el nombre de una película*\n\n*❍ EJEMPLO: ${usedPrefix + command} Batman*`
 let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`)
 let x = await a.json()
 let caption = `*⎔┉━「 ${command} 」━┉⎔*\n
-*TITULO: ${x.Title || ''}*
-*AÑO: ${x.Year || ''}*
-*Duración: ${x.Runtime || ''}*
-*Género: ${x.Genre || ''}*
-*Actores: ${x.Actors || ''}*`
+🔍 *TITULO: ${x.Title || ''}*
+🗒️ *AÑO: ${x.Year || ''}*
+⏱️ *Duración: ${x.Runtime || ''}*
+📗 *Género: ${x.Genre || ''}*
+👥 *Actores: ${x.Actors || ''}*
+
+❕ Pedido por ${taguser}`
 conn.fakeReply(m.chat, caption, '0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast')
 
 }
