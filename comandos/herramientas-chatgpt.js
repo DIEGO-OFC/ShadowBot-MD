ac86c38c793a61c50af1704b62f45bd209d18e70
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-28 15:45:33 -0400

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -1,9 +1,8 @@
 import { openAi } from '../lib/chatgpt.js'
-
 let handler = async(m, { conn, text }) => {
 if (!text) return m.reply("Ingresa un texto.")
  let result_chatgpt = await openAi(text);
- conn.sendMessage(m.chat, {text: result_chatgpt.dorrat}, {quoted: m})
+ m.reply(result_chatgpt.dorrat)
 }
 handler.command = /^(chatgpt|iatext)$/i
 export default handler
