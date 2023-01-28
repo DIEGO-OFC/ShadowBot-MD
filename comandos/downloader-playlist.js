5a7c7e03ffb572b198b87ddbdc4e150e123f4a96
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-27 23:18:17 -0400

    Update downloader-playlist.js

--- comandos/downloader-playlist.js
+++ comandos/downloader-playlist.js
@@ -1,7 +1,7 @@
 
 import { youtubeSearch } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Begin you*`
+if (!text) throw `*[❕] INGRESA UNA CANCION PARA ENVIAR LA LISTA, EJEMPLO:\n■ ${usedPrefix + command} phonk`
 try {
 const { video } = await youtubeSearch(text)
 const listSections = []
