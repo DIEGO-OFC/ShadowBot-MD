916f4023e7c884fad5f82066053a0c917bf29381
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-29 15:32:40 -0400

    Update owner-reporte.js

--- comandos/owner-reporte.js
+++ comandos/owner-reporte.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚁𝙴𝙿𝙾𝚁𝚃𝙴*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} el comando ${usedPrefix}play no manda nada*`
 if (text.length < 10) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝙴 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 𝙳𝙴 𝙼𝙸𝙽𝙸𝙼𝙾 𝟷0 𝙲𝙰𝚁𝙰𝙲𝚃𝙴𝚁𝙴𝚂!*`
 if (text.length > 1000) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚁𝙴𝙿𝙾𝚁𝚃𝙴 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 𝙳𝙴 𝙼𝙰𝚇𝙸𝙼𝙾 𝟷000 𝙲𝙰𝚁𝙰𝙲𝚃𝙴𝚁𝙴𝚂!*`
-let teks = `*┣━━━❰･ REPORTE ･❱━━━┫*\n┣\n*❀••►☪︎ NUMERO:* wa.me/${m.sender.split`@`[0]}\n❀••►☪︎ *MENSAJE:* ${text}\n*`
+let teks = `*┣━━━❰･ REPORTE ･❱━━━┫*\n┣\n*❀••►☪︎ NUMERO:* wa.me/${m.sender.split`@`[0]}\n❀••►☪︎ *MENSAJE: ${text}\n*`
 conn.reply('584125778026@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {
 contextInfo: {
 mentionedJid: [m.sender]
