83036a9d6991532dfde1e34abf021e44bfc640dc
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-29 14:57:42 -0400

    Create owner-npm.js

---
+++ comandos/owner-npm.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, args, command, usedPrefix }) => {
+if (!text) throw `_Ingresa el nombre del paquete npm_\n_Ejemplo_ : ${prefix}npmsearch whatsapp-web.js`
+let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
+let { objects } = await res.json()
+if (!objects.length) return m.reply(`Paquete "${text}" no encontrado`)
+let txt2 = objects.map(({ package: pkg }) => {
+return `*◦ Paquete:* ${pkg.name || '-'}
+*🔍 Version:* ${pkg.version || '-'}
+*🔗 Enlace:* ${pkg.links.npm || '-'}
+*📗 Descripcion:* ${pkg.description || '-'}
+*📌 KeyWords:* ${pkg.keywords || '-'}
+*📦 Publicado:* ${pkg.date}
+*👥 Username:* ${pkg.publisher.username || '-'}
+*💳 Gmail:* ${pkg.publisher.email || '-'}
+*🖊️ HomePage:* ${pkg.links.homepage || '-'} `
+	}).join`\n\n`
+	m.reply(txt2)
+}
+handler.command = /(npmsh)/i 
+handler.rowner = true 
+export default handler
