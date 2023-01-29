228325a296571b466769c37190a437f38fefd70c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-01-29 15:03:02 -0400

    Update owner-npm.js

--- comandos/owner-npm.js
+++ comandos/owner-npm.js
@@ -4,6 +4,7 @@ if (!text) throw `_Ingresa el nombre del paquete npm_\n_Ejemplo_ : ${prefix}npms
 let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
 let { objects } = await res.json()
 if (!objects.length) return m.reply(`Paquete "${text}" no encontrado`)
+let bg = 'https://telegra.ph/file/42ca1bf46a366d929e7ce.jpg'
 let txt2 = objects.map(({ package: pkg }) => {
 return `*◦ Paquete:* ${pkg.name || '-'}
 *🔍 Version:* ${pkg.version || '-'}
@@ -15,7 +16,9 @@ return `*◦ Paquete:* ${pkg.name || '-'}
 *💳 Gmail:* ${pkg.publisher.email || '-'}
 *🖊️ HomePage:* ${pkg.links.homepage || '-'} `
 	}).join`\n\n`
-	m.reply(txt2)
+conn.sendButton(m.chat, txt2, wm, bg,
+[
+['ok', `#ok`]], m)
 }
 handler.command = /(npmsh)/i 
 handler.rowner = true 
