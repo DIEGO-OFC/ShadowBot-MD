247823962a9110295151599602c9be0e0f4cca88
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-01-30 08:57:56 -0500

    Add files via upload

---
+++ comandos/anime-foxgirl.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+let handler = async(m, { usedPrefix, conn }) => {
+  let foxgirl_irius = await axios.get(`https://nekos.life/api/v2/img/fox_girl`);
+  let buttons = [
+    {
+      buttonId: `${usedPrefix}foxgirl`,
+      buttonText: { displayText: "Siguiente 🦊" },
+      type: 1,
+    },
+  ];
+  let buttonMessage = {
+    image: { url: foxgirl_irius.data.url },
+    caption: `*乂 ⺀ ANIME - RANDOM ⺀ 乂*`,
+    footer: `*🔥 THE DORRAT - BOT 🔥*`,
+    buttons: buttons,
+    headerType: 4,
+    };
+    conn.sendMessage(m.chat, buttonMessage, { quoted: m });
+}
+
+handler.command = /^(foxgirl|girlfox)$/i
+export default handler
