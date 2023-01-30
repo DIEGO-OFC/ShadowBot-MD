381ea195652d43808c78d34eca60df67a905f3fa
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-01-29 22:08:00 -0500

    Update buscador-peliculas.js

--- comandos/buscador-peliculas.js
+++ comandos/buscador-peliculas.js
@@ -6,7 +6,27 @@ let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 if (!text) throw `*[❕] Ingrese el nombre de una película*\n\n*❍ EJEMPLO: ${usedPrefix + command} Batman*`
 let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`)
 let x = await a.json()
-let caption = `*${comienzo}「 PELÍCULAS 」${fin}*\n
+let mov_txt =  `*${comienzo}「 PELÍCULAS 」${fin}*\n
+*Titulo ∙* ${x.title || '-'}
+*Publicado ∙* ${x.Year || '-'}
+*Duracion ∙* ${x.Runtime || '-'}
+*Genero ∙* ${x.Genre || '-'}
+*Director ∙* ${x.Director || '-'}
+*Actores ∙* ${x.Actors || '-'}
+*Lenguajes ∙* ${x.Language || '-'}
+*Awards ∙* ${x.Awards || '-'}
+*Votos ∙* ${x.imdbVotes || '-'}
+*Score ∙* ${x.Metascore || '-'}
+*Tipo ∙* ${x.Type || '-'}
+*Recaudado ∙* ${x.BoxOffice || '-'}
+*Grabacion ∙* ${x.Country || '-'}
+
+   ❕Pedido por @${m.sender.split("@")[0]}
+*🔥 THE DORRAT - BOT 🔥*`
+
+conn.sendMessage(m.chat, {image: {url: x.Poster}, caption: mov_txt, mentions: [m.sender]}, {quoted: m})
+  
+/*let caption = `*${comienzo}「 PELÍCULAS 」${fin}*\n
 🔍 *Titulo: ${x.Title || ''}*
 🗒️ *Año: ${x.Year || ''}*
 ⏱️ *Duración: ${x.Runtime || ''}*
@@ -14,7 +34,7 @@ let caption = `*${comienzo}「 PELÍCULAS 」${fin}*\n
 👥 *Actores: ${x.Actors || ''}*
 
 ❕ Pedido por ${taguser}`
-conn.fakeReply(m.chat, caption, '0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast')
+conn.fakeReply(m.chat, caption, '0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast')*/
 
 }
 handler.command = /^(película|pelicula|peli)$/i
