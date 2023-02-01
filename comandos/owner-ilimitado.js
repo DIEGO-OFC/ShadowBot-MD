695b608b377d57ffd667042d796b15f6875c3ae8
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-01 16:49:04 -0400

    Create owner-ilimitado.js

---
+++ comandos/owner-ilimitado.js
@@ -0,0 +1,26 @@
+
+import MessageType from '@adiwajshing/baileys'
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+        conn.reply(m.chat, `*éxito, ahora tiene todo ilimitado ✅*`, m)
+        global.db.data.users[m.sender].dorracoins = Infinity
+        global.db.data.users[m.sender].limit = Infinity
+        global.db.data.users[m.sender].level = Infinity
+        
+}
+handler.help = ['cheat']
+handler.tags = ['owner']
+handler.command = /^(ilimitado|infiniy)$/i
+handler.rowner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+//handler.money = 0
+
+export default handler
