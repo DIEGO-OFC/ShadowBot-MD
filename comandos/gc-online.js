45116cad4297d385303cf9d3b09249df154df7f2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-02 19:57:56 -0400

    Update gc-online.js

--- comandos/gc-online.js
+++ comandos/gc-online.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args, isAdmin, participants, isOwner }) => {
      } 
    } 
    let id = args && /.*@g.us/.test(args[0]) ? args[0] : m.chat 
-   let online = [...Object.keys(conn.chats(participants).presences), conn.user.jid] 
+   let online = [...Object.keys(conn.reply(participants).presences), conn.user.jid] 
    conn.reply(m.chat, '┌「 *Daftar Wibu* 」\n' + online.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', m, { 
      contextInfo: { mentionedJid: online } 
    }) 
