08136e470c6d673949043d84f82d8fc647e31b20
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-02 19:56:19 -0400

    Update gc-online.js

--- comandos/gc-online.js
+++ comandos/gc-online.js
@@ -1,20 +1,20 @@
 //By https://github.com/DIEGO-OFC
 
-let handler = async (m, { conn, args, isAdmin, participants, isOwner }) => { 
-   if (m.isGroup) { 
-     if (!(isAdmin || isOwner)) { 
-       dfail('admin', m, conn) 
-       throw false 
-     } 
-   } 
-    let id = args && /.*@g.us/.test(args[0]) ? args[0] : m.chat 
-   let online = Object.keys(conn.chats(participants).presences), conn.user.jid] 
-   conn.reply(m.chat, '┌「 *Daftar Wibu* 」\n' + online.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', m, { 
-     contextInfo: { mentionedJid: online } 
-   }) 
+let handler = async (m, { conn, args, isAdmin, participants, isOwner }) => { 
+   if (m.isGroup) { 
+     if (!(isAdmin || isOwner)) { 
+       dfail('admin', m, conn) 
+       throw false 
+     } 
+   } 
+   let id = args && /.*@g.us/.test(args[0]) ? args[0] : m.chat 
+   let online = [...Object.keys(conn.chats(participants).presences), conn.user.jid] 
+   conn.reply(m.chat, '┌「 *Daftar Wibu* 」\n' + online.map(v => '├ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', m, { 
+     contextInfo: { mentionedJid: online } 
+   }) 
  } 
- handler.help = ['online'] 
- handler.tags = ['group'] 
- handler.command = /^(here|(list)?online)$/i 
-  
- export default handler
+ handler.help = ['online'] 
+ handler.tags = ['group'] 
+ handler.command = /^(here|(list)?online)$/i 
+
+ export default handler
