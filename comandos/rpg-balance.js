c1175745d6ad2364e2afcdad3024c04c54cbf90c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-03 15:46:29 -0400

    Update rpg-balance.js

--- comandos/rpg-balance.js
+++ comandos/rpg-balance.js
@@ -3,7 +3,7 @@ let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
-Let bank = `
+let bank = `
 ╭━━━━━━ 𝐁𝐀𝐍𝐂𝐎 🏦 ━━━╾•
 ❍ *Nombre:* ${name}
 ❍ *Diamantes:* ${global.db.data.users[who].limit}💎
