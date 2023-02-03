ab9fd773e795f4f2533981646b97337347a41192
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-03 15:29:47 -0400

    Update rpg-balance.js

--- comandos/rpg-balance.js
+++ comandos/rpg-balance.js
@@ -4,16 +4,18 @@ if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
 m.reply(`
-┌───⊷ 𝐁𝐀𝐍𝐂𝐎 🏦 ⊶
-▢ *Nombre:* ${name}
-▢ *Diamantes:* ${global.db.data.users[who].limit}💎
-▢ *Tokens:* ${global.db.data.users[who].joincount}☯️
-▢ *Dólares:* $${global.db.data.users[who].dolares} 💸
-└──────────────
+╭━━━━━━ 𝐁𝐀𝐍𝐂𝐎 🏦 ━━━╾•
+❍ *Nombre:* ${name}
+❍ *Diamantes:* ${global.db.data.users[who].limit}💎
+❍ *Tokens:* ${global.db.data.users[who].joincount}☯️
+❍ *Dólares:* $${global.db.data.users[who].dolares} 💸
+╰━━━━━━━━━━━━━━━━╾•
 *𝙽𝙾𝚃𝙰:* 
 *PUEDES COMPRAR USANDO LOS COMANDOS:*
 ❏ *${usedPrefix}buy <cantidad>*
-❏ *${usedPrefix}buyall*`)
+❏ *${usedPrefix}buyall*
+
+${wm3}`)
 }
 handler.help = ['bal']
 handler.tags = ['xp']
