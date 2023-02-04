781b3697813ebdc2e92a8fea00a8619071cb8aeb
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-04 17:04:08 -0400

    Update downloader-spotify.js

--- comandos/downloader-spotify.js
+++ comandos/downloader-spotify.js
@@ -1,37 +1,4 @@
 import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-	if (!text) throw `*Usage : ${usedPrefix + command} url*\n\nExample: ${usedPrefix + command} https://open.spotify.com/track/0ZEYRVISCaqz5yamWZWzaA\n\n*Tips :* Untuk mencari link spotify, bisa juga dengan command *${usedPrefix}spotsearch*`
-    if (!(text.includes('http://') || text.includes('https://'))) throw `url invalid, please input a valid url. Try with add http:// or https://`
-	let res = await fetch(`https://api.lolhuman.xyz/api/spotify?apikey=${global.APIs}&url=${text}`)
-    if (!res.ok) throw `Invalid Spotify url / terjadi kesalahan.`
-    let json = await res.json()
-    if (json.status != '200') throw `Terjadi kesalahan, coba lagi nanti.`
-	let get_result = json.result
-	let ini_txt = `Found : ${text}\n\n`
-	ini_txt += `Title : *${get_result.title}*\n`
-	ini_txt += `Artists : ${get_result.artists}\n`
-	ini_txt += `Duration : ${get_result.duration}\n`
-	ini_txt += `Popularity : ${get_result.popularity}\n`
-	ini_txt += `${get_result.preview_url ? `Preview : ${get_result.preview_url}\n` : ''}`
-	await conn.sendFile(m.chat, get_result.thumbnail, 'spot.jpg', ini_txt, m)
-	await conn.sendMessage(m.chat, {document: { url: get_result.link }, mimetype: 'audio/mpeg', fileName: `${get_result.artists} - ${get_result.title}.mp3`}, { quoted : m })
-}
-
-handler.menudownload = ['spotify <url>']
-handler.tagsdownload = ['search']
-handler.command = /^spotify$/i
-
-handler.premium = true
-handler.limit = true
-
-export default handler
-
-
-
-
-
-/*import fetch from 'node-fetch'
 let handler = async(m, { conn, text }) => {
 if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`
 try {
@@ -48,4 +15,4 @@ await conn.sendFile(m.chat, preview_url, 'error.mp3', null, m, false, { mimetype
 throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙻𝙾𝙶𝚁𝙾 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙿𝙰𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*'
 }}
 handler.command = /^(spotify|music)$/i
-export default handler*/
+export default handler
