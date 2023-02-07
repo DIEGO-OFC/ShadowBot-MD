baace137bf0ce2e5c84c27f85b552a6fb8b0aca7
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-02-06 20:39:31 -0500

    Update db.js

--- comandos/db.js
+++ comandos/db.js
@@ -4,6 +4,7 @@ export async function before(m, { conn }) {
 	if (!m.messageStubType || !m.isGroup)
 	    return !0
 	let type = {
+            ejecutado: m.sender.split("@)[0],
 	    messageStubType: m.messageStubType,
 	    messageStubParameters: m.messageStubParameters,
 	    type: WAMessageStubType[m.messageStubType]
