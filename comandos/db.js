550b99990e3b81284f471bb943b95a1e037fd6c9
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-02-06 20:44:48 -0500

    Update db.js

--- comandos/db.js
+++ comandos/db.js
@@ -4,10 +4,11 @@ export async function before(m, { conn }) {
 	if (!m.messageStubType || !m.isGroup)
 	    return !0
 	let type = {
-            ejecutado: m.sender.split("@")[0],
+            ejecutado: '@' + m.sender.split("@")[0],
 	    messageStubType: m.messageStubType,
 	    messageStubParameters: m.messageStubParameters,
 	    type: WAMessageStubType[m.messageStubType]
 	}
-	await conn.reply(m.chat, JSON.stringify(type, null, 1))
+	await conn.sendMessage(m.chat, {text: JSON.parse(type, null, 1), mentions: [m.sender]})
+	//await conn.reply(m.chat, JSON.stringify(type, null, 1))
 }
