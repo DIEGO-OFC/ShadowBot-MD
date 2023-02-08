ceec91e1100d7297d7ff0384b1c3f3d12b16252c
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-02-08 16:03:02 -0500

    Update rpg-rob.js

--- comandos/rpg-rob.js
+++ comandos/rpg-rob.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, text, usedPrefix, command, groupMetadata }) => {
     if (!text) return m.reply(`*• Etiquetɑ ɑl usuɑrio que quierɑ sɑqueɑr*\n\n*Ejemplo de uso:*\n1. ${usedPrefix}sɑqueɑr <usuɑrio/@tɑg>\n2.`)
     let _user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
     if (!_user in global.db.data.users) return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`)
-    if (global.db.data.users[_user] == undefined) return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`)
+    if (global.db.data.users[_user] == 'undefined') return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`)
     if (_user.startsWith(conn.user.jid.split`@`[0])) return m.reply('No puedes saquear a la bot :I')
     let user = global.db.data.users[m.sender]
     let uuser = global.db.data.users[_user]
