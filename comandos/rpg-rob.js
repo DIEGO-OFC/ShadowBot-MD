fbe682022e6c12ec01b5cfe0710f1ed4bac839d1
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-08 17:09:08 -0400

    Update rpg-rob.js

--- comandos/rpg-rob.js
+++ comandos/rpg-rob.js
@@ -58,7 +58,18 @@ let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
 
 return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
 
-
+function msToTime(duration) {
+    var milliseconds = parseInt((duration % 1000) / 100),
+      seconds = Math.floor((duration / 1000) % 60),
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+  
+    hours = (hours < 10) ? "0" + hours : hours
+    minutes = (minutes < 10) ? "0" + minutes : minutes
+    seconds = (seconds < 10) ? "0" + seconds : seconds
+  
+    return hours + " Hora(s) " + minutes + " Minuto(s)"
+  }
 
 
 
