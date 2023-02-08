6f7c65b1638dcd2090660a6cf63ab17b5ff96945
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-08 16:53:01 -0400

    Update rpg-rob.js

--- comandos/rpg-rob.js
+++ comandos/rpg-rob.js
@@ -5,7 +5,7 @@ const cooldown = 10800000
 
 let handler = async (m, { conn, text, usedPrefix, command, groupMetadata }) => {
     let rauser = groupMetadata.participants.map(v => v.jid)[Math.floor(Math.random() * groupMetadata.participants.map(v => v.jid).length)]
-    if (!text) return m.reply(`*• Etiquetɑ ɑl usuɑrio que quierɑ sɑqueɑr*\n\n*Ejemplo de uso:*\n1. ${usedPrefix}sɑqueɑr <usuɑrio/@tɑg>\n2. ${usedPrefix}sɑqueɑr @${rauser.split("@s.whatsapp.net")[0]}`)
+    if (!text) return m.reply(`*• Etiquetɑ ɑl usuɑrio que quierɑ sɑqueɑr*\n\n*Ejemplo de uso:*\n1. ${usedPrefix}sɑqueɑr <usuɑrio/@tɑg>\n2.`)
     let _user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
     if (!_user in global.db.data.users) return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`)
     if (global.db.data.users[_user] == undefined) return m.reply(`El usuɑrio no estά registrɑdo en lɑ bɑse de dɑtos!`)
