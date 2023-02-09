0fb473e6d25462a6c145759ef680b98cde1ff97e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-09 17:16:54 -0400

    Update afk-afk.js

--- comandos/afk-afk.js
+++ comandos/afk-afk.js
@@ -2,8 +2,12 @@ let handler = async (m, { text }) => {
 let user = global.db.data.users[m.sender]
 user.afk = + new Date
 user.afkReason = text
-m.reply(`*⚡ ${conn.getName(m.sender)} ESTARA AFK*\n\n*❍ MOTIVO (𝙰𝙵𝙺)${text ? ': ' + text : ''}*
-`)}
+let afk2 = `*❍ MOTIVO (𝙰𝙵𝙺)${text ? ': ' + text : ''}*
+`)`
+let afkinfo = `*⚡ ${conn.getName(m.sender)} ESTARA AFK*`
+conn.sendButton(m.chat, afkinfo, afk2, 
+[
+['ok', `ok`]], m)}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
