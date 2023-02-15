b89b2d481479ce0cb52c455d32728b80b81decdc
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-15 19:38:56 -0400

    Update afk-afk.js

--- comandos/afk-afk.js
+++ comandos/afk-afk.js
@@ -10,5 +10,5 @@ await conn.sendButton(m.chat, afkinfo, afk2,
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
-handler.dolares = 4
+handler.bitcoins = 1
 export default handler
