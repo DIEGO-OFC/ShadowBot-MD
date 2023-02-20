6c5fdbba3d74c09685f595c67038ed08f9590a4e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-20 18:58:21 -0400

    Create herramientas-leerimagen.js

---
+++ comandos/herramientas-leerimagen.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch'
+import { webp2png } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn }) => {
+	let q = m.quoted ? m.quoted : m,
+		mime = (q || q.msg).mimetype || q.mediaType || ''
+	if (/image/.test(mime)) {
+		let url = await webp2png(await q.download()),
+			res = await fetch(API('https://api.ocr.space', '/parse/imageurl', { apikey: '8e65f273cd88957', url }))
+		if (res.status !== 200) throw res.statusText
+		let json = await res.json()
+		m.reply(json?.ParsedResults?.[0]?.ParsedText)
+	} else throw '*[❕] esponde a una imagen!*'
+}
+handler.help = ['ocr']
+handler.tags = ['tools']
+handler.command = /^ocr|leerimagen$/i
+
+export default handler
