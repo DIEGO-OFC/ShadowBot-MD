b9c67805a110d5650340d8c40f2d6ac8e2838142
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-20 12:56:57 -0400

    Update owner-banuser.js

--- comandos/owner-banuser.js
+++ comandos/owner-banuser.js
@@ -4,13 +4,15 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {//prems
 
 let who
 //let pp = './galeria/menudorrat3.jpg'
+let txt = text.replace('@' + who.split`@`[0], '').trim()
+if (!txt) return conn.reply(m.chat, `$*ESCRIBA EL MOTIVO DEL BANEO\n` + `*${usedPrefix + command} @${who.split`@`[0]} *Motivo*`, m)  
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who)
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/9b1353deceded7f387713.jpg')
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
 let banu = `Tal vez el ban los haga reflexionar`
-let banu2 = `*✅ USUARIO BANEADO*\n*NOMBRE:* ${name}\n*NUMERO:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\nRazon: ${text ? ': ' + text : ''}*`
+let banu2 = `*✅ USUARIO BANEADO*\n*NOMBRE:* ${name}\n*NUMERO:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\nRazon: ${text}*`
 let user = global.db.data.users[who]
 if (!who) throw `*[❕] ETIQUETA A LA PERSONA QUE SERA BANEADA*`
 let users = global.db.data.users
