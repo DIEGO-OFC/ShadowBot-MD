5cd0f4cfcca5d5e9aa812e6347f7ef21a9e91cc4
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-26 10:05:31 -0400

    Update downloader-stickerly.js

--- comandos/downloader-stickerly.js
+++ comandos/downloader-stickerly.js
@@ -2,15 +2,12 @@ import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `*[❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command} memes*`
-try {
 let res = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${args[0]}&APIKEY=5bd33b276d41d6b4`)
 let json = await res.json()
 for (let data of (json.result.stickerlist || json)) {
 const stikers = await sticker(false, data, global.packname, global.author)
 conn.sendFile(m.chat, stikers, 'sticker.webp', '', m, { asSticker: true })}
 //await delay(1500)
-} catch {  
-await m.reply('*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾*')  
 }}
 handler.command = /^stickerly|stickerpack$/i
 export default handler
