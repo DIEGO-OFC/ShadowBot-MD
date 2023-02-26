74e3701a71302879e6f68e76558f113e1b4b69be
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-26 10:40:44 -0400

    Create dowloader-shipost.js

---
+++ comandos/dowloader-shipost.js
@@ -0,0 +1,12 @@
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+const res = await googleImage(shipost)
+let image = await res.getRandom()
+let link = image
+let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
+await conn.sendButton(m.chat, captionn, author, link, [['🔷 [STICKER]', `#s`]], m)}
+handler.help = ['shipost <query>', 'shipost <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(shipost)$/i
+handler.bitcoins = 5
+export default handler
