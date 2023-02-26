bc4d5e9aba8a8b6ed9bc2400bfdc3d10fbef48b7
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-26 10:24:33 -0400

    Update dowloader-stickerly.js

--- comandos/dowloader-stickerly.js
+++ comandos/dowloader-stickerly.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `*✳️ EJEMPLO DE USO DEL COMANDO ${usedPrefix + command} Anya Forger*`
 try {
-let res = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${args[0]}&APIKEY=${keysxteam}`)
+let res = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${args[0]}&APIKEY=HIRO`)
 let json = await res.json()
 for (let data of (json.result.stickerlist || json)) {
 const stikers = await sticker(false, data, global.packname, global.author)
