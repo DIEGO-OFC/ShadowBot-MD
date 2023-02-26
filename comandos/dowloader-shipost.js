13dedf5a85ec3c1629315dc19adfba938eccde1c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-26 10:44:30 -0400

    Update dowloader-shipost.js

--- comandos/dowloader-shipost.js
+++ comandos/dowloader-shipost.js
@@ -4,7 +4,7 @@ let shipost = 'shipost'
 const res = await googleImage(shipost)
 let image = await res.getRandom()
 let link = image
-let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
+let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* shipost\n🔗 *𝙻𝙸𝙽𝙺* ${link}`
 await conn.sendButton(m.chat, captionn, author, link, [['🔷 [STICKER]', `#s`]], m)}
 handler.help = ['shipost <query>', 'shipost <query>']
 handler.tags = ['internet', 'tools']
