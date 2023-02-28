3ea392cb486c1e50868191674341318f50c5ffb8
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-02-28 11:52:10 -0500

    Update ytvideodl.js

--- comandos/ytvideodl.js
+++ comandos/ytvideodl.js
@@ -4,10 +4,10 @@ import fs from 'fs'
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
 
   const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
-  if (args.length === 0) { reply(`*Ingresa un enlace de YouTube.*`); return; }
+  if (args.length === 0) { m.reply(`*Ingresa un enlace de YouTube.*`); return; }
   try {
       let urlYt = args[0];
-      if (!urlYt.startsWith("http")) { reply(`Ingresa un link de youtube`); return; }
+      if (!urlYt.startsWith("http")) { m.reply(`Ingresa un link de youtube`); return; }
      let infoYt = await ytdl.getInfo(urlYt);
      let titleYt = infoYt.videoDetails.title;
      let randomName = getRandom(".mp4");
@@ -21,17 +21,17 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
      if (fileSizeInMegabytes <= 999) {
      conn.sendMessage(from, { video: fs.readFileSync(`./${randomName}`), caption: `${titleYt}`}, { quoted: m });
      } else {
-     reply(`*El archivo es superior a 999 MB.*`);
+     m.reply(`*El archivo es superior a 999 MB.*`);
      }
      fs.unlinkSync(`./${randomName}`);
   } catch (e) {
-     reply(e.toString())
+     m.reply(e.toString())
   }
 
 }
 handler.help = ['ytd']
 handler.tags = ['downloader']
-handler.command = ['ytdocumento']
+handler.command = ['ytdocumentotest']
 handler.exp = 3
 
 export default handler
