11967aa13dad786e46f93e65fae52aa6b91bdfa2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-28 00:17:56 -0400

    Update rpg-work.js

--- comandos/rpg-work.js
+++ comandos/rpg-work.js
@@ -4,7 +4,7 @@ let d = Math.floor(Math.random() * 20)
 let time = global.db.data.users[m.sender].lastwork + 600000
 if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `*𝙴𝚜𝚝𝚊𝚜 𝚌𝚊𝚗𝚜𝚊𝚍𝚘, 𝚍𝚎𝚋𝚎𝚜 𝚍𝚎𝚜𝚌𝚊𝚗𝚜𝚊𝚛 𝚌𝚘𝚖𝚘 𝚖𝚒𝚗𝚒𝚖𝚘 ${msToTime(time - new Date())} 𝚙𝚊𝚛𝚊 𝚟𝚘𝚕𝚟𝚎𝚛 𝚊 𝚝𝚛𝚊𝚋𝚊𝚓𝚊𝚛!*`
 let pp = 'galeria/menudorrat3.jpg'
-conn.sendButton(m.chat, `*${pickRandom(global.work)} $${d} DOLARES*`, wm, pp,
+conn.sendButton(m.chat, `*${pickRandom(global.work)} $${d}* *DOLARES*`, wm, pp,
 [['balance', `/bal`]], m)
 global.db.data.users[m.sender].dolares += d * 1  
 global.db.data.users[m.sender].lastwork = new Date * 1
