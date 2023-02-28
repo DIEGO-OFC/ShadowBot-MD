13e931954743e01656bc51cc4ee7a58dec713652
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-28 17:59:00 -0400

    Update info-dasboard.js

--- comandos/info-dasboard.js
+++ comandos/info-dasboard.js
@@ -8,8 +8,8 @@ let pp = imagen1
   stats = stats.sort((a, b) => b.total - a.total)
   let txt = stats.slice(0, 15).map(({ name, total, last }, idx) => {
     return `${htki} ${idx + 1} ${htka}
-${htjava} 𝐂𝐌𝐃 ${htjava}\n⮕ ${name}
-${htjava} 𝐇𝐈𝐓 ${htjava}\n⮕ ${total}`
+${htjava} COMANDO ${htjava}\n⮕ ${name}
+${htjava} USOS ${htjava}\n⮕ ${total}`
 }).join`\n\n`
   m.reply(txt)}
 handler.help = ['dashboard']
