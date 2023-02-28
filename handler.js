327a1b4e92af102cd84aaff607e1b94ef12c2562
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-28 00:37:30 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,11 +1,10 @@
 import { generateWAMessageFromContent } from "@adiwajshing/baileys"
-
 import { smsg } from './lib/simple.js'
 import { format } from 'util'
 import { fileURLToPath } from 'url'
 import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
-import chalk from 'chalk'  
+import chalk from 'chalk' 
 
 /**
  * @type {import('@adiwajshing/baileys')}  
