04e0450800438872325affa04eecf7f4c7a448ed
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-28 00:03:21 -0400

    Update downloader-mediafire.js

--- comandos/downloader-mediafire.js
+++ comandos/downloader-mediafire.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import fetch from 'node-fetch'
+import cheerio from 'cheerio'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
    if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/xo6msugx6u2fhlw/1.19.30.apk/file*`
