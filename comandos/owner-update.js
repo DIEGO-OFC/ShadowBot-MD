bb6416fef28c9a13fcb38cd4b9d80af189224c97
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-02-28 01:54:42 -0400

    Update owner-update.js

--- comandos/owner-update.js
+++ comandos/owner-update.js
@@ -1,10 +1,10 @@
 
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
+m.reply(`*[ 𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 ]*`)
 try {  
 if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-m.reply(`*[ 𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 ]*`)
 await await conn.reply(m.chat, stdout.toString(), m)}
 } catch {
 var update = execSync('git remote set-url origin https://github.com/DIEGO-OFC/DORRAT-BOT-MD.git && git pull')
