04e05c0eff658c4a980e48714c7ecddab4c8a3f6
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-04 18:44:32 -0400

    Update buscador-mediafire.js

--- comandos/buscador-mediafire.js
+++ comandos/buscador-mediafire.js
@@ -8,19 +8,10 @@ if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳�
 let res = await axios.get(`https://www.mediafire.com/api/1.5/search/get_web_results.php?search_term=${text}`)
 let json = res.result
 let listSerch = []
-let teskd = `Links 𝗿𝗲𝗹𝗮𝗰𝗶𝗼𝗻𝗮𝗱𝗼𝘀 𝐜𝐨𝐧: ${args.join(" ")}`
 const sections = [{
-title: `ⓡⓔⓢⓤⓛⓣⓐⓓⓞⓢ`,
-rows: listSerch }]
-const listMessage = {
-text: teskd,
-footer: '𝐄𝐥𝐢𝐣𝐚 𝐮𝐧𝐚 𝐨𝐩𝐜𝐢𝐨𝐧 𝐲 𝐩𝐫𝐞𝐜𝐢𝐨𝐧𝐞 𝐄𝐧𝐯𝐢𝐚𝐫',
-title: " 『 𝗩𝗜𝗗𝗘𝗢𝗦 𝗥𝗘𝗟𝗔𝗖𝗜𝗢𝗡𝗔𝗗𝗢𝗦 』",
-buttonText: "[♦ 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 ♦]",
-sections}
-for (let i of json) {
-listSerch.push({title: i.title, description: '⇧ sᴇʟᴇᴄᴄɪᴏɴᴀ ᴇsᴛᴀ ᴏᴘᴄɪᴏɴ ᴘᴀʀᴀ ᴅᴇsᴄᴀʀɢᴀʀ ᴇsᴛᴇ ᴠɪᴅᴇᴏ ⇧', rowId: `${usedPrefix}xnxxdl ${i.link}`})} 
-conn.sendMessage(m.chat, listMessage, { quoted: m })
-}
+title: comienzo + ' ' + lenguajeGB['smsLista1']() + ' ' + fin,
+rows: [
+{title: "│🌐│MINECRAFT-SERVER", rowId:`${usedPrefix}mc-server`},  
+{title: lenguajeGB['smsLista7'](), rowId:`${usedPrefix}instalarbot`}]}]
 handler.command = /^mediafiresearch$/i
 export default handler
