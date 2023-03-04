c2da0a7d73a914898b7a10ba853a56116c1a2472
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-03 23:08:28 -0400

    Update info-infobot.js

--- comandos/info-infobot.js
+++ comandos/info-infobot.js
@@ -2,43 +2,10 @@ import util from 'util'
 import si from 'systeminformation'
 import { performance } from 'perf_hooks'
 import { sizeFormatter } from 'human-readable'
-let format = sizeFormatter({
-  std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
-  decimalPlaces: 2,
-  keepTrailingZeroes: false,
-  render: (literal, symbol) => `${literal} ${symbol}B`,
-})
-
 import { cpus as _cpus, totalmem, freemem } from 'os'
 import MessageType from '@adiwajshing/baileys'
 import fs from 'fs'
-let handler = async (m, { conn, usedPrefix }) => {
-  const used = process.memoryUsage()
-  const cpus = _cpus().map(cpu => {
-    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-    return cpu
-  })
-  const cpu = cpus.reduce((last, cpu, _, { length }) => {
-    last.total += cpu.total
-    last.speed += cpu.speed / length
-    last.times.user += cpu.times.user
-    last.times.nice += cpu.times.nice
-    last.times.sys += cpu.times.sys
-    last.times.idle += cpu.times.idle
-    last.times.irq += cpu.times.irq
-    return last
-  }, {
-    speed: 0,
-    total: 0,
-    times: {
-      user: 0,
-      nice: 0,
-      sys: 0,
-      idle: 0,
-      irq: 0
-    }
-  })
-  let old = performance.now()
+let handler = async (m, { conn, usedPrefix }) => {  
 let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime) 
 let totalreg = Object.keys(global.db.data.users).length
