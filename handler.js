fb24896b2d789e2b36f42df06d904b221b8917bd
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-05 09:21:55 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1211,7 +1211,7 @@ export async function handler(chatUpdate) {
                     continue // Limit habis
                 }
 if (!isPrems && comandos.dolares && global.db.data.users[m.sender].dolares < comandos.dolares * 1) {
-                    this.reply(m.chat, `*No tiene dólares* 💵\n\n*puede conseguir 💵 con el comando #trabajar o comprando con #buy dolares [cantidad]*`, m)
+                    this.reply(m.chat, `${ag}\n*No tiene dólares* 💵\n\n*puede conseguir 💵 con el comando #trabajar o comprando con #buy dolares [cantidad]*`, m)
                     continue 
 
 }
