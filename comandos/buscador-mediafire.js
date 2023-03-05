b950b393b3c19fcdd6eabc7eb4e525733a60d848
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-04 20:15:24 -0400

    Update buscador-mediafire.js

--- comandos/buscador-mediafire.js
+++ comandos/buscador-mediafire.js
@@ -16,7 +16,7 @@ let fnaf6 = `https://www.mediafire.com/file/2bk1h9bnhgouen7/Fnaf_6.apk/file`
 let ucn = `https://www.mediafire.com/file/jb17ec412gv91q6/UCN_v1.11.apk/file`
 let PicsArt = `https://www.mediafire.com/file/iwylgkdw4o22k45/v20.4.2.apk/file`
 let VivaCutPro = `https://www.mediafire.com/file/l2xro1fapo8v68h/v2.16.0.apk/file`
-let inShotPro = `https://www.mediafire.com/file/i30zxa81luipmec/v1.852.1371.apk/file`
+let InShotPro = `https://www.mediafire.com/file/i30zxa81luipmec/v1.852.1371.apk/file`
 let jgp = `JUEGOS 🎮`
 let app = `APPS PREMIUM`
 const sections = [{
@@ -34,7 +34,7 @@ title: comienzo + ' ' + app + ' ' + fin,
 rows: [
 {title: "PicsArt Premium", description: "Descargar PicsArt Premium Photo editor", rowId: `${usedPrefix}mediafire ${PicsArt}`},
 {title: "VivaCut Premium", description: "Descargar VivaCutPro", rowId: `${usedPrefix}mediafire ${VivaCutPro}`},
-{title: "inShot Premium", description: "Descargar inShotPro", rowId: `${usedPrefix}mediafire ${inShotPro}`}
+{title: "InShot Premium", description: "Descargar inShotPro", rowId: `${usedPrefix}mediafire ${InShotPro}`}
 
 ]}]
 const listMessage = {
