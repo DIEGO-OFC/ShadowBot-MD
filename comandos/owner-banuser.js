a4381a0f3897ab9c0217b41f347f06ee74a2e950
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-05 09:46:30 -0400

    Update owner-banuser.js

--- comandos/owner-banuser.js
+++ comandos/owner-banuser.js
@@ -24,9 +24,8 @@ else who = m.chat
 if (!who) throw `*[❕] ETIQUETA A LA PERSONA QUE SERA BANEADA*`
 let user = global.db.data.users[who]
 let name = conn.getName(who)
-if (!txt) return conn.reply(m.chat, `${mg}*ESCRIBA EL MOTIVO DEL BANEO*\n` + `*${usedPrefix + command} @${who.split`@`[0]} *Motivo*`, fkontak, m)  
-
 let txt = text.replace(name).trim()
+if (!txt) return conn.reply(m.chat, `${mg}*ESCRIBA EL MOTIVO DEL BANEO*\n` + `*${usedPrefix + command} @${who.split`@`[0]} *Motivo*`, fkontak, m)  
 let chatstext = text.replace(who.split("@")[0], '').replace("@", '')
 let users = global.db.data.users
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/9b1353deceded7f387713.jpg')
