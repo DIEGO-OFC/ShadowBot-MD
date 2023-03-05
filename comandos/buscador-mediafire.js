8cfab543bfa504e18ce4d45062f94b2c087462c7
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-04 20:18:58 -0400

    Update buscador-mediafire.js

--- comandos/buscador-mediafire.js
+++ comandos/buscador-mediafire.js
@@ -17,6 +17,7 @@ let ucn = `https://www.mediafire.com/file/jb17ec412gv91q6/UCN_v1.11.apk/file`
 let PicsArt = `https://www.mediafire.com/file/iwylgkdw4o22k45/v20.4.2.apk/file`
 let VivaCutPro = `https://www.mediafire.com/file/l2xro1fapo8v68h/v2.16.0.apk/file`
 let InShotPro = `https://www.mediafire.com/file/i30zxa81luipmec/v1.852.1371.apk/file`
+let SnapTube = `https://www.mediafire.com/file/gi94c68i9bva77f/v6.11.0.6116410.apk/file`
 let jgp = `JUEGOS 🎮`
 let app = `APPS PREMIUM`
 const sections = [{
@@ -34,7 +35,8 @@ title: comienzo + ' ' + app + ' ' + fin,
 rows: [
 {title: "PicsArt Premium", description: "Descargar PicsArt Premium Photo editor", rowId: `${usedPrefix}mediafire ${PicsArt}`},
 {title: "VivaCut Premium", description: "Descargar VivaCutPro", rowId: `${usedPrefix}mediafire ${VivaCutPro}`},
-{title: "InShot Premium", description: "Descargar inShotPro", rowId: `${usedPrefix}mediafire ${InShotPro}`}
+{title: "InShot Premium", description: "Descargar inShotPro", rowId: `${usedPrefix}mediafire ${InShotPro}`},
+{title: "SnapTube Premium", description: "Descargar SnapTube Premium", rowId: `${usedPrefix}mediafire ${SnapTube}`}
 
 ]}]
 const listMessage = {
