de6bd540ef1cc2db3d586f421ca171e617a427a3
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-06 14:40:37 -0400

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 try {
-if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*'
+if (!text) throw `*[❕] INGRESE UN TEXTO/ORDEN PARA EJECUTAR LA FUNCIÓN CHATGPT*\n\n*❍ EJEMPLO DE PETICIONES Y ORDENES*\n*❍ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*❍ ${usedPrefix + command} Codigo en JS para un juego de cartas*`
 await await m.reply(`*[ 𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 ]*`)
 let tiores = await await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=user-unique-id`)
 let hasil = await await tiores.json()
