a604e75b634d6464b68df011a4c20b6a360a690c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-08 00:44:31 -0400

    Update herramientas-whatmusic.js

--- comandos/herramientas-whatmusic.js
+++ comandos/herramientas-whatmusic.js
@@ -16,7 +16,7 @@ fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media)
 let res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`))
 let { code, msg } = res.status
 if (code !== 0) throw msg
-let { title, artists, album, genres, release_date, link } = res.metadata.music[0]
+let { title, artists, album, genres, release_date, url } = res.metadata.music[0]
 let txt = `
 🔰 RESULTADOS 🔰
 • 📌 TITULO: ${title}
@@ -24,7 +24,7 @@ let txt = `
 • 💾 ALBUM: ${album.name || 'No encontrado'}
 • 🌐 GENERO: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'No encontrado'}
 • 📆 FECHA: ${release_date || 'No encontrado'}
-${link}
+${url}
 `.trim()
 fs.unlinkSync(`./tmp/${m.sender}.${ext}`)
 m.reply(txt)
