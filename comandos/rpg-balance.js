f31ecf3a6b374b3261600851bcabf465ef14fe40
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-07 22:28:36 -0400

    Update rpg-balance.js

--- comandos/rpg-balance.js
+++ comandos/rpg-balance.js
@@ -4,12 +4,12 @@ if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
 let db = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/e01c177fb1c61f453c659.jpg')
-let bank = `╭━━━━━━ 𝐁𝐀𝐍𝐂𝐎 🏦 ━━━╾•
+let bank = `╭━━━━ 𝐁𝐀𝐍𝐂𝐎 🏦 ━━━╾•
 ❍ *Nombre:* ${name}
 ❍ *Diamantes:* ${global.db.data.users[who].limit} 💎
 ❍ *Tokens:* ${global.db.data.users[who].joincount} ☯️
 ❍ *Dólares:* $${global.db.data.users[who].dolares} 💸
-╰━━━━━━━━━━━━━━━━╾•`.trim()
+╰━━━━━━━━━━━━╾•`.trim()
 conn.sendMessage(m.chat, {
      image: {
          url: "https://telegra.ph/file/e01c177fb1c61f453c659.jpg"
