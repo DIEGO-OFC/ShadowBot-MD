0825e44ed4b8695811860b7249dedc87e72811a0
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-08 01:05:45 -0400

    Update downloader-imagen.js

--- comandos/downloader-imagen.js
+++ comandos/downloader-imagen.js
@@ -5,7 +5,9 @@ const res = await googleImage(text)
 let image = await res.getRandom()
 let link = image
 let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
-await await conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)}
+//*await await conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)*/
+m.reply(captionn)
+}
 handler.help = ['gimage <query>', 'imagen <query>']
 handler.tags = ['internet', 'tools']
 handler.command = /^(gimage|image|imagen)$/i
