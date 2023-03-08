10737c0f4e87caffdf53baca3905119f6b9360e9
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-08 00:16:37 -0400

    Update herramientas-whatmusic.js

--- comandos/herramientas-whatmusic.js
+++ comandos/herramientas-whatmusic.js
@@ -1,34 +1,49 @@
 import fs from 'fs'
-import acrcloud from 'acrcloud'
-let acr = new acrcloud({
-host: 'identify-eu-west-1.acrcloud.com',
-access_key: 'c33c767d683f78bd17d4bd4991955d81',
-access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
-})
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import FormData from 'form-data'
 
 let handler = async (m) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (/audio|video/.test(mime)) {
-let media = await q.download()
-let ext = mime.split('/')[1]
-fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media)
-let res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`))
-let { code, msg } = res.status
-if (code !== 0) throw msg
-let { title, artists, album, genres, release_date } = res.metadata.music[0]
-let txt = `
-𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾𝚂 𝙳𝙴 𝙻𝙰 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰
-
-• 📌 𝚃𝙸𝚃𝚄𝙻𝙾: ${title}
-• 👨‍🎤 𝙰𝚁𝚃𝙸𝚂𝚃𝙰: ${artists !== undefined ? artists.map(v => v.name).join(', ') : 'No encontrado'}
-• 💾 𝙰𝙻𝙱𝚄𝙼: ${album.name || 'No encontrado'}
-• 🌐 𝙶𝙴𝙽𝙴𝚁𝙾: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'No encontrado'}
-• 📆 𝙵𝙴𝙲𝙷𝙰 𝙳𝙴 𝙻𝙰𝙽𝚉𝙰𝙼𝙸𝙴𝙽𝚃𝙾: ${release_date || 'No encontrado'}
-`.trim()
-fs.unlinkSync(`./tmp/${m.sender}.${ext}`)
-m.reply(txt)
-} else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰 𝚄𝙽 𝙰𝚄𝙳𝙸𝙾*'
+	let q = m.quoted ? m.quoted : m
+	let mime = q.mediaType || ''
+	if (/image|video|audio|sticker|document/.test(mime)) {
+		let media = await q.download(true)
+		let data = await uploadFile(media)
+		let res = await fetch(`https://api.audd.io/?url=${data.url}&return=apple_music&api_token=945881d8079d620d74e93a218c42f8c6`)
+  let json = await res.json()
+  let x = json.result
+ return m.reply(`*RESULTADOS* :\n
+🔍 *TÍTULO ${x.title}*
+📍 *ARTISTA : ${x.artist}*
+⬇️ *Label* : ${x.label}*
+📂 *Album* : ${x.album}*
+📅 *FECHA* : ${x.release_date}*
+📎 *Link* : ${x.song_link}*`)
+	} else throw 'No media found'
 }
-handler.command = /^quemusica|quemusicaes|whatmusic$/i
+handler.help = ['whatmusic']
+handler.tags = ['tools']
+
+handler.command = /^(whatmusic)$/i
+
 export default handler
+
+async function uploadFile(path) {
+	let form = new FormData()
+	form.append('file', fs.createReadStream(path))
+	let res = await (await fetch('https://api.anonfiles.com/upload', {
+		method: 'post',
+		headers: {
+			...form.getHeaders()
+		},
+		body: form
+	})).json()
+	await fs.promises.unlink(path)
+	if (!res.status) throw res.error.message
+	let data = await fetch(res.data.file.url.full)
+	let $ = cheerio.load(await data.text())
+	return {
+		url: res.data.file.url.full
+		url2: res.data.file.url.short
+	}
+}
