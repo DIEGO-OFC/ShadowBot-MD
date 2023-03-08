5fa202fd0ed465f019da5404cf55f85ac1f23e67
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-03-07 22:27:42 -0400

    Update rpg-balance.js

--- comandos/rpg-balance.js
+++ comandos/rpg-balance.js
@@ -3,14 +3,32 @@ let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
 else who = m.sender
 let name = conn.getName(who) 
-let db = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/9b1353deceded7f387713.jpg')
+let db = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/e01c177fb1c61f453c659.jpg')
 let bank = `╭━━━━━━ 𝐁𝐀𝐍𝐂𝐎 🏦 ━━━╾•
 ❍ *Nombre:* ${name}
 ❍ *Diamantes:* ${global.db.data.users[who].limit} 💎
 ❍ *Tokens:* ${global.db.data.users[who].joincount} ☯️
 ❍ *Dólares:* $${global.db.data.users[who].dolares} 💸
 ╰━━━━━━━━━━━━━━━━╾•`.trim()
-await conn.sendMessage(m.chat, image: { url: "https://telegra.ph/file/e01c177fb1c61f453c659.jpg"}, caption: bank}, {quoted: m})}
+conn.sendMessage(m.chat, {
+     image: {
+         url: "https://telegra.ph/file/e01c177fb1c61f453c659.jpg"
+     },
+     caption: bank,
+     contextInfo: {
+         mentionedJid: [m.sender],
+         externalAdReply: {
+             title: `RPG BANK`,
+             sourceUrl: 'https://chat.whatsapp.com/',
+             mediaType: 1,
+             showAdAttribution: true,
+             thumbnailUrl: "https://telegra.ph/file/e01c177fb1c61f453c659.jpg",
+             //sourceUrl: 'https://chat.whatsapp.com/DBTKQLDShvoKjsNdLMs386'
+         }
+     }
+ }, {
+     quoted: m
+ })}
 handler.help = ['bal']
 handler.tags = ['xp']
 handler.command = ['bal', 'diamantes', 'diamond', 'balance', 'banco'] 
