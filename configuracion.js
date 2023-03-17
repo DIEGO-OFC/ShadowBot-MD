ea5bd61f4456f49500af42dcdbd4386467a66c53
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-03-16 20:54:29 -0600

    Diego no le sabe a lo ofuscado, Bruno le ayuda :D

--- configuracion.js
+++ configuracion.js
@@ -81,6 +81,11 @@ global.APIKeys = { // APIKey Here
   'https://violetics.pw': 'beta'
 }
 
+global.openai_key = 'sk-0'
+/* Consigue tu ApiKey en este enlace: https://platform.openai.com/account/api-keys */
+
+global.openai_org_id = 'org-3'
+/* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
 
 global.lenguajeGB = es
 
