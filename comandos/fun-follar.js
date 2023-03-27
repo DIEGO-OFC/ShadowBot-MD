87e8f1b9c2b040c5da5ccedbd0d957a2de850e9b
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-03-27 01:07:09 -0400

    Create fun-follar.js

---
+++ comandos/fun-follar.js
@@ -0,0 +1,13 @@
+
+ let handler = async (m, { conn, command, text }) => { 
+ let coger = `🥵 te acabas acabas de coger a ${text}!🥵 
+    
+  te acabas de coger a la puta de ${text} ⁩mientras gemia como una maldita puta
+    
+  ${text} ¡te han cogido! 😏
+ `.trim() 
+ m.reply(coger, null, { mentions: conn.parseMention(coger) })} 
+ handler.help = ['follar'] 
+ handler.tags = ['fun'] 
+ handler.command = /^(coger|violar|follar)$/i 
+ export default handler
