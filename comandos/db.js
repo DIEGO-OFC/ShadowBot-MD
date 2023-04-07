7a8b48a12d03c1b8fcb1d37c0d4835d790de14bd
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-04-06 21:36:21 -0400

    Update db.js

--- comandos/db.js
+++ comandos/db.js
@@ -1,4 +1,4 @@
-import { WAMessageStubType } from '@adiwajshing/baileys'
+/*import { WAMessageStubType } from '@adiwajshing/baileys'
 
 export async function before(m, { conn }) {
 	if (!m.messageStubType || !m.isGroup)
@@ -12,3 +12,4 @@ export async function before(m, { conn }) {
 	await conn.sendMessage(m.chat, {text: JSON.stringify(type, null, 1), mentions: [m.sender]})
 	//await conn.reply(m.chat, JSON.stringify(type, null, 1))
 }
+*/
