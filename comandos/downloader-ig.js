f2a7b306f91b1686d956248df8eedfbaa2fe9615
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-04-10 22:17:19 -0400

    Update downloader-ig.js

--- comandos/downloader-ig.js
+++ comandos/downloader-ig.js
@@ -1,26 +1,53 @@
-
 import fetch from 'node-fetch'
-import instagramGetUrl from 'fg-ig'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
-let results = (await instagramGetUrl(args[0])).url_list[0]
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${results}`)).text()
-let txt = `🔗 *Url:* ${shortUrl}`.trim()
-await conn.sendFile(m.chat, results, 'error.mp4', txt, m)}
-handler.command =/^(instagram|ig(dl)?)$/i
-handler.dfail = null
+import axios from 'axios'
+import cheerio from 'cheerio'
+import instagramGetUrl from 'instagram-url-direct'
+import vm from 'node:vm'
+import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link` 
+m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`)
+try {  
+const AA = await savefrom(args[0])    
+let shortUrl222 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt222 = `🔗 *Url:* ${shortUrl222}`.trim()
+await conn.sendFile(m.chat, AA.url[0].url, 'error.mp4', txt222, m)
+} catch {
+console.log('Error en el intento 1')  
+try {    
+const resultss = await instagramGetUrl(args[0]).url_list[0]    
+let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
+await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+} catch {  
+console.log('Error en el intento 2')  
+try {
+const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt4 = `🔗 *Url:* ${shortUrl3}`.trim()
+for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
+} catch {
+console.log('Error en el intento 3')  
+try {   
+let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+let json = await human.json()
+let videoig = json.result
+let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)     
+} catch { 
+console.log('Error en el intento 4, sin resultados')  
+throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`
+}}}}}
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
 export default handler
-
-/*import hx from 'hxz-api'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!(args[0])) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
-hx.igdl(args[0]).then(async (r) => {
-for (let i = 0; i < r.medias.length; i++) {
-let shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
-let txt = `🔗 *Url:* ${shortUrl}`.trim()
-await conn.sendFile(m.chat, r.medias[i].url, '', txt, m)
-}})}
-handler.command = /^instagramdl|instagram|igdl|ig$/i
-handler.dfail = null
-export default handler*/
+async function savefrom(urlL) {
+let body = new URLSearchParams({ "sf_url": encodeURI(urlL), "sf_submit": "", "new": 2, "lang": "id", "app": "", "country": "id", "os": "Windows", "browser": "Chrome", "channel": " main", "sf-nomad": 1 });
+let { data } = await axios({ "url": "https://worker.sf-tools.com/savefrom.php", "method": "POST", "data": body, "headers": { "content-type": "application/x-www-form-urlencoded", "origin": "https://id.savefrom.net", "referer": "https://id.savefrom.net/", "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36" }});
+let exec = '[]["filter"]["constructor"](b).call(a);';
+data = data.replace(exec, `\ntry {\ni++;\nif (i === 2) scriptResult = ${exec.split(".call")[0]}.toString();\nelse (\n${exec.replace(/;/, "")}\n);\n} catch {}`);
+let context = { "scriptResult": "", "i": 0 };
+vm.createContext(context);
+new vm.Script(data).runInContext(context);
+return JSON.parse(context.scriptResult.split("window.parent.sf.videoResult.show(")?.[1].split(");")?.[0])
+}
