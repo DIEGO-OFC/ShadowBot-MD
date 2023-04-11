17f478d6ca970c28a8d1f794328146ee200edcf3
VIRUZZ-OFC <113963745+VIRUZOFC@users.noreply.github.com>
2023-04-11 17:55:02 -0400

    Create owner-noetiquetar2.js

---
+++ comandos/owner-noetiquetar2.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, text }) => {
+    let name = await conn.getName(m.sender)
+
+conn.sendButton(m.chat, '*[ ⚠ ️] No etiquetes a VIRUZZ-OFC, si es algo urgente contacta con el a su chat privado*', wm3, [['📋 TERMINOS Y CONDICIONES', `#terminos`]], m)
+    let mentionedJid = [m.sender]
+}
+handler.customPrefix = /@595986674310/i
+handler.command = new RegExp
+
+export default handler
