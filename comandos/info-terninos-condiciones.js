c5e5eb0b3e246bdfb4d7424f526dd82fd5dc9bbd
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-04-10 22:27:25 -0400

    Update info-terninos-condiciones.js

--- comandos/info-terninos-condiciones.js
+++ comandos/info-terninos-condiciones.js
@@ -84,9 +84,9 @@ _- Al hacer uso de ciertos comandos que tengan como objetivo socavar la incomodi
 *~ Muchas Gracias Por tomarte el tiempo en informate sobre DorratBot
 `.trim()
 
-conn.sendButton(m.chat, terminoA, wm, pp, [
+await await conn.sendButton(m.chat, terminoA, wm, pp, [
 ['𝘿𝙊𝙉𝘼𝙍', `#donar`]], m)
-await await m.reply(`si no quiere leer ese texto tan grande puede verlo en:
+m.reply(`si no quiere leer ese texto tan grande puede verlo en:
 https://dorrat-bot-oficial.ml/terminos.html`)
 /*await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, {
 
