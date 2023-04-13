e46482a5b0f7c493828f4ab65a90555235d772e5
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-04-13 14:41:46 -0400

    Update owner-join.js

--- comandos/owner-join.js
+++ comandos/owner-join.js
@@ -1,4 +1,4 @@
-/*let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text, isMods, isOwner, isPrems }) => {
 let link = (m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text
@@ -22,11 +22,4 @@ handler.tags = ['premium']
 handler.command = /^join|nuevogrupo$/i
 export default handler
 
-let handler = async (m, { conn }) => {
-m.reply(global.join)
 
-}
-handler.command = /^(join|unete|nuevogrupo)$/i
-export default handler
-
-global.join =  `*LO SIENTO, EL BOT OFICIAL NO SE UNE A GRUPOS, PARA EVITAR SATURAS, PUEDES BUSCAR OTRO BOT OFICIAL, O BUSCANDO UN SUBBOT CON EL COMANDO #listjadibot o unete al grupo oficial ${md}*`*/
