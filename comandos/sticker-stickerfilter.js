b4884767b671308f51e7363b7ba4cc7265e189cd
Zastinian <zastinian@gmail.com>
2023-04-17 09:35:48 -0600

    feat: Removed unused code

--- comandos/sticker-stickerfilter.js
+++ comandos/sticker-stickerfilter.js
@@ -1,6 +1,5 @@
 import uploadImage from '../lib/uploadImage.js'
 import { sticker } from '../lib/sticker.js'
-import MessageType from '@adiwajshing/baileys'
 const effects = ['greyscale', 'invert', 'brightness', 'threshold', 'sepia', 'red', 'green', 'blue', 'blurple', 'pixelate', 'blur']
 
 let handler = async (m, { conn, usedPrefix, text }) => {
