1221d9a3916c37158f91fd502011d18c78eb162b
Zastinian <zastinian@gmail.com>
2023-04-17 09:32:51 -0600

    feat: Removed unused code

--- comandos/rpg-bot_temporal.js
+++ comandos/rpg-bot_temporal.js
@@ -1,5 +1,4 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args, usedPrefix, command, isPrems, isOwner, isROwner}) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
