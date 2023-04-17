36cb84cee6167fa26a4258203e59acdde75f6ae5
Zastinian <zastinian@gmail.com>
2023-04-17 09:29:00 -0600

    feat: Removed unused code

--- comandos/menu-descargas.js
+++ comandos/menu-descargas.js
@@ -1,18 +1,6 @@
 import { xpRange } from '../lib/levelling.js'
 
-import PhoneNumber from 'awesome-phonenumber'
-
-import { promises } from 'fs'
-
-import { join } from 'path'
-
 let handler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dirname, isOwner, text, isAdmin, isROwner }) => {
-
-  
-
-  
-
-const { levelling } = '../lib/levelling.js'
 
 //let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
 
