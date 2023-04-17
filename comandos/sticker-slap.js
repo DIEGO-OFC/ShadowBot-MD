ee833d04fa8a72b4a42bdf3a2718d1d02abf8aa7
Zastinian <zastinian@gmail.com>
2023-04-17 09:35:32 -0600

    feat: Removed unused code

--- comandos/sticker-slap.js
+++ comandos/sticker-slap.js
@@ -1,6 +1,5 @@
 import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
-import MessageType from '@adiwajshing/baileys'
 let handler = async (m, { conn}) => {
 try {   
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
