9b7cdcf7335640c2e14a24337cb227e05bc33b54
Zastinian <zastinian@gmail.com>
2023-04-17 09:29:33 -0600

    feat: Removed unused code

--- comandos/menu-menu.js
+++ comandos/menu-menu.js
@@ -3,10 +3,8 @@
 *         DORRAT-BOT-MD BY DIEGO-OFC        *
 /********************************************/
 import moment from 'moment-timezone'
-import fs, { promises } from 'fs'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, args, usedPrefix: _p, __dirname, isOwner, text, isAdmin, isROwner }) => {
-let vn = './media/menu.mp3'
 let d = new Date(new Date + 3600000)
 let locale = 'es'
 let week = d.toLocaleDateString(locale, { weekday: 'long' })
