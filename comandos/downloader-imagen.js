5a0e56991d2e92f0a7bdd4d588a7831f45c51b4f
Zastinian <zastinian@gmail.com>
2023-04-18 10:10:36 -0600

    perf: Down

--- comandos/downloader-imagen.js
+++ comandos/downloader-imagen.js
@@ -4,7 +4,6 @@ let handler = async (m, {conn, text, usedPrefix, command}) => {
   const res = await googleImage(text);
   let image = await res.getRandom();
   let link = image;
-  let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`;
 
   /*await await conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)*/
 
