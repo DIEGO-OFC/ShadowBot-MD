98b046a1cfcfe05f50f9dfa628ba66b3f20cb385
Zastinian <zastinian@gmail.com>
2023-04-18 10:06:41 -0600

    perf: Pelis

--- comandos/buscador-peliculas.js
+++ comandos/buscador-peliculas.js
@@ -2,7 +2,6 @@
 
 import fetch from "node-fetch";
 let handler = async (m, {text, usedPrefix, command, conn}) => {
-  let taguser = "@" + m.sender.split("@s.whatsapp.net")[0];
   if (!text) throw `*[❕] Ingrese el nombre de una película*\n\n*❍ EJEMPLO: ${usedPrefix + command} Batman*`;
   let a = await fetch(`https://www.omdbapi.com/?t=${text}&apikey=caba8d6f`);
   let x = await a.json();
@@ -25,16 +24,6 @@ let handler = async (m, {text, usedPrefix, command, conn}) => {
 *🔥 THE DORRAT - BOT 🔥*`;
 
   conn.sendMessage(m.chat, {image: {url: x.Poster}, caption: mov_txt, mentions: [m.sender]}, {quoted: m});
-
-  /*let caption = `*${comienzo}「 PELÍCULAS 」${fin}*\n
-🔍 *Titulo: ${x.Title || ''}*
-🗒️ *Año: ${x.Year || ''}*
-⏱️ *Duración: ${x.Runtime || ''}*
-📗 *Género: ${x.Genre || ''}*
-👥 *Actores: ${x.Actors || ''}*
-
-❕ Pedido por ${taguser}`
-conn.fakeReply(m.chat, caption, '0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast')*/
 };
 handler.command = /^(película|pelicula|peli)$/i;
 handler.limit = true;
