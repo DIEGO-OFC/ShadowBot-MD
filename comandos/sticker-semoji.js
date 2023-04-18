4ac8c9490e8d003c705e02caf7f828619a362f8c
Zastinian <zastinian@gmail.com>
2023-04-18 09:40:30 -0600

    fix: Sticker

--- comandos/sticker-semoji.js
+++ comandos/sticker-semoji.js
@@ -1,6 +1,5 @@
 import {sticker} from "../lib/sticker.js";
-import {EmojiAPI} from "emoji-api";
-const emoji = new EmojiAPI();
+import * as emoji from "emoji-api";
 
 let handler = async (m, {conn, args, usedPrefix, command, isPrems}) => {
   let er = `
