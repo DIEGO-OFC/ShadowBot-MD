8a24a87f21065e7ef3830f232233847c8e3c9eb1
Zastinian <zastinian@gmail.com>
2023-04-18 10:19:35 -0600

    perf: Welcome

--- comandos/gc-setwelcome.js
+++ comandos/gc-setwelcome.js
@@ -1,4 +1,4 @@
-let handler = async (m, {conn, text, isROwner, isOwner}) => {
+let handler = async (m, {text}) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text;
     m.reply("*[❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙱𝙸𝙽𝚅𝙴𝙽𝙸𝙳𝙰 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴 𝙿𝙰𝚁𝙰 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾*");
