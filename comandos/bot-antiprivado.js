fe7267375261076dbd472d58b4c78571cd28d6c6
Zastinian <zastinian@gmail.com>
2023-04-18 10:05:30 -0600

    perf: Priv

--- comandos/bot-antiprivado.js
+++ comandos/bot-antiprivado.js
@@ -1,9 +1,8 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, {isOwner, isROwner}) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
   if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
-  let chat = global.db.data.chats[m.chat];
   let bot = global.db.data.settings[this.user.jid] || {};
 
   if (bot.antiPrivate && !isOwner && !isROwner) {
