455d126e5cf799016ba7330eb05b3e4052a443b9
Zastinian <zastinian@gmail.com>
2023-04-18 10:19:12 -0600

    perf: Bye

--- comandos/gc-setbye.js
+++ comandos/gc-setbye.js
@@ -1,4 +1,4 @@
-let handler = async (m, {conn, text, isROwner, isOwner}) => {
+let handler = async (m, {text}) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text;
     m.reply("*[❗] 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙳𝙴 𝙳𝙴𝚂𝙿𝙴𝙳𝙸𝙳𝙰 𝙲𝙾𝙽𝙵𝙸𝙶𝚄𝚁𝙰𝙳𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴 𝙿𝙰𝚁𝙰 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾*");
