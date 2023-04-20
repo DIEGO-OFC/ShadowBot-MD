6a1fcb44e610c7a5f866c92a23e6b95880963585
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-04-20 13:04:44 -0400

    Update prueba.js

--- galeria/prueba.js
+++ galeria/prueba.js
@@ -1,5 +1,13 @@
 const prueba1 = `hola`;
 const prueba2 = `xd`;
 
-const prueba = [prueba1, prueba2];
+
+const prueba = pickRandom([prueba1, prueba2])
+
 export default prueba;
+
+function pickRandom(list) {
+
+    return list[Math.floor(list.length * Math.random())]
+
+}
