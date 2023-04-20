8048f546dc4dc860bfc4c4744c25cd823d59b69c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-04-19 22:42:56 -0400

    Create _prueja2.js

---
+++ comandos/_prueja2.js
@@ -0,0 +1,9 @@
+import {prueba1, prueba2} from './galeria/prueba.js'
+
+let handler = async (m, {text, command, conn}) => {
+
+m.reply(prueba1)}
+
+handler.command = ["prueba3"];
+
+export default handler;
