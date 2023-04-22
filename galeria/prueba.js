292ab47f24c035018415487b5c2d6f4c086cf519
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2023-04-21 19:05:36 -0500

    Update prueba.js

--- galeria/prueba.js
+++ galeria/prueba.js
@@ -1,9 +1,7 @@
-const prueba = `gola`
-
-
-export default {prueba}
-function pickRandom(list) {
-
-    return list[Math.floor(list.length * Math.random())]
-
+async function prueba() {
+return `gola`
 }
+
+export default { prueba }
+function pickRandom(list) {
+return list[Math.floor(list.length * Math.random())]}
