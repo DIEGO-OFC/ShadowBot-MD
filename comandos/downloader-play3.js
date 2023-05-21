1e47cfda9738fc7a78d4db2017ecdf92e8764255
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-05-21 14:23:12 -0400

    Update downloader-play3.js

--- comandos/downloader-play3.js
+++ comandos/downloader-play3.js
@@ -5,68 +5,8 @@ let handler = async (m, {conn, command, text, usedPrefix}) => {
     throw `*[❗𝐈𝐍𝐅𝐎❗] NOMBRE DE LA CANCION FALTANTE, POR FAVOR INGRESE EL COMANDO MAS EL NOMBRE/TITULO DE UNA CANCIÓN*\n\n*➢ EJEMPLO:*\n*${
       usedPrefix + command
     } Phonk*`;
-  try {
-    let vid = (await youtubeSearch(text)).video[0];
-    let {title, description, thumbnail, videoId, durationH, viewH, publishedTime} = vid;
-    const urll = "https://www.youtube.com/watch?v=" + videoId;
-    var doc = [
-      "pdf",
-      "zip",
-      "vnd.openxmlformats-officedocument.presentationml.presentation",
-      "vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      "vnd.openxmlformats-officedocument.wordprocessingml.document",
-    ];
-    var document = doc[Math.floor(Math.random() * doc.length)];
-    const buttons = [
-      {buttonId: `#ytmp3doc ${urll}`, buttonText: {displayText: "🎵 𝐀𝐔𝐃𝐈𝐎𝐃𝐎𝐂 🎵"}, type: 1},
-      {buttonId: `#ytmp4doc ${urll}`, buttonText: {displayText: "🎥 𝐕𝐈𝐃𝐄𝐎𝐃𝐎𝐂 🎥"}, type: 1},
-      {buttonId: `#playlist ${text}`, buttonText: {displayText: "📋 𝐌𝐀𝐒 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 📋"}, type: 1},
-    ];
-    let texto1 = `*◉— 𝐏𝐋𝐀𝐘 𝐃𝐎𝐂𝐔𝐌𝐄𝐍𝐓 —◉*\n╔═══════❰  *🔰*  ❱══════⬣
-║ *TITULO | TITLE*
-║ ${title}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *DESCRIPCIÓN  | DESCRIPTION*
-║ ${description}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *PUBLICADO | PUBLISHE*
-║ ${publishedTime}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *DURACION | DURATION*
-║ ${durationH}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *VISTAS | VIEWS*
-║ ${viewH}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ *URL*
-║ ${urll}
-╚═══════❰ *${vs}* ❱══════⬣`.trim();
-
-    let buttonMessage = {
-      document: {url: "https://wa.me/593959425714"},
-      fileName: "❏ 🌿 ʀᴇᴘʀᴏᴅᴜᴄᴛᴏʀ ᴅᴇ ʏᴏᴜᴛᴜʙᴇ",
-      mimetype: "application/vnd.ms-excel",
-      caption: texto1,
-      fileLength: "99999999999999",
-      mentions: [m.sender],
-      footer: wm,
-      buttons: buttons,
-      headerType: 4,
-      contextInfo: {
-        mentionedJid: [m.sender],
-        externalAdReply: {
-          showAdAttribution: true,
-          title: `${title}`,
-          mediaType: 2,
-          previewType: "VIDEO",
-          thumbnail: await (await fetch(thumbnail)).buffer(),
-          mediaUrl: `${urll}`,
-          sourceUrl: `https://github.com/DIEGO-OFC/DORRAT-BOT-MD`,
-        },
-      },
-    };
-    conn.sendMessage(m.chat, buttonMessage, {quoted: m});
-  } catch {
+  
+   
     try {
       let vid2 = await (await fetch(`https://api.lolhuman.xyz/api/ytsearch?apikey=${lolkeysapi}&query=${text}`)).json();
       let {videoId, title, views, published, thumbnail} = await vid2.result[0];
@@ -89,7 +29,7 @@ let handler = async (m, {conn, command, text, usedPrefix}) => {
       throw "*[❗𝐈𝐍𝐅𝐎❗] ERROR, POR FAVOR VUELVA A INTENTARLO*";
     }
   }
-};
+
 handler.help = ["playdoc", "play3"].map((v) => v + " <pencarian>");
 handler.tags = ["downloader"];
 handler.command = /^play3|playdoc?$/i;
