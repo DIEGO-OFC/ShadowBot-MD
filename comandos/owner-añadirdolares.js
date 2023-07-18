164cc40bdff64d1479b974b9a7035c0957963b04
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-18 00:15:08 -0400

    Actualizar owner-añadirdolares.js

--- comandos/owner-añadirdolares.js
+++ comandos/owner-añadirdolares.js
@@ -28,22 +28,7 @@ let handler = async (m, {conn, text}) => {
 
   users[who].dolares += dmt;
 
-  conn.sendHydrated(
-    m.chat,
-    `╭[ DOLARES 💵 ]⬣\n┃\n┃ღ *PARA:*\n┃ღ *${text}*\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *SE LE AÑADIÓ | NOW YOU HAVE*\n┃ღ *$${dmt} Dolare(s)* 💸\n┃\n╰━━━━━━━━━━━━━━⬣`,
-    wm3,
-    null,
-    md,
-    "†𝐃⃟𝕺𝐑⃯𝐑𝐇⃯𝚵𝐓᪣𝕭⃯𝚹⃯𝐓⃤† ",
-    null,
-    null,
-    [
-      ["💗 𝙈𝙚𝙣𝙪 𝘼𝙫𝙚𝙣𝙩𝙪𝙧𝙖 | 𝙍𝙋𝙂 💗", ".rpgmenu"],
-
-      ["𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️", "/menu"],
-    ],
-    m
-  );
+m.reply(`╭[ DOLARES 💵 ]⬣\n┃\n┃ღ *PARA:*\n┃ღ *${text}*\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ღ *SE LE AÑADIÓ | NOW YOU HAVE*\n┃ღ *$${dmt} Dolare(s)* 💸\n┃\n╰━━━━━━━━━━━━━━⬣`)
 };
 
 handler.help = ["adddi <@user>"];
