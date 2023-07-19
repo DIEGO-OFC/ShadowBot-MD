af97190cdeeabfd4b448b5f4df80ca42be95d184
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-19 18:57:28 -0400

    Actualizar adult-pack.js

--- comandos/adult-pack.js
+++ comandos/adult-pack.js
@@ -15,37 +15,8 @@ handler.command = /^(pack)$/i;
 export default handler;
 
 global.pack = [
-  "https://i.imgur.com/XbW7FO2.jpg",
-  "https://i.imgur.com/ciuzM98.jpg",
-  "https://i.imgur.com/uHsrrrx.jpg",
-  "https://i.imgur.com/PVi8YDi.jpg",
-  "https://i.imgur.com/FLC3ZXE.jpg",
-  "https://i.imgur.com/54m52tX.jpg",
-  "https://i.imgur.com/OsxYPgQ.jpg",
-  "https://i.imgur.com/vpw6Xnr.jpg",
-  "https://i.imgur.com/aAyPrZx.jpg",
-  "https://i.imgur.com/Gh3ORCd.jpg",
-  "https://i.imgur.com/rjGhygM.jpg",
-  "https://i.imgur.com/zdHVFEI.jpg",
-  "https://i.imgur.com/kAplnSG.jpg",
-  "https://i.imgur.com/15UiO8o.jpg",
-  "https://i.imgur.com/qjjyr6G.jpg",
-  "https://i.imgur.com/bQZRhBU.jpg",
-  "https://i.imgur.com/vpw6Xnr.jpg",
-  "https://i.imgur.com/aAyPrZx.jpg",
-  "https://i.imgur.com/Gh3ORCd.jpg",
-  "https://i.imgur.com/rjGhygM.jpg",
-  "https://i.imgur.com/0MhmmF4.jpg",
-  "https://i.imgur.com/2MX4wvq.jpg",
-  "https://i.imgur.com/HYL5ggu.jpg",
-  "https://i.imgur.com/7ZjOD2a.jpg",
-  "https://i.imgur.com/zbEUy3m.jpg",
-  "https://i.imgur.com/tZ6vlg6.jpg",
-  "https://i.imgur.com/jdPns8O.jpg",
-  "https://i.imgur.com/VyjBQHT.jpg",
-  "https://i.imgur.com/ozAGqBD.jpg",
-  "https://i.imgur.com/DsSj9S1.jpg",
-  "https://i.imgur.com/KYHpjNc.jpg",
+  "https://telegra.ph/file/f8bc435732650131192a5.jpg",
+
 ];
 
 handler.limit = 3;
