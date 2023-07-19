d39a5751335b5367e702f57f637617248ad69229
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-19 14:53:20 -0400

    Actualizar game-_suitpvp.js

--- comandos/game-_suitpvp.js
+++ comandos/game-_suitpvp.js
@@ -1,7 +1,7 @@
 
 let handler = m => m;
 
-let handler = { async before (m, {this}) {
+let handler = { async before (m, {conn}) {
  this.suit = this.suit ? this.suit : {} 
  if (db.data.users[m.sender].suit < 0) db.data.users[m.sender].suit = 0 
  let room = Object.values(this.suit).find(room => room.id && room.status && [room.p, room.p2].includes(m.sender)) 
