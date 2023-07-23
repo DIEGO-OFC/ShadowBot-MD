5283931210c37ae7b86f07eb9f77b43662317d1f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-23 11:37:00 -0400

    Actualizar downloader-yta.2.js

--- comandos/downloader-yta.2.js
+++ comandos/downloader-yta.2.js
@@ -2,7 +2,7 @@
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper' 
  import fetch from 'node-fetch' 
  let handler = async (m, { conn, args }) => { 
- if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*' 
+ if (!args[0]) throw '*[✳️] INGRESE EL COMANDO MAS UN ENLACE DE YOUTUBE.*' 
  await m.reply(`*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴀᴜᴅɪᴏ...⏳_*\n\n*◉ Sɪ Sᴜ ᴀᴜᴅɪᴏ ɴᴏ ᴇs ᴇɴᴠɪᴀᴅᴏ, ᴘʀᴜᴇʙᴇ ᴄᴏɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`) 
  try { 
  let q = '128kbps' 
