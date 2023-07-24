5bcc0c7236ee33aef7ac8fe3842b856d88e3fce6
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-24 16:34:30 -0400

    Actualizar downloader-mediafire.js

--- comandos/downloader-mediafire.js
+++ comandos/downloader-mediafire.js
@@ -1,21 +1,21 @@
-import axios from 'axios' 
- import fetch from 'node-fetch' 
- import cheerio from 'cheerio' 
- import { mediafiredl } from '@bochilteam/scraper' 
+import axios from 'axios';
+ import fetch from 'node-fetch';
+ import cheerio from 'cheerio';
+ import { mediafiredl } from '@bochilteam/scraper';
   
  let handler = async (m, { conn, args, usedPrefix, command }) => { 
- if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*` 
+ if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝚅𝙰𝙻𝙸𝙳𝙾 𝙳𝙴 𝙼𝙴𝙳𝙸𝙰𝙵𝙸𝚁𝙴, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*`;
  try{ 
- let resEX = await mediafiredl(args[0]) 
+ let resEX = await mediafiredl(args[0]);
  let captionES = ` 
- ❒➢ 🔍 *NOMBRE:* ${resEX.filename} 
- ❒➢ 📦 *PESO:* ${resEX.filesizeH} 
- ❒➢ 🔰 *TIPO:* ${resEX.ext} 
+ ❒➢ 🔍 *NOMBRE:* ${resEX.filename};
+ ❒➢ 📦 *PESO:* ${resEX.filesizeH};
+ ❒➢ 🔰 *TIPO:* ${resEX.ext};
   
  ➢ *⏰ ENVIANDO ARCHIVO, POR FAVOR ESPERE. . . .* 
- `.trim()
+ `.trim();
  m.reply(captionES) 
- await conn.sendFile(m.chat, resEX.url, resEX.filename, '', m, null, { mimetype: resEX.ext, asDocument: true })     
+ await conn.sendFile(m.chat, resEX.url, resEX.filename, '', m, null, { mimetype: resEX.ext, asDocument: true });    
  } catch { 
  try {   
  let res = await mediafireDl(args[0]) 
@@ -26,15 +26,16 @@ import axios from 'axios'
 ❒➢ 🔰 *TIPO:* ${mime}
 
 ➢ *⏰ ENVIANDO ARCHIVO, POR FAVOR ESPERE. . . .*  
- `.trim() 
- await m.reply(caption) 
- await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true }) 
+ `.trim();
+ await m.reply(caption);
+ await conn.sendFile(m.chat, link, name, '', m, null, { mimetype: mime, asDocument: true });
  } catch {   
- await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*') 
+ await m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE*');
  }}} 
- handler.help = ['mediafire'].map(v => v + ' <url>') 
- handler.tags = ['downloader'] 
- handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i 
+ handler.help = ['mediafire'].map(v => v + ' <url>');
+ handler.tags = ['downloader'];
+ handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i;
+handler.dolares = 8;
  export default handler 
   
  async function mediafireDl(url) { 
