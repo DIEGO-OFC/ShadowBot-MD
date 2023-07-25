a328715bd2133a8ebcd1b58dbc0602376d2ce626
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-24 21:25:35 -0400

    Actualizar herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -24,7 +24,7 @@ let handler = async (m, {
         } catch (e) {
         try {
             let res = await ChatGptV3(text)
-           // await m.reply(res[0].generated_text)
+           await m.reply(res[0].generated_text)
             } catch (e) {
             throw eror
             }
