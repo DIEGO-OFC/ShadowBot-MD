788b762420c130f81683ba42dba5f532bafed847
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-25 01:08:47 -0400

    Update herramientas-chatgpt.js

--- comandos/herramientas-chatgpt.js
+++ comandos/herramientas-chatgpt.js
@@ -1,109 +1,5 @@
-import puppeteer from "puppeteer"
-import fetch from "node-fetch"
-
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    command
-}) => {
-    let text
-    if (args.length >= 1) {
-        text = args.slice(0).join(" ")
-    } else if (m.quoted && m.quoted.text) {
-        text = m.quoted.text
-    } else throw "Input Teks"
-    await m.reply(wait)
-    try {
-        let res = await ChatGpt(text)
-      await m.reply(res)
-    } catch (e) {
-        try {
-            let res = await ChatGptV2(text)
-           await m.reply(res.answer)
-        } catch (e) {
-        try {
-            let res = await ChatGptV3(text)
-           await m.reply(res[0].generated_text)
-            } catch (e) {
-            throw eror
-            }
-        }
-    }
-}
-handler.help = ["chatgpt"]
-handler.tags = ["internet"]
-handler.command = /^chatgpt$/i
-
-export default handler
-
-/* New Line */
-async function ChatGpt(input) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://chatgpt.botwa.me/');
-
-    // Masukkan teks ke dalam tag div dengan class input
-    await page.type('div.input', input);
-
-    // Masukkan teks ke dalam tag input dengan class user-input
-    await page.type('input.user-input', input);
-
-    // Klik tombol dengan class button-submit
-    await page.click('button.button-submit');
-
-    // Tunggu sampai pesan muncul di dalam class message animated fadeInLeftBig, kecuali yang sebelumnya
-    await page.waitForSelector('.message.animated.fadeInLeftBig:not(:first-child)');
-
-    // Ambil teks dari pesan yang muncul
-    const message = await page.$eval('.message.animated.fadeInLeftBig:not(:first-child)', el => el.textContent);
-    // Cetak output pesan yang muncul
-    return message;
-    await browser.close();
-}
-
-async function ChatGptV2(query) {
-  try {
-    const response = await fetch(`https://api.caonm.net/api/ai/o.php?msg=${query}`);
-    if (!response.ok) {
-      throw new Error('Network response was not OK');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
-}
-async function ChatGptV3(query) {
-    try {
-        const response = await fetch(
-            "https://api-inference.huggingface.co/models/gpt2", {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                    Authorization: "Bearer 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",
-                },
-                body: JSON.stringify({
-                    inputs: query
-                }),
-            }
-        );
-
-        return await response.json();
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
-
-
-
-
-
-
-/*import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 import cheerio from 'cheerio'
-import gpt from 'api-dylux'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `*[❕] HAGA PETICIÓN, EJEMPLO*
 *${usedPrefix + command} que es el planeta*\n*SI QUIERES EN MODO HARD*:
@@ -114,15 +10,18 @@ await conn.sendPresenceUpdate('composing', m.chat)
 var syms = `${jailbreak}`
 
 
-let res = await gpt.ChatGpt(text, syms)
-let rgapt = `${res.text}`
+let fgapi1 = await fetch(`https://api-fgmods.ddns.net/api/info/openai?text=${text}&symsg=${syms}&apikey=fg-dylux`)
+let fgjson1 = await fgapi1.json()
+if (fgjson1.result == 'error' || fgjson1.result == '' || !fgjson1.result) return XD //causar error undefined para lanzar msg de error
+let fgjson1_result = await translate(`${fgjson1.result}`, { to: 'es', autoCorrect: true })  
+    let fgresultd = `${fgjson1_result.text.trim()}`
  await await conn.sendMessage(
       m.chat,
       {
         image: {
           url: "https://telegra.ph/file/10e013d9ae4d9cdf5af14.jpg",
         },
-        caption: rgapt,
+        caption: fgresultd,
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
