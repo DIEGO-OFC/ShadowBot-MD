bcc1fc38844ce16ec37ba02e60987b09f131753c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-26 21:26:15 -0400

    Actualizar menu-menu.js

--- comandos/menu-menu.js
+++ comandos/menu-menu.js
@@ -4,7 +4,12 @@
 import moment from 'moment-timezone';
 import cheerio from 'cheerio';  
 import fetch from 'node-fetch';  
-let handler = async (m {conn, args, usedPrefix, text, command
+let handler = async (m {
+conn, 
+args, 
+usedPrefix, 
+text, 
+command
 }) => {
     let d = new Date(new Date() + 3600000); 
    let locale = "es"; 
