0d68f13bde3e4a4ad1f71eda041d1646959eb6b0
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-28 00:43:10 -0400

    Update configuracion.js

--- configuracion.js
+++ configuracion.js
@@ -92,7 +92,7 @@ var openai_key = "sk-0";
 var openai_org_id = "org-3";
 /* Consigue tu ID de organizacion en este enlace: https://platform.openai.com/account/org-settings */
 
-var lenguajeGB = es;
+global.lenguajeGB = es;
 
 /* ______________________________________________ */
 const creadorname = "𝐃𝐈𝐄𝐆𝐎-𝐎𝐅𝐂";
