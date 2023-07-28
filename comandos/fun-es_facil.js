256e970793ebdd9fff01c119ecb31e9e0d41648b
Darlyn <89032212+darlyn1234@users.noreply.github.com>
2023-07-28 14:28:22 -0500

    Update fun-es_facil.js

--- comandos/fun-es_facil.js
+++ comandos/fun-es_facil.js
@@ -2,7 +2,7 @@ var handler = async (m, {conn, groupMetadata}) => {
   const toM = (a) => "@" + a.split("@")[0];
   const ps = groupMetadata.participants.map((v) => v.id);
   const a = ps.getRandom();
-  conn.sendMessage(m.chat, {text: `Pero no tanto como la mama de ${toM(a)}`, mentions: conn.parseMention[a]}, {quoted: m})
+  conn.sendMessage(m.chat, {text: `Pero no tanto como la mama de ${toM(a)}`, mentions: [a]}, {quoted: m})
   //conn.sendMessage(m.chat, { text: `Pero no tanto como la mama de ${toM(a)}`}, { quoted: m}, { mentions: conn.parseMention[a]})
 };
 
