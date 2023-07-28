32f4f6a5943b0e4a475cd571652b8e8c228425d4
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-28 13:56:16 -0400

    Actualizar fun-es_facil.js

--- comandos/fun-es_facil.js
+++ comandos/fun-es_facil.js
@@ -2,7 +2,7 @@ let handler = async (m, {conn, groupMetadata}) => {
   let toM = (a) => "@" + a.split("@")[0];
   let ps = groupMetadata.participants.map((v) => v.id);
   let a = ps.getRandom();
-  conn.sendButton(m.chat, `Pero no tanto como la mama de ${toM(a)}`, author, null, [["Callate bot", "calla bot"]], m, {mentions: [a]});
+  conn.sendMessage(m.chat, { text: `Pero no tanto como la mama de ${toM(a)}`}, { quoted: m}, { mentions: [a]})
 };
 
 handler.customPrefix = /^facil|es facil|fácil|es fácil|fasil|es fasil|la chupa bien|se le da bien chupar|chupa bn|chupa bien|la chupa bn/i;
