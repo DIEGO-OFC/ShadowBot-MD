8dcd9ea4fe070140d7d19f072a0963fdf9c4ccb9
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-29 16:25:40 -0400

    Actualizar maker-itssostupid.js

--- comandos/maker-itssostupid.js
+++ comandos/maker-itssostupid.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn, args}) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   conn.sendFile(
     m.chat,
-    global.API("https://some-random-api.ml", "/canvas/its-so-stupid", {
+    global.API("https://some-random-api.com", "/canvas/its-so-stupid", {
       avatar: await conn.profilePictureUrl(who, "image").catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png"),
       dog: text || "im+stupid",
     }),
