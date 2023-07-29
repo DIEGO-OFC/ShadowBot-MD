312e61a00389b4827a96b62f0c642ba25b8040bb
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-29 01:53:53 -0400

    Actualizar downloader-soundcloud.js

--- comandos/downloader-soundcloud.js
+++ comandos/downloader-soundcloud.js
@@ -2,19 +2,19 @@ import fetch from 'node-fetch';
  var handler = async (m, {conn, text}) => { 
    if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`; 
    try { 
-     const res = await fetch(`https://api-v2.soundcloud.com/search/tracks?q=${text}&client_id=iZIs9mchVcX5lhVRyQGGAYlNPVldzAoX`); 
-     const json2 = await res.json(); 
+     var res = await fetch(`https://api-v2.soundcloud.com/search/tracks?q=${text}&client_id=iZIs9mchVcX5lhVRyQGGAYlNPVldzAoX`); 
+     var json2 = await res.json(); 
      let permalinkUrl; 
      if (json2.collection.length > 0) { 
-       const randomIndex = Math.floor(Math.random() * json2.collection.length); 
-       const randomObject = json2.collection[randomIndex]; 
+       var randomIndex = Math.floor(Math.random() * json2.collection.length); 
+       var randomObject = json2.collection[randomIndex]; 
        permalinkUrl = randomObject.permalink_url; 
      } else { 
        permalinkUrl = await json2.collection[0].permalink_url; 
      } 
-     const res2 = await fetch(`https://api.akuari.my.id/downloader/scdl?link=${permalinkUrl}`); 
-     const json = await res2.json(); 
-     const shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${json.link}`)).text(); 
+     var res2 = await fetch(`https://api.akuari.my.id/downloader/scdl?link=${permalinkUrl}`); 
+     var json = await res2.json(); 
+     var shortUrl = await (await fetch(`https://tinyurl.com/api-create.php?url=${json.link}`)).text(); 
      const soundcloudt = `❒═══❬ 𝐒𝐎𝐔𝐍𝐃𝐂𝐋𝐎𝐔𝐃 ❭═══╾❒\n┬\n├‣✨ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${json.title}\n┴\n┬\n├‣💚 *𝚄𝚁𝙻 𝙳𝙸𝚁𝙴𝙲𝚃𝙾:* ${shortUrl}\n┴\n┬\n├‣ *- 𝙴𝚗𝚟𝚒𝚊𝚗𝚍𝚘 𝚖𝚞𝚜𝚒𝚌𝚊...*\n├‣ _${wm}_\n┴`; 
      await conn.sendFile(m.chat, json.thumb, '', soundcloudt, m); 
      await conn.sendMessage(m.chat, {audio: {url: json.link}, fileName: `${json.title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m}); 
