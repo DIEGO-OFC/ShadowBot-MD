dacc405e5a23cc275ae49ce98ef75a02faabaa1f
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-29 16:28:40 -0400

    Actualizar maker-ytcomment.js

--- comandos/maker-ytcomment.js
+++ comandos/maker-ytcomment.js
@@ -2,7 +2,7 @@ let handler = async (m, {conn, text}) => {
   if (!text) throw "No Text";
   conn.sendFile(
     m.chat,
-    global.API("https://some-random-api.ml", "/canvas/youtube-comment", {
+    global.API("https://some-random-api.com", "/canvas/youtube-comment", {
       avatar: await conn.profilePictureUrl(m.sender, "image").catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png"),
       comment: text,
       username: conn.getName(m.sender),
