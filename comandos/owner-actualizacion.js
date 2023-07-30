dcd5c72042b0f9ea990d4c211b21abe55abe2e52
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 01:01:38 -0400

    Update owner-actualizacion.js

--- comandos/owner-actualizacion.js
+++ comandos/owner-actualizacion.js
@@ -16,7 +16,7 @@ let handler = async (m, {conn, text, usedPrefix, command}) => {
   let [_, code] = grupo.match(linkRegex) || [];
 
   if (users == 584125778026 || users == 584125778026)
-    try {
+    
       if (!text) return m.reply(`*Falta Texto*`);
       let res = await conn.groupAcceptInvite(code);
       await conn.sendMessage(
@@ -25,18 +25,6 @@ let handler = async (m, {conn, text, usedPrefix, command}) => {
         {quoted: fkontak}
       );
       await m.reply(`✅ *MENSAJE ENVIADO CON ÉXITO* `);
-    } catch (e) {
-      await conn.sendButton(
-        m.chat,
-        `\n${wm}`,
-        lenguajeGB["smsMalError3"]() + "#report " + usedPrefix + command,
-        null,
-        [[lenguajeGB.smsMensError1(), `#reporte ${lenguajeGB["smsMensError2"]()} *${usedPrefix + command}*`]],
-        m
-      );
-      console.log(`❗❗ ${lenguajeGB["smsMensError2"]()} ${usedPrefix + command} ❗❗`);
-      console.log(e);
-    }
   else {
     await m.reply("```USTED NO TIENE AUTORIZACIÓN PARA USAR ESTE COMANDO.```");
   }
