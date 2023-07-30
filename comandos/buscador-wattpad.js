e57c6576f8de8e873528140a84377255cfd479f0
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 18:46:42 -0400

    Update buscador-wattpad.js

--- comandos/buscador-wattpad.js
+++ comandos/buscador-wattpad.js
@@ -1,11 +1,14 @@
 import fetch from 'node-fetch'
+import translate from '@vitalets/google-translate-api'
 
 let handler = async(m, { conn, text }) => {
   if (!text) throw `Mau Cari Apa?`
   let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&apikey=uwgflzFEh6`)
   let anu = await res.json()
   anu = anu.data.map((v) => `*Title:* ${v.title}\n*Reads:* ${v.reads}\n*Vote:* ${v.vote}\n*Chapter:* ${v.chapter}\n*Link:* ${v.link}\nDescription:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
-  conn.sendFile(m.chat, anu[0].thumb, 'dorrat.jpg', anu, m)
+  let akuariapiresult2 = await translate(`${anu}`, { to: 'es', autoCorrect: true })
+  
+  conn.sendFile(m.chat, anu[0].thumb, 'dorrat.jpg', akuariapiresult2, m)
 }
 handler.help = ['wattpad']
 handler.tags = ['internet']
