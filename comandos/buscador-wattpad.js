6cc63950b4c123efd02934303710034362c3127e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 19:01:20 -0400

    Update buscador-wattpad.js

--- comandos/buscador-wattpad.js
+++ comandos/buscador-wattpad.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
   if (!text) throw `Mau Cari Apa?`
   let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&lc=es&apikey=uwgflzFEh6`)
   let anu = await res.json()
-  anu = anu.data.map((v) => `*Title:* ${v.titulo}\n*Reads:* ${v.reads}\n*Vote:* ${v.vote}\n*Chapter:* ${v.chapter}\n*Link:* ${v.link}\nDescription:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
+  anu = anu.data.map((v) => `*Title:* ${v.title}\n*Reads:* ${v.reads}\n*Vote:* ${v.vote}\n*Chapter:* ${v.chapter}\n*Link:* ${v.link}\nDescription:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
  // let akuariapiresult2 = await translate(`${anu}`, { to: 'es', autoCorrect: true })
   
   conn.sendFile(m.chat, anu[0].thumb, 'dorrat.jpg',  anu, m)
