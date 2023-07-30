c97ed178fede086d7d1887ba2d706700686f2143
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 02:08:35 -0400

    Update afk-afk.js

--- comandos/afk-afk.js
+++ comandos/afk-afk.js
@@ -2,8 +2,7 @@ let handler = async (m, {text}) => {
   let user = global.db.data.users[m.sender];
   user.afk = +new Date();
   user.afkReason = text;
-  let afk2 = `*❍ MOTIVO (𝙰𝙵𝙺)${text ? ": " + text : ""}*\n❍ NO LO ETIQUETEN`;
-  let afkinfo = `*DORRAT - AFK*\n\n*👤 USUARIO QUE ESTARA AFK: ${conn.getName(m.sender)}*`;
+  let afkinfo = `*DORRAT - AFK*\n\n*👤 USUARIO QUE ESTARA AFK: ${conn.getName(m.sender)}*\n*❍ MOTIVO (𝙰𝙵𝙺)${text ? ": " + text : ""}*\n❍ NO LO ETIQUETEN`;
   await m.reply(afkinfo)
 };
 handler.help = ["afk [alasan]"];
