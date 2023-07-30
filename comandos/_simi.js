fa3026999b5f377cbeb98c079e1d0da4cd08349a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 01:32:41 -0400

    Update _simi.js

--- comandos/_simi.js
+++ comandos/_simi.js
@@ -1,14 +1,30 @@
-import fetch from "node-fetch";
-let handler = (m) => m;
+import fetch from 'node-fetch';
+var handler = (m) => m;
 
 handler.before = async (m) => {
-  let chat = global.db.data.chats[m.chat];
+  var chat = global.db.data.chats[m.chat];
   if (chat.simi) {
     if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
-    let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje());
-    let data = await ressimi.json();
-    if (data.success == "No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.") await m.reply("😵‍💫");
-    await m.reply(data.success);
+    let textodem = m.text;
+    try {
+      const ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`);
+      const data = await ressimi.json();
+      if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`); /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
+      await m.reply(data.success);
+    } catch {
+      /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
+      if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
+      if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello');
+      if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO');
+      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + textodem);
+      const resu = await reis.json();
+      const nama = m.pushName || '1';
+      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+      const res = await api.json();
+      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+      const resu2 = await reis2.json();
+      await m.reply(resu2[0][0][0]);
+    }
     return !0;
   }
   return true;
