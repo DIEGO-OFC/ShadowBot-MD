8afa200287341451d92f305c73bb28d83eb69fa6
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 18:01:07 -0400

    Update buscador-wattpad.js

--- comandos/buscador-wattpad.js
+++ comandos/buscador-wattpad.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async(m, { conn, text }) => {
   if (!text) throw `Mau Cari Apa?`
-  let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&apikey=apikeylu`)
+  let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&apikey=uwgflzFEh6`)
   let anu = await res.json()
   anu = anu.data.map((v) => `*Title:* ${v.title}\n*Reads:* ${v.reads}\n*Vote:* ${v.vote}\n*Chapter:* ${v.chapter}\n*Link:* ${v.link}\nDescription:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
   conn.sendFile(m.chat, anu.data[0].thumb, '', anu, m)
