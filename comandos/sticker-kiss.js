fcec6e38b4089d3b07022ad3d0547acd64c650e2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 07:59:58 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -1,9 +1,9 @@
 import {sticker} from "../lib/sticker.js";
 import fetch from "node-fetch";
 let handler = async (m, {conn}) => {
-let besoo = await axios.get(`https://nekos.life/api/v2/img/kiss`);
+let besoo = await axios.get(`https://nekos.life/api/kiss`);
 let buttonMessage = {
-    image: {url: besoo.data.url},
+    video: {url: besoo.data.url},
     caption: `+${m.sender.split("@")[0]} le dio besos a ${m.mentionedJid
         .map((user) => (user === m.sender ? "alguien " : `+${user.split("@")[0]}`))
         .join(", ")}`,
