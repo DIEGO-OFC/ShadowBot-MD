ad151c5dde185766dcfd7912e2117c472b2aae8a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 14:35:20 -0400

    Actualizar random-meme.js

--- comandos/random-meme.js
+++ comandos/random-meme.js
@@ -5,7 +5,7 @@ let handler = async (m, {command, conn}) => {
    if (!res.ok) throw await res.text();
    let json = await res.json();
    if (!json.image) throw 'Err!';
-   conn.sendFile(m.chat, res, 'meme.png', json.caption, m) 
+   conn.sendFile(m.chat, res, 'meme.png',  m) 
  
 };
 handler.help = ["meme"];
