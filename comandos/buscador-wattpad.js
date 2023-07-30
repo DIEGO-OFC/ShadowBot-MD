9ec02aea7e39cfc0101b0c9c7fe911b76c30adc3
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-07-30 18:33:00 -0400

    Update buscador-wattpad.js

--- comandos/buscador-wattpad.js
+++ comandos/buscador-wattpad.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text }) => {
   let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&apikey=uwgflzFEh6`)
   let anu = await res.json()
   anu = anu.data.map((v) => `*Title:* ${v.title}\n*Reads:* ${v.reads}\n*Vote:* ${v.vote}\n*Chapter:* ${v.chapter}\n*Link:* ${v.link}\nDescription:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
-  conn.sendFile(m.chat, anu.data[0].thumb, '', anu, m)
+  conn.sendFile(m.chat, anu[0].thumb, '', anu, m)
 }
 handler.help = ['wattpad']
 handler.tags = ['internet']
