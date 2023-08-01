0f08977cedc7fc816181a3bd14fa5c0ef5e69699
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 17:16:40 -0400

    Actualizar herramientas-ssweb.js

--- comandos/herramientas-ssweb.js
+++ comandos/herramientas-ssweb.js
@@ -1,11 +1,9 @@
-import axios from 'axios'
-import fetch from 'node-fetch'
-let handler = async (m, {conn,text, args}) => {
-  if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m);
-    const r = await fetch(`https://api.lolhuman.xyz/api/SSWeb?apikey=GataDios&url=${text}`)
-  conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m});
-};
-handler.help = ["ss", "ssf"].map((v) => v + " <url>");
-handler.tags = ["internet"];
-handler.command = /^ss(web)?f?$/i;
-export default handler;
+let handler = async (m, {conn,text, args}) => { 
+   if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m); 
+     const r =  `https://api.lolhuman.xyz/api/SSWeb?apikey=GataDios&url=${text}`
+   conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); 
+ }; 
+ handler.help = ["ss", "ssf"].map((v) => v + " <url>"); 
+ handler.tags = ["internet"]; 
+ handler.command = /^ss(web)?f?$/i; 
+ export default handler;
