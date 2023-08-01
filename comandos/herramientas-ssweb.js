9ace40b867ecc34e7a892931de6bddaa005f2bd4
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 17:28:26 -0400

    Actualizar herramientas-ssweb.js

--- comandos/herramientas-ssweb.js
+++ comandos/herramientas-ssweb.js
@@ -1,6 +1,7 @@
 var handler = async (m, {conn,text, args}) => { 
-try {
+
    if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m); 
+try {
      const r =  `https://api.lolhuman.xyz/api/SSWeb?apikey=GataDios&url=${text}`
    conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); 
 } catch {
