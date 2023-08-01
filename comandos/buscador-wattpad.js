b94e369519886144f3391b8f14d794827ab7c771
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-01 12:15:49 -0500

    Update buscador-wattpad.js

--- comandos/buscador-wattpad.js
+++ comandos/buscador-wattpad.js
@@ -1,18 +1,20 @@
+// ADAPTADO POR @gata_dios
+
 import fetch from 'node-fetch'
 import translate from '@vitalets/google-translate-api'
 
-let handler = async(m, { conn, text }) => {
-  if (!text) throw `Mau Cari Apa?`
-  let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&lc=es&apikey=uwgflzFEh6`)
-  let anu = await res.json()
-  anu = anu.data.map((v) => `*Title:* ${v.title}\n*Reads:* ${v.reads}\n*Vote:* ${v.vote}\n*Chapter:* ${v.chapter}\n*Link:* ${v.link}\nDescription:* ${v.desc}`).join`\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n`
- // let akuariapiresult2 = await translate(`${anu}`, { to: 'es', autoCorrect: true })
-  
-  conn.sendFile(m.chat, anu[0].thumb, 'dorrat.jpg',  anu, m)
+let handler = async (m, { conn, text }) => {
+if (!text) throw `*Falta texto*`
+let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&lc=es&apikey=uwgflzFEh6`)
+let anu = await res.json()
+let translatedResults = await Promise.all(anu.data.map(async (v) => {
+let titulo = await translate(v.title, { to: 'es', autoCorrect: true })
+let descs = await translate(v.desc, { to: 'es', autoCorrect: true })
+return `*Título:* ${titulo.text}\n*Leídos:* ${v.reads}\n*Votos:* ${v.vote}\n*Capítulos:* ${v.chapter}\n*Enlace:* ${v.link}\n*Descripción:* ${descs.text}`
+}))
+let resultText = translatedResults.join('\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n')
+await m.reply(resultText)   
 }
-handler.help = ['wattpad']
-handler.tags = ['internet']
 handler.command = /^(wattpad)$/i
-handler.limit = true
 
 export default handler
