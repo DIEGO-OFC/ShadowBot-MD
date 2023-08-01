898085f4609f3d5c7285683734391ac6b2c9db71
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 17:02:55 -0400

    Actualizar herramientas-ssweb.js

--- comandos/herramientas-ssweb.js
+++ comandos/herramientas-ssweb.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let handler = async (m, {conn,text, args}) => {
   if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m);
     const r = await axios.get(`https://api.lolhuman.xyz/api/SSWeb?apikey=GataDios&url=${text}`)
-  conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m});
 };
 handler.help = ["ss", "ssf"].map((v) => v + " <url>");
 handler.tags = ["internet"];
