1a150866ea0af6f2cb5c02be1a070af410efa428
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 02:14:09 -0400

    Actualizar _PruebaZ.js

--- comandos/_PruebaZ.js
+++ comandos/_PruebaZ.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
-const handler = async (m, {conn, args, command, usedPrefix}) => {
+var handler = async (m, {conn, args, command, usedPrefix}) => {
   let r = await axios.get("https://api.lolhuman.xyz/api/pornhub?apikey=GataDios&url=${text}")
   let ress = r.data.result
   let img = r.data.result.thumb;
