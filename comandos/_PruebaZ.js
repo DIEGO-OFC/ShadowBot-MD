446eca5da1155c054b8cb6d1ce7ac800428adc13
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 01:50:51 -0400

    Update _PruebaZ.js

--- comandos/_PruebaZ.js
+++ comandos/_PruebaZ.js
@@ -1,3 +1,4 @@
+
 import fetch from 'node-fetch';
 const handler = async (m, {conn, args, command, usedPrefix}) => {
   if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
