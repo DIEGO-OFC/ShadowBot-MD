596bcf015ee7cd06ff2fd4efd75aa067bf740468
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 17:27:53 -0400

    Actualizar herramientas-ssweb.js

--- comandos/herramientas-ssweb.js
+++ comandos/herramientas-ssweb.js
@@ -1,8 +1,12 @@
 var handler = async (m, {conn,text, args}) => { 
+try {
    if (!args[0]) return conn.reply(m.chat, "*[🔎] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙻𝙰 𝚄𝚁𝙻 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙰𝙶𝙸𝙽𝙰*", m); 
      const r =  `https://api.lolhuman.xyz/api/SSWeb?apikey=GataDios&url=${text}`
    conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); 
- }; 
+} catch {
+let link = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`;
+conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m}); 
+}};
  handler.help = ["ss", "ssf"].map((v) => v + " <url>"); 
  handler.tags = ["internet"]; 
  handler.command = /^ss(web)?f?$/i; 
