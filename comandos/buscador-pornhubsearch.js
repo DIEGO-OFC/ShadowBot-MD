cfb530ac07d02d9d7867f0008a8833efbfaec9b1
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 01:22:48 -0400

    Update buscador-pornhubsearch.js

--- comandos/buscador-pornhubsearch.js
+++ comandos/buscador-pornhubsearch.js
@@ -1,29 +1,114 @@
 
-import fetch from "node-fetch"; 
- let handler = async (m, {text, usedPrefix, command}) => { 
-   if (!text) throw `uhm.. mau cari apa?\n\nContoh: ${usedPrefix + command} bokep🐒`; 
- if (text.includes(`${antiyaoi}`)) return m.reply('${sinyaoi}') 
-   try { 
-     let res = await fetch(`https://api.lolhuman.xyz/api/pornhubSearch?apikey=${lolkeysapi}&query=${text}`); 
-     let json = res.result:
-     let cap = `Hasil Pencarian Dari ${text}\n`; 
-     for (let v of json) { 
-       cap += `• *Title :* ${v.title} 
- • *Info :* ${v.info} 
- • *Link :* ${v.link} 
- `; 
-       cap += "\n" + "••••••••••••••••••••••••" + "\n"; 
-     } 
-     m.reply(cap); 
-   } catch (e) { 
-     console.error(e); 
-     throw e; 
-   } 
- }; 
- handler.help = ["pornhubsearch"].map((v) => v + " <query>"); 
- handler.tags = ["downloader", "premium"]; 
- handler.command = /^pornhubsearch|pornhub$/i; 
-  
- handler.register = false; 
- handler.premium = true;
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    text,
+    command
+}) => {
+
+    let lister = [
+        "search",
+        "gif"
+    ]
+
+    let [feature, inputs, inputs_, inputs__, inputs___] = text.split("|")
+    if (!lister.includes(feature)) return m.reply("*Example:*\n.pornhub search|vpn\n\n*Pilih type yg ada*\n" + lister.map((v, index) => "  ○ " + v).join("\n"))
+    
+    if (lister.includes(feature)) {
+    
+        if (feature == "search") {
+        if (!inputs) return m.reply("Input query")
+            try {
+                let res = await searchVideo(inputs)
+                let teks = res.map((item, index) => {
+                    return `*[ RESULT ${index + 1} ]*
+*Link:* ${item.link}
+*Title:* ${item.title}
+*Uploader:* ${item.uploader}
+*Views:* ${item.views}
+*Duration:* ${item.duration}
+`
+                }).filter(v => v).join("\n\n________________________\n\n")
+                await m.reply(teks)
+            } catch (e) {
+                await m.reply(eror)
+            }
+        }
+        if (feature == "gif") {
+        if (!inputs) return m.reply("Input query")
+            try {
+                let res = await searchGif(inputs)
+                let teks = res.map((item, index) => {
+                    return `*[ RESULT ${index + 1} ]*
+*Title:* ${item.title}
+*Url:* ${item.url}
+*Webm:* ${item.webm}
+`
+                }).filter(v => v).join("\n\n________________________\n\n")
+                await m.reply(teks)
+            } catch (e) {
+                await m.reply(eror)
+            }
+        }
+        
+    }
+}
+handler.help = ["pornhub"]
+handler.tags = ["internet"]
+handler.command = /^(pornhub)$/i
 export default handler
+
+/* New Line */
+async function searchVideo(query) {
+  const url = `https://www.pornhub.com/video/search?search=${query}`;
+  const response = await fetch(url);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  
+  const videoList = [];
+
+  $('li[data-video-segment]').each((index, element) => {
+    const $element = $(element);
+    
+    const link = $element.find('.title a').attr('href').trim();
+    const title = $element.find('.title a').text().trim();
+    const uploader = $element.find('.videoUploaderBlock a').text().trim();
+    const views = $element.find('.views').text().trim();
+    const duration = $element.find('.duration').text().trim();
+    
+    const videoData = {
+      link: "https://www.pornhub.com" + link,
+      title: title,
+      uploader: uploader,
+      views: views,
+      duration: duration
+    };
+    
+    videoList.push(videoData);
+  });
+  
+  return videoList;
+}
+
+async function searchGif(query) {
+  const url = `http://www.pornhub.com/gifs/search?search=${query}`;
+  const response = await fetch(url);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  
+  const gifs = $('ul.gifs.gifLink li');
+
+    return gifs.map((i, gif) => {
+      const data = $(gif).find('a');
+
+      return {
+        title: data.find('span').text(),
+        url: 'http://dl.phncdn.com#id#.gif'.replace('#id#', data.attr('href')),
+        webm: data.find('video').attr('data-webm'),
+      };
+    }).get();
+}
