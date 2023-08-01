ef8aa277e9ba3d07e93f5d39b1265b2db64a4894
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 17:34:15 -0400

    Actualizar herramientas-ssweb.js

--- comandos/herramientas-ssweb.js
+++ comandos/herramientas-ssweb.js
@@ -6,7 +6,7 @@ try {
    conn.sendMessage(m.chat, {image: {url: r}}, {quoted: m}); 
 } catch {
 var link = `https://api.screenshotmachine.com/?key=c04d3a&url=${args[0]}&screenshotmachine.com&dimension=720x720`;
-conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m})}; 
+conn.sendMessage(m.chat, {image: {url: link}}, {quoted: m});
 } catch {        
 var ssweb2 =`https://api.lolhuman.xyz/api/SSWeb2?apikey=${lolkeysapi)&url=${text}`
 conn.sendMessage(m.chat, {image: {url: ssweb2}}, {quoted: m}); 
