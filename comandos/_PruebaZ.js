2b41ac82fd8538ddcbb3c55512686918f2ee89f3
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-02 02:23:18 -0400

    Actualizar _PruebaZ.js

--- comandos/_PruebaZ.js
+++ comandos/_PruebaZ.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
-
+import axios from 'axios'
 let handler = async (m, { conn, usedPrefix, args, command, text }) => {
 if (!text) throw `Linknya Mana?\nExample: *.twitterdl https://www.xnxx.com/video-141ewlbb/free_use_anytime_sex_big_ass_latina_milf_step_mom_after_deal_with_step_son*`
 m.reply(wait)
-  let res = await fetch(`https://api.xyroinee.xyz/api/downloader/pornhub?url=${text}&apikey=uwgflzFEh6`)
+  let res = await axios.get(`https://api.xyroinee.xyz/api/downloader/pornhub?url=${text}&apikey=uwgflzFEh6`)
   let json = await res.json()
   conn.sendMessage(m.chat, { video: { url: json }, caption: `xdddd` }, { quoted: m })
   }
