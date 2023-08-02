f9353b4fa480af5281101dd23218a19a9611f6ce
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-02 02:29:49 -0400

    Update _PruebaZ.js

--- comandos/_PruebaZ.js
+++ comandos/_PruebaZ.js
@@ -4,15 +4,13 @@ import axios from 'axios'
 var handler = async (m, {conn, args, text, command, usedPrefix}) => {
     const r = await axios.get(`https://api.xyroinee.xyz/api/downloader/pornhub?url=${text}&apikey=uwgflzFEh6`)
   const ress = r.data.result
-  const img = r.data.result.thumb;
   const a = `*Titulo :* ${ress.title}
 *Duracion :* ${ress.duration}
 *Vistas :* ${ress.views}
 *Publicado :* ${ress.upload}
 *Likes :* ${ress.like}
 *Dislikes :* ${ress.dislike}`;
-  conn.sendMessage(m.chat, {image: {url: img}, caption: a}, {quoted: m})
-   let quality = ress.media.find(item => item.quality === 480);
+     let quality = ress.media.find(item => item.quality === 480);
    if (quality.quality == 480) {
     conn.sendMessage(m.chat, {document: {url: quality.url}, fileName: ress.title + '.mp4', mimetype: 'video/mp4'}, {quoted: m})
   };
