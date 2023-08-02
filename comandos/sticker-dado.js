06659aa5673abf3800345445ec2cf0885a3af4cb
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-01 21:00:05 -0400

    Actualizar sticker-dado.js

--- comandos/sticker-dado.js
+++ comandos/sticker-dado.js
@@ -1,16 +1,5 @@
-const dir = [
-  "https://tinyurl.com/dados01",
-  "https://tinyurl.com/dados02",
-  "https://tinyurl.com/dados03",
-  "https://tinyurl.com/dados04",
-  "https://tinyurl.com/dados05",
-  "https://tinyurl.com/dados06",
-];
-let handler = async (m, {conn}) => {
-  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], "dado.webp", "", m);
-};
-handler.help = ["dado"];
-handler.tags = ["game"];
-handler.command = ["dado", "dados"];
-
-export default handler;
+let handler = async (m, { conn }) => {
+let dir = 'https://api.lolhuman.xyz/api/sticker/dadu?apikey=GataDios'
+conn.sendFile(m.chat, dir, 'error.webp', null, m, { asSticker: true })}
+handler.command = ['dado', 'dados', 'dadu'] 
+export default handler
