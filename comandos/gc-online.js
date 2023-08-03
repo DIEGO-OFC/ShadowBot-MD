4089bbb25bc0f1086e3bd2c1c6fab33750d67d9f
Zastinian <zastinian@gmail.com>
2023-08-02 23:06:34 -0600

    fix: gc-online

--- comandos/gc-online.js
+++ comandos/gc-online.js
@@ -1,32 +1,34 @@
-handler = async (m, { conn }) => {
-  if (!m.quoted) m.reply('*Reply message*')
-                let msg = await conn.serializeM(await m.getQuotedObj())
-                if (!m.quoted.isBaileys) throw '*The message was not sent by a bot!*'
-                let teks = ''
-                for (let i of msg.userReceipt) {
-                    let read = i.readTimestamp
-                    let unread = i.receiptTimestamp
-                    let waktu = read ? read : unread
-                    teks += `> @${i.userJid.split('@')[0]}\n`
-                    teks += ` ┗━> *TIME :* ${moment(waktu * 1000).format('DD/MM/YY HH:mm:ss')} > *STATUS :* ${read ? 'READ' : 'SENT'}\n\n`
-                }
-                await conn.reply(m.chat, teks, m)
-}
-handler.help = ['sider']
-handler.tags = ['group']
-handler.command = /^(getsider|nyimak|sider)$/i
+handler = async (m, {conn}) => {
+  if (!m.quoted) m.reply("*Reply message*");
+  let msg = await conn.serializeM(await m.getQuotedObj());
+  if (!m.quoted.isBaileys) throw "*The message was not sent by a bot!*";
+  let teks = "";
+  for (let i of msg.userReceipt) {
+    let read = i.readTimestamp;
+    let unread = i.receiptTimestamp;
+    let waktu = read ? read : unread;
+    teks += `> @${i.userJid.split("@")[0]}\n`;
+    teks += ` ┗━> *TIME :* ${formatDate(waktu * 1000)} > *STATUS :* ${read ? "READ" : "SENT"}\n\n`;
+  }
+  await conn.reply(m.chat, teks, m);
+};
 
-export default handler
+handler.help = ["sider"];
+handler.tags = ["group"];
+handler.command = /^(getsider|nyimak|sider)$/i;
 
-function formatDate(n, locale = 'id') {
-  let d = new Date(n)
-  return d.toLocaleDateString(locale, {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-  })
+function formatDate(timestamp, locale = "id") {
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+
+  return new Date(timestamp).toLocaleDateString(locale, options);
 }
+
+export default handler;
