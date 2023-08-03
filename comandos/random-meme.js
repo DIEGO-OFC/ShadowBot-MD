f1f7b38225d5e7fce62dcffde5d0fe08f97dc241
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-03 01:28:22 -0400

    Actualizar random-meme.js

--- comandos/random-meme.js
+++ comandos/random-meme.js
@@ -2,7 +2,7 @@ import hispamemes from "hispamemes";
 import fetch from "node-fetch"
 let handler = async (m, {command, conn}) => {
     
-    let res = await fetch("https://api.humorapi.com/memes/random?api-key=c1be3de018d14210981b291d6e2b3dde`)
+    let res = await fetch(`https://api.humorapi.com/memes/random?api-key=c1be3de018d14210981b291d6e2b3dde`)
    if (!res.ok) throw await res.text();
    let json = await res.json();
    if (!json.image) throw 'Err!';
