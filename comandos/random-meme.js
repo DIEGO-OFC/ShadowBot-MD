11a1a7b4a9d869040a1aa73152dfb18f8bc595b1
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-03 01:26:27 -0400

    Actualizar random-meme.js

--- comandos/random-meme.js
+++ comandos/random-meme.js
@@ -1,7 +1,7 @@
 import hispamemes from "hispamemes";
 import fetch from "node-fetch"
 let handler = async (m, {command, conn}) => {
-    let res = await fetch(`https://some-random-api.com/meme`);
+    let res = (`https://some-random-api.com/meme`);
    if (!res.ok) throw await res.text();
    let json = await res.json();
    if (!json.image) throw 'Err!';
