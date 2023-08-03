a16a3309e7c9c9c8075c3a580ad4f4dc69409db2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-03 17:21:18 -0400

    Actualizar buscador-lyrics.js

--- comandos/buscador-lyrics.js
+++ comandos/buscador-lyrics.js
@@ -1,38 +1,33 @@
-import fetch from "node-fetch";
-import {lyrics, lyricsv2} from "@bochilteam/scraper";
-let handler = async (m, {conn, text, usedPrefix, command}) => {
-  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : "";
-  if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`;
-  try {
-    const result = await lyricsv2(teks).catch(async (_) => await lyrics(teks));
-    let res = await fetch(
-      global.API("https://some-random-api.ml", "/lyrics", {
-        title: result.author + result.title,
-      })
-    );
-    if (!res.ok) throw await res.text();
-    let json = await res.json();
-    if (!json.thumbnail.genius) throw json;
-    let letratexto = `𝚃𝙸𝚃𝚄𝙻𝙾: *${result.title}*\n𝙰𝚄𝚃𝙾𝚁: *${result.author}*\n\n𝙻𝙴𝚃𝚁𝙰: ${result.lyrics}`.trim();
-    let linkresult = monospace + result.link + monospace;
-    conn.sendButton(
-      m.chat,
-      letratexto,
-      `\n𝚄𝚁𝙻: ${linkresult}\n${wm}`,
-      json.thumbnail.genius,
-      [
-        ["🎵 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝙰𝚄𝙳𝙸𝙾 🎵", `#play.1 ${text}`],
-        ["🎥 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝚁 𝚅𝙸𝙳𝙴𝙾 🎥", `#play.2 ${text}`],
-      ],
-      m
-    );
-  } catch {
-    await m.reply("*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*");
-  }
-};
-handler.help = ["lirik", "letra"].map((v) => v + " <Apa>");
-handler.tags = ["internet"];
-handler.command = /^(lirik|lyrics|lyric|letra)$/i;
-export default handler;
-let mono = "`" + "`" + "`";
-global.monospace = mono;
+import {find_lyrics} from '@brandond/findthelyrics'; 
+ import {getTracks} from '@green-code/music-track-data'; 
+ import {googleImage} from '@bochilteam/scraper'; 
+ const handler = async (m, {conn, text, usedPrefix, command}) => { 
+   const teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''; 
+   if (!teks) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙹𝙴𝙼𝙿𝙻𝙾 𝙳𝙴 𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 𝙳𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾: ${usedPrefix + command} beret ojala*`; 
+   try { 
+     const result = await getTracks(teks); 
+     const lyrics = await find_lyrics(`${result[0].artist} ${result[0].title}`); 
+     const res = await fetch(global.API('https://some-random-api.com', '/lyrics', {title: result[0].artist + result[0].title})); 
+     const json = await res.json(); 
+     let img; 
+     try { 
+       img = result.album.artwork; 
+     } catch { 
+       try { 
+         img = json.thumbnail.genius; 
+       } catch { 
+         const bochil = await googleImage(`${result[0].artist} ${result[0].title}`); 
+         img = await bochil.getRandom(); 
+       } 
+     } 
+     const textoLetra = `🔍 TITULO: *${result[0].title || ''}*\n👤 AUTHOR: *${result[0].artist || ''}*\n\n📃🎵 LETRA:\n${lyrics || ''}`; 
+     await conn.sendMessage(m.chat, {image: {url: img}, caption: textoLetra}, {quoted: m}); 
+     await conn.sendMessage(m.chat, {audio: {url: result[0].preview}, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m}); 
+   } catch { 
+     throw `*[❌] Ocurrio un error, inténtalo de nuevo*`; 
+   } 
+ }; 
+ handler.help = ['lirik', 'letra'].map((v) => v + ' <Apa>'); 
+ handler.tags = ['internet']; 
+ handler.command = /^(lirik|lyrics|lyric|letra)$/i; 
+ export default handler;
