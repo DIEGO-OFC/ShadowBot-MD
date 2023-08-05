7adc92289dc8064a3480b1ed34facf5487fc0b8d
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2023-08-04 23:21:10 -0500

    Update downloader-tiktok.js

--- comandos/downloader-tiktok.js
+++ comandos/downloader-tiktok.js
@@ -2,7 +2,7 @@ import fg from 'api-dylux';
  import axios from 'axios';
  import cheerio from 'cheerio';
  import { tiktok } from "@xct007/frieren-scraper"; 
- import { generateWAMessageFromContent } from '@adiwajshing/baileys';
+ import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
  import { tiktokdl } from '@bochilteam/scraper';
  let handler = async (m, { conn, text, args, usedPrefix, command}) => { 
  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙽 𝙴𝙽𝙻𝙰𝙲𝙴/𝙻𝙸𝙽𝙺 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command}* https://vm.tiktok.com/ZMYWFhKt2/` 
