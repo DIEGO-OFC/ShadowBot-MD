45568b5a607c4e220cacec23ffea8181212db574
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-06 02:52:38 -0400

    Actualizar frase-frases.js

--- comandos/frase-frases.js
+++ comandos/frase-frases.js
@@ -125,5 +125,104 @@ const frasesRomanticas = [
   "Me haces sentir como si pudiera amar.",
   "Me haces sentir como si pudiera ser amado.",
   "Me haces sentir como si pudiera ser feliz.",
-  "Me haces sentir como si pudiera amar."]
+  "Me haces sentir como si pudiera amar.",
+    "El amor es una fortaleza que me hace mejor cada día",
+
+  "Cuando te veo, el mundo parece dejar de girar",
+
+  "Eres la luz del sol en mi vida oscura",
+
+  "Me enamoro de ti cada vez que estamos juntos",
+
+  "Eres mi sueño hecho realidad",
+
+  "Mi amor por ti nunca se desvanecerá, incluso a medida que pase el tiempo",
+
+  "Eres mi aliento, sin ti mi vida no será perfecta",
+
+  "En tus brazos, me siento seguro y protegido",
+
+  "Cada segundo contigo es un milagro inolvidable",
+
+   "Eres la razón por la que sonrío todos los días",
+
+  "Mi amor por ti nunca terminará, incluso cuando estamos lejos",
+
+ "Eres una estrella que ilumina mi vida",
+
+  "En tus ojos, encuentro una belleza irremplazable",
+
+  "Eres un milagro que hace que mi vida sea más significativa",
+
+  "Mi amor por ti nunca se desvanecerá, porque eres todo para mí",
+
+   "Eres un ángel enviado por Dios a mí",
+
+  "En tus brazos, encontré una paz invaluable",
+
+  "Eres un sueño que se hace realidad en mi vida",
+
+  "Todos los días contigo es una aventura inolvidable",
+
+  "Mi amor por ti nunca cambiará, porque tú eres la luz de mi vida",
+
+  "Tú eres el poder que me hace sobrevivir en cada tormenta",
+
+  "En tu sonrisa, encuentro la felicidad infinita",
+
+  "Eres el regalo más hermoso que he recibido",
+
+  "Mi amor por ti nunca se desvanecerá, porque eres un verdadero amor para mí",
+
+  "Eres la fuente de inspiración en mi vida",
+
+  "En tus brazos, me siento completo y feliz",
+
+ "Eres una música hermosa en mi vida",
+
+ "Mi amor por ti nunca terminará, porque eres todo para mí",
+
+ "Eres un ángel que me protege y me protege",
+
+  "En tus ojos, encontré un milagro irremplazable",
+
+  "Eres la luz que ilumina cada paso de mi",
+
+  "En tu sonrisa, encontré una calidez insustituible",
+
+  "Eres un sueño que se hace realidad en mi vida",
+
+  "Mi amor por ti nunca se desvanecerá, porque eres todo para mí",
+
+  "Eres un poder que me mantiene fuerte en cada prueba",
+
+   "En tus brazos, encontré la calma y la felicidad",
+
+ "Eres el regalo más hermoso que he recibido",
+
+  "Mi amor por ti nunca cambiará, porque eres un verdadero amor para mí",
+
+  "Tú eres la fuente de la felicidad en mi vida",
+
+  "En tus brazos, me siento seguro y protegido",
+
+ "Eres una estrella que ilumina mi vida",
+
+  "En tu sonrisa, encontré una belleza irremplazable",
+
+   "Eres un sueño que se hace realidad en mi vida",
+
+  "Mi amor por ti nunca se desvanecerá, porque eres todo para mí",
+
+  "Eres un poder que me mantiene entusiasmado en cada desafío",
+
+  "En tus brazos, encontré paz y felicidad",
+
+   "Eres el regalo más hermoso que he recibido",
+
+   "Mi amor por ti nunca terminará, porque eres un verdadero amor para mí",
+
+  "Tú eres la fuente de la felicidad en mi vida",
+
+   "En tus ojos, encontré un milagro irremplazable"]
 
