bf4db1c447fd6b2d2c2bb03f74153b798a9723af
Zastinian <zastinian@gmail.com>
2023-08-07 17:21:33 -0600

    feat: simi

--- comandos/_simi.js
+++ comandos/_simi.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 var handler = (m) => m;
 
 handler.before = async (m) => {
@@ -9,19 +9,20 @@ handler.before = async (m) => {
     try {
       const ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`);
       const data = await ressimi.json();
-      if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`); /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
+      if (data.success == "No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.")
+        return m.reply(`${lol}`); /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
       await m.reply(data.success);
     } catch {
       /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
-      if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
-      if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello');
-      if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO');
-      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + textodem);
+      if (textodem.includes("Hola")) textodem = textodem.replace("Hola", "Hello");
+      if (textodem.includes("hola")) textodem = textodem.replace("hola", "hello");
+      if (textodem.includes("HOLA")) textodem = textodem.replace("HOLA", "HELLO");
+      const reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + textodem);
       const resu = await reis.json();
-      const nama = m.pushName || '1';
-      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+      const nama = m.pushName || "1";
+      const api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0]);
       const res = await api.json();
-      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+      const reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt);
       const resu2 = await reis2.json();
       await m.reply(resu2[0][0][0]);
     }
