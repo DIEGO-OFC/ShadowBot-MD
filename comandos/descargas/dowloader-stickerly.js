2a682425f350843513936b03fb26b25a710e65ed
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-07 15:09:50 -0400

    Actualizar dowloader-stickerly.js

--- comandos/descargas/dowloader-stickerly.js
+++ comandos/descargas/dowloader-stickerly.js
@@ -3,25 +3,17 @@ import fetch from "node-fetch";
 let handler = async (m, {conn, args, usedPrefix, command}) => {
   if (!args[0]) throw `*✳️ EJEMPLO DE USO DEL COMANDO ${usedPrefix + command} Anya Forger*`;
   try {
-    let res = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${args[0]}&APIKEY=HIRO`);
-    let json = await res.json();
-    for (let data of json.result.stickerlist || json) {
-      const stikers = await sticker(false, data, global.packname, global.author);
-      conn.sendFile(m.chat, stikers, "sticker.webp", "", m, {asSticker: true});
-    }
-    //await delay(1500)
-  } catch {
-    try {
-      let res2 = await fetch(`https://api.xteam.xyz/sticker/stickerly?q=${args[0]}&APIKEY=${keysxteam}`);
-      let json2 = await res2.json();
-      for (let data2 of json2.result.stickerlist || json2) {
-        const stikers2 = await sticker(false, data2, global.packname, global.author);
-        conn.sendFile(m.chat, stikers2, "sticker.webp", "", m, {asSticker: true});
-      }
-      //await delay(1500)
+       const res2 = await googleImage(text); 
+     const sfoto = res2.getRandom(); 
+     const json = await fetch(`https://api.lolhuman.xyz/api/stickerwa?apikey=${lolkeysapi}&query=${text}`); 
+     const jsons = await json.json(); 
+     const {stickers} = jsons.result[0]; 
+     const res = jsons.result.map((v, index) => `🌅 • Resultado: ${1 + index}\n*🥗 • Nombre:* ${v.title}\n*🐢 • Autor:* ${v.author}\n*🍂 • Url:* ${v.url}`).join`\n\n───\n\n`; 
+     await conn.sendFile(m.chat, sfoto, 'error.jpg', res, m); 
+      
     } catch {
       await m.reply("*⚠️ ERROR INTENTALO DE NUEVO*");
-    }
+   
   }
 };
 handler.command = /^stickerly$/i;
