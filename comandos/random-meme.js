bbe492e63679dfe610e9c24119bdf8a4a3e5d534
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-07 15:42:15 -0400

    Update random-meme.js

--- comandos/random-meme.js
+++ comandos/random-meme.js
@@ -2,8 +2,8 @@ import meme from "../lib/memes.js";
 import {eMeme, sMeme, all} = from "discord.js-memes";
 
 var handler = async (m, {conn}) => {
-const all = new sMeme();
-  await conn.sendFile(m.chat, all, "", "", m);
+const allmeme = new sMeme();
+  await conn.sendFile(m.chat, allmeme, "", "", m);
 };
 handler.help = ["memes"];
 handler.tags = ["internet"];
