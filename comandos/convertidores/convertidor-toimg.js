2a68cf88417dd29abf455f324963da8922dcbfbc
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-07 07:56:53 -0400

    Update convertidor-toimg.js

--- comandos/convertidores/convertidor-toimg.js
+++ comandos/convertidores/convertidor-toimg.js
@@ -1,4 +1,4 @@
-/*import {webp2png} from "../lib/webp2mp4.js";*/
+//import {webp2png} from "../lib/webp2mp4.js";
 let handler = async (m, {conn, usedPrefix, command}) => {
   const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
   if (!m.quoted) throw notStickerMessage;
