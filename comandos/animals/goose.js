1fe5fbc06a44eecde9eec93058f6e469268775a9
Zastinian <zastinian@gmail.com>
2023-08-07 18:43:05 -0600

    feat: goose

---
+++ comandos/animals/goose.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+let handler = async (m, {conn}) => {
+  try {
+    let res = await fetch("https://nekos.life/api/v2/img/goose");
+    let json = await res.json();
+    let img = await json.url;
+    let caption = `
+_©The DorrAt - Bot_
+`.trim();
+    conn.sendFile(m.chat, img, "goose.jpg", caption, m);
+  } catch (e) {
+    console.log(e);
+    throw "*Error!*";
+  }
+};
+handler.help = ["goose"];
+handler.tags = ["animals"];
+handler.command = ["goose", "ganso"];
+handler.fail = null;
+export default handler;
