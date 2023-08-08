c7955ef5c19aa90423ebf9edd42bc1ad55f80d7c
Zastinian <zastinian@gmail.com>
2023-08-07 18:44:39 -0600

    feat: Random

--- comandos/random/lizard.js
+++ comandos/random/lizard.js
@@ -1,20 +1,20 @@
 import fetch from "node-fetch";
 let handler = async (m, {conn}) => {
   try {
-    let res = await fetch("https://nekos.life/api/v2/img/woof");
+    let res = await fetch("https://nekos.life/api/v2/img/lizard");
     let json = await res.json();
     let img = await json.url;
     let caption = `
 _©The DorrAt - Bot_
 `.trim();
-    conn.sendFile(m.chat, img, "dog.jpg", caption, m);
+    conn.sendFile(m.chat, img, "lizard.jpg", caption, m);
   } catch (e) {
     console.log(e);
     throw "*Error!*";
   }
 };
-handler.help = ["dog"];
-handler.tags = ["animals"];
-handler.command = ["dog", "perro"];
+handler.help = ["lizard"];
+handler.tags = ["random"];
+handler.command = ["lizard", "lagarto"];
 handler.fail = null;
 export default handler;
