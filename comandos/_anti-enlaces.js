a5495e18831792de6604ce1f385407003a8bc31d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-09 13:31:22 -0400

    Actualizar _anti-enlaces.js

--- comandos/_anti-enlaces.js
+++ comandos/_anti-enlaces.js
@@ -1,27 +1,40 @@
 var linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
-export async function before(m, {isAdmin, isBotAdmin}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (!m.isGroup) return !1;
+
+export async function before(m, { isAdmin, isBotAdmin }) {
+  if (m.isBaileys && m.fromMe) return true;
+  if (!m.isGroup) return false;
+
   let chat = global.db.data.chats[m.chat];
   let delet = m.key.participant;
   let bang = m.key.id;
   const isGroupLink = linkRegex.exec(m.text);
   const grupo = `https://chat.whatsapp.com`;
-  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply("*𝙷𝙴𝚈!! 𝙴𝙻 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 𝙴𝚂𝚃𝙰 𝙰𝙲𝚃𝙸𝚅𝙾, 𝙿𝙴𝚁𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝙰𝙳𝙼𝙸𝙽 🤑, 𝚂𝙰𝙻𝚅𝙰𝙳𝙾/𝙰!*");
+
+  if (isAdmin && chat.antiLink && m.text.includes(grupo)) {
+    return m.reply("*¡Hey! El antilink está activo, pero eres un admin 🤑, salvado/a!*");
+  }
+
   if (chat.antiLink && isGroupLink && !isAdmin) {
     if (isBotAdmin) {
       const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
-      if (m.text.includes(linkThisGroup)) return !0;
+      if (m.text.includes(linkThisGroup)) return true;
     }
+
     conn.sendMessage(
       m.chat,
-      {text: `se detecto un enlace serás eliminado ${isBotAdmin ? "" : "\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*"}`},
-      {quoted: m}
+      {
+        text: `Se detectó un enlace, serás eliminado ${
+          isBotAdmin ? "" : "\n\n*[❗INFO❗] El bot no es admin, no puede eliminar a las personas*"
+        }`,
+        quoted: m,
+      },
     );
+
     if (isBotAdmin) {
-      await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
+      await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
       await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove");
     }
   }
-  return !0;
+
+  return true;
 }
