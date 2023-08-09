333a58ad98e193c595ccea41d02fa7b272d40cdf
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-09 02:38:20 -0400

    Actualizar rpg-dep.js

--- comandos/rpg-dep.js
+++ comandos/rpg-dep.js
@@ -1,30 +1,30 @@
-import db from '../lib/database.js'
+import db from '../lib/database.js';
 
 let handler = async (m, { args }) => {
    let who;
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender;
   else who = m.sender;
    
-   let users =  global.db.data.users[m.sender]
-   if (!args[0]) return m.reply('Ingresa la cantidad de dinero que deseas Depositar.')
+   let users =  global.db.data.users[m.sender];
+   if (!args[0]) return m.reply('Ingresa la cantidad de dinero que deseas Depositar.');
    if (args[0] == '--all') {
-      let count = parseInt(users.dolares)
+      let count = parseInt(users.dolares);
       users.dolares -= count * 1
       users.bank += count * 1
-      await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`)
-      return !0
-   }
-   if (!Number(args[0])) return m.reply('La cantidad deve ser un Numero.')
-   let count = parseInt(args[0])
-   if (!users.dolares) return m.reply('No tienes dolares en la Cartera.')
-   if (users.dolares < count) return m.reply(`Solo tienes ${users.dolares} dolares en la Cartera.`)
-   users.dolares -= count * 1
-   users.bank += count * 1
-   await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`)
-}
+      await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`);
+      return !0;
+   };
+   if (!Number(args[0])) return m.reply('La cantidad deve ser un Numero.');
+   let count = parseInt(args[0]);
+   if (!users.dolares) return m.reply('No tienes dolares en la Cartera.');
+   if (users.dolares < count) return m.reply(`Solo tienes ${users.dolares} dolares en la Cartera.`);
+   users.dolares -= count * 1;
+   users.bank += count * 1;
+   await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`);
+};
 
-handler.help = ['deposit']
-handler.tags = ['economy']
-handler.command = ['deposit', 'depositar', 'dep']
+handler.help = ['deposit'];
+handler.tags = ['economy'];
+handler.command = ['deposit', 'depositar', 'dep'];
 
 export default handler
