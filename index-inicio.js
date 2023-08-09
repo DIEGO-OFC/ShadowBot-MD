5df347e7b1bb60a016133e215a0c322c8863f417
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-09 13:16:46 -0400

    Actualizar index-inicio.js

--- index-inicio.js
+++ index-inicio.js
@@ -1,12 +1,13 @@
-console.log("☑️ㅤIniciando bot...");
-import {join, dirname} from "path";
-import {fileURLToPath} from "url";
-import {setupMaster, fork} from "cluster";
+
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { setupMaster, fork } from "cluster";
 import cfonts from "cfonts";
-import {createInterface} from "readline";
+import { createInterface } from "readline";
 import yargs from "yargs";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const {say} = cfonts;
+const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
 say("ZIRAX-BOT-MD", {
@@ -14,54 +15,66 @@ say("ZIRAX-BOT-MD", {
   align: "center",
   colors: ["#ff8000"],
 });
-say(`CREADO POR DIEGO-OFC`, {
+
+say("CREADO POR DIEGO-OFC", {
   font: "console",
   align: "center",
   colors: ["red"],
 });
 
-var isRunning = false
+var isRunning = false;
+
 /**
-* Start a js file
-* @param {String} file `path/to/file`
-*/
+ * Iniciar un archivo js
+ * @param {String} file `path/to/file`
+ */
 function start(file) {
-if (isRunning) return
-isRunning = true
-let args = [join(__dirname, file), ...process.argv.slice(2)]
-  
-setupMaster({
-exec: args[0],
-args: args.slice(1), })
-let p = fork()
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
-case 'uptime':
-p.send(process.uptime())
-break }})
-p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚠️ Error Inesperado ⚠️', code)
-  
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-  
-if (process.env.pm_id) {
-process.exit(1)
-} else {
-process.exit()
+  if (isRunning) return;
+  isRunning = true;
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1),
+  });
+
+  const p = fork();
+  p.on("message", (data) => {
+    switch (data) {
+      case "reset":
+        p.process.kill();
+        isRunning = false;
+        start(file);
+        break;
+      case "uptime":
+        p.send(process.uptime());
+        break;
+    }
+  });
+
+  p.on("exit", (_, code) => {
+    isRunning = false;
+    console.error("⚠️ Error Inesperado ⚠️", code);
+
+    p.process.kill();
+    isRunning = false;
+    start(file);
+
+    if (process.env.pm_id) {
+      process.exit(1);
+    } else {
+      process.exit();
+    }
+  });
+
+  const opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
+  if (!opts["test"]) {
+    if (!rl.listenerCount()) {
+      rl.on("line", (line) => {
+        p.emit("message", line.trim());
+      });
+    }
+  }
 }
-})
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())
-})}
-start('main.js')
-            
+
+start("main.js");
