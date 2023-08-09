b4d4dadcdb05f2a8ba778e964a4245b052b1caa5
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-09 02:34:22 -0400

    Update rpg-dep.js

--- comandos/rpg-dep.js
+++ comandos/rpg-dep.js
@@ -17,7 +17,7 @@ let handler = async (m, { args }) => {
    if (!Number(args[0])) return m.reply('La cantidad deve ser un Numero.')
    let count = parseInt(args[0])
    if (!users.dolares) return m.reply('No tienes dolares en la Cartera.')
-   if (users.dolares < count) return m.reply(`Solo tienes ${user.dolares} dolares en la Cartera.`)
+   if (users.dolares < count) return m.reply(`Solo tienes ${users.dolares} dolares en la Cartera.`)
    users.dolares -= count * 1
    users.bank += count * 1
    await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`)
