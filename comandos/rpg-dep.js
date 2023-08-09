c08576a251e3eda59f37bc9cbdc90bff0f26a82d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-09 02:42:14 -0400

    Actualizar rpg-dep.js

--- comandos/rpg-dep.js
+++ comandos/rpg-dep.js
@@ -6,21 +6,21 @@ let handler = async (m, { args }) => {
   else who = m.sender;
    
    let users =  global.db.data.users[m.sender];
-   if (!args[0]) return m.reply('Ingresa la cantidad de dinero que deseas Depositar.');
+   if (!args[0]) return m.reply('[❌] Ingresa la cantidad de dinero que deseas Depositar.');
    if (args[0] == '--all') {
       let count = parseInt(users.dolares);
       users.dolares -= count * 1
       users.bank += count * 1
-      await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`);
+      await m.reply(`*Depositaste ${count} de dinero al Banco.* 💸`);
       return !0;
    };
-   if (!Number(args[0])) return m.reply('La cantidad debe ser un Numero.');
+   if (!Number(args[0])) return m.reply('[⚠️] La cantidad debe ser un Numero.');
    let count = parseInt(args[0]);
-   if (!users.dolares) return m.reply('No tienes dolares en la Cartera.');
-   if (users.dolares < count) return m.reply(`Solo tienes ${users.dolares} dolares en la Cartera.`);
+   if (!users.dolares) return m.reply('[🥺] No tienes dolares en la Cartera.');
+   if (users.dolares < count) return m.reply(`[❌] Solo tienes ${users.dolares} dolares en la Cartera.`);
    users.dolares -= count * 1;
    users.bank += count * 1;
-   await m.reply(`*Depositaste ${count} de dinero al Banco.* 🍟`);
+   await m.reply(`*Depositaste ${count} de dinero al Banco.* 💸`);
 };
 
 handler.help = ['deposit'];
