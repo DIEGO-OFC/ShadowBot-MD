2cb218484407111363a0ebaefd900412c07d377c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-10 15:46:57 -0400

    Actualizar random-neko.js

--- comandos/random/random-neko.js
+++ comandos/random/random-neko.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import akaneko from "akaneko";
 let handler = async (m, {conn, command}) => {
     const Neko = await akaneko.neko(); 
-   await conn.sendFile(m.chat, Neko, "", caption: `*乂 ⺀ ANIME - RANDOM ⺀ 乂*`, m);
+   await conn.sendFile(m.chat, Neko, "", caption: `*乂 ⺀ ANIME - NEKO ⺀ 乂*`, m);
 }
 handler.command = /^(neko)$/i;
 handler.tags = ["anime"];
