c6936a997d133fbfae530ce0bfd2731845da4fca
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-10 17:39:27 -0400

    Actualizar adult-comandos+18.js

--- comandos/adult-comandos+18.js
+++ comandos/adult-comandos+18.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import akaneko from "akaneko";
-import UltimateNSFW from "ultimate-nsfw";
 import fetch from 'node-fetch';
 const handler = async (m, {command, conn}) => {
   if (!db.data.chats[m.chat].modohorny && m.isGroup) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*';
@@ -67,8 +66,7 @@ const handler = async (m, {command, conn}) => {
   }
 
   if (command == 'tetas') {
-  const xtimate = new UltimateNSFW();
-   const tetasgod = await xtimate.boobs();
+    const tetasgod = await axios.get(`https://nekos.life/api/v2/img/fox_girl`);
     const caption = `*乂 ⺀ NSFW - TETASGOD ⺀ 乂*` 
     await conn.sendFile(m.chat, tetasgod, "zirax.jpg", caption, m); 
  }
