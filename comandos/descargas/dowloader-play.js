31d6400f282823e2813b603e0eb410b36bfbaf69
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-10 14:54:40 -0400

    Actualizar dowloader-play.js

--- comandos/descargas/dowloader-play.js
+++ comandos/descargas/dowloader-play.js
@@ -102,104 +102,135 @@ handler.dolares = 4
 handler.command = ["musica", "música", "play2"]
 export default handler
 
-async function search(query, options = {}) {
- const search = await yts.search({ query, hl: "es", gl: "ES", ...options });
-return search.videos};
-
-  return search.videos.map((video) => ({
-    title: video.title,
-    url: video.url,
-    thumbnail: video.thumbnail,
-  }));
-};
-
-const MilesNumber = (number) => {
-  const exp = /(\d)(?=(\d{3})+(?!\d))/g;
-  const rep = "$1.";
-  return number.toString().replace(exp, rep);
-};
-
-
-function secondString(seconds) {
-seconds = Number(seconds);
-var d = Math.floor(seconds / (3600 * 24));
-var h = Math.floor((seconds % (3600 * 24)) / 3600);
-var m = Math.floor((seconds % 3600) / 60);
-var s = Math.floor(seconds % 60);
-var dDisplay = d > 0 ? d + (d == 1 ? " día, " : " días, ") : "";
-var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " horas, ") : "";
-var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " minutos, ") : "";
-var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " segundos") : "";
-return dDisplay + hDisplay + mDisplay + sDisplay};
-const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-
-const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-
-function bytesToSize(bytes) {
-  if (bytes === 0) {
-    return "n/a";
-  };
-
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  const decimals = i === 0 ? 0 : 1;
-
-  return `${(bytes / Math.pow(1024, i)).toFixed(decimals)} ${sizes[i]}`;
-};
-
-async function ytMp3(url) {
-return new Promise((resolve, reject) => {
-ytdl.getInfo(url).then(async(getUrl) => {
-let result = [];
-for(let i = 0; i < getUrl.formats.length; i++) {
-let item = getUrl.formats[i];
-if (item.mimeType == 'audio/webm; codecs=\"opus\"') {
-let { contentLength } = item;
-let bytes = await bytesToSize(contentLength);
-result[i] = { audio: item.url, size: bytes }}};
-let resultFix = result.filter(x => x.audio != undefined && x.size != undefined) 
-let tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].audio}`);
-let tinyUrl = tiny.data;
-let title = getUrl.videoDetails.title;
-let thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-resolve({ title, result: tinyUrl, result2: resultFix, thumb })}).catch(reject)})};
-
-async function ytMp4(url) {
-return new Promise(async(resolve, reject) => {
-ytdl.getInfo(url).then(async(getUrl) => {
-let result = [];
-for(let i = 0; i < getUrl.formats.length; i++) {
-let item = getUrl.formats[i];
-if (item.container == 'mp4' && item.hasVideo == true && item.hasAudio == true) {
-let { qualityLabel, contentLength } = item;
-let bytes = await bytesToSize(contentLength);
-result[i] = { video: item.url, quality: qualityLabel, size: bytes }}};
-let resultFix = result.filter(x => x.video != undefined && x.size != undefined && x.quality != undefined) 
-let tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].video}`);
-let tinyUrl = tiny.data;
-let title = getUrl.videoDetails.title;
-let thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-resolve({ title, result: tinyUrl, rersult2: resultFix[0].video, thumb })}).catch(reject)})};
-
-async function ytPlay(query) {
-return new Promise((resolve, reject) => {
-yts(query).then(async(getData) => {
-let result = getData.videos.slice( 0, 5 );
-let url = [];
-for (let i = 0; i < result.length; i++) { url.push(result[i].url) }
-let random = url[0];
-let getAudio = await ytMp3(random);
-resolve(getAudio)}).catch(reject)})};
-
-async function ytPlayVid(query) {
-return new Promise((resolve, reject) => {
-yts(query).then(async(getData) => {
-let result = getData.videos.slice( 0, 5 );
-let url = [];
-for (let i = 0; i < result.length; i++) { url.push(result[i].url) }
-let random = url[0];
-let getVideo = await ytMp4(random);
-resolve(getVideo)}).catch(reject)})};
-
+async function search(query, options = {}) { 
+   const search = await yts.search({query, hl: 'es', gl: 'ES', ...options}); 
+   return search.videos; 
+ } 
+  
+ function MilesNumber(number) { 
+   const exp = /(\d)(?=(\d{3})+(?!\d))/g; 
+   const rep = '$1.'; 
+   const arr = number.toString().split('.'); 
+   arr[0] = arr[0].replace(exp, rep); 
+   return arr[1] ? arr.join('.') : arr[0]; 
+ } 
+  
+ function secondString(seconds) { 
+   seconds = Number(seconds); 
+   const d = Math.floor(seconds / (3600 * 24)); 
+   const h = Math.floor((seconds % (3600 * 24)) / 3600); 
+   const m = Math.floor((seconds % 3600) / 60); 
+   const s = Math.floor(seconds % 60); 
+   const dDisplay = d > 0 ? d + (d == 1 ? ' día, ' : ' días, ') : ''; 
+   const hDisplay = h > 0 ? h + (h == 1 ? ' hora, ' : ' horas, ') : ''; 
+   const mDisplay = m > 0 ? m + (m == 1 ? ' minuto, ' : ' minutos, ') : ''; 
+   const sDisplay = s > 0 ? s + (s == 1 ? ' segundo' : ' segundos') : ''; 
+   return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
+  
+ function bytesToSize(bytes) { 
+   return new Promise((resolve, reject) => { 
+     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']; 
+     if (bytes === 0) return 'n/a'; 
+     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10); 
+     if (i === 0) resolve(`${bytes} ${sizes[i]}`); 
+     resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`); 
+   }); 
+ } 
+  
+ async function ytMp3(url) { 
+   return new Promise((resolve, reject) => { 
+     ytdl.getInfo(url).then(async (getUrl) => { 
+       const result = []; 
+       for (let i = 0; i < getUrl.formats.length; i++) { 
+         const item = getUrl.formats[i]; 
+         if (item.mimeType == 'audio/webm; codecs=\"opus\"') { 
+           const {contentLength} = item; 
+           const bytes = await bytesToSize(contentLength); 
+           result[i] = {audio: item.url, size: bytes}; 
+         } 
+       } 
+       const resultFix = result.filter((x) => x.audio != undefined && x.size != undefined); 
+       const tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].audio}`); 
+       const tinyUrl = tiny.data; 
+       const title = getUrl.videoDetails.title; 
+       const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url; 
+       resolve({title, result: tinyUrl, result2: resultFix, thumb}); 
+     }).catch(reject); 
+   }); 
+ } 
+  
+ async function ytMp4(url) { 
+   return new Promise(async (resolve, reject) => { 
+     ytdl.getInfo(url).then(async (getUrl) => { 
+       const result = []; 
+       for (let i = 0; i < getUrl.formats.length; i++) { 
+         const item = getUrl.formats[i]; 
+         if (item.container == 'mp4' && item.hasVideo == true && item.hasAudio == true) { 
+           const {qualityLabel, contentLength} = item; 
+           const bytes = await bytesToSize(contentLength); 
+           result[i] = {video: item.url, quality: qualityLabel, size: bytes}; 
+         } 
+       } 
+       const resultFix = result.filter((x) => x.video != undefined && x.size != undefined && x.quality != undefined); 
+       const tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].video}`); 
+       const tinyUrl = tiny.data; 
+       const title = getUrl.videoDetails.title; 
+       const thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url; 
+       resolve({title, result: tinyUrl, rersult2: resultFix[0].video, thumb}); 
+     }).catch(reject); 
+   }); 
+ } 
+  
+ async function ytPlay(query) { 
+   return new Promise((resolve, reject) => { 
+     yts(query).then(async (getData) => { 
+       const result = getData.videos.slice( 0, 5 ); 
+       const url = []; 
+       for (let i = 0; i < result.length; i++) { 
+         url.push(result[i].url); 
+       } 
+       const random = url[0]; 
+       const getAudio = await ytMp3(random); 
+       resolve(getAudio); 
+     }).catch(reject); 
+   }); 
+ } 
+  
+ async function ytPlayVid(query) { 
+   return new Promise((resolve, reject) => { 
+     yts(query).then(async (getData) => { 
+       const result = getData.videos.slice( 0, 5 ); 
+       const url = []; 
+       for (let i = 0; i < result.length; i++) { 
+         url.push(result[i].url); 
+       } 
+       const random = url[0]; 
+       const getVideo = await ytMp4(random); 
+       resolve(getVideo); 
+     }).catch(reject); 
+   }); 
+ } 
+  
+ const getBuffer = async (url, options) => { 
+   try { 
+     options ? options : {}; 
+     const res = await axios({ 
+       method: 'get', 
+       url, 
+       headers: { 
+         'DNT': 1, 
+         'Upgrade-Insecure-Request': 1, 
+       }, 
+       ...options, 
+       responseType: 'arraybuffer', 
+     }); 
+  
+     return res.data; 
+   } catch (e) { 
+     console.log(`Error : ${e}`); 
+   } 
+ };
 
 
 
