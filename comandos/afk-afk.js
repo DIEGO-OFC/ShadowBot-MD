c701015a927f054472049a98b4732a8502bb4c14
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-10 12:25:57 -0400

    Update afk-afk.js

--- comandos/afk-afk.js
+++ comandos/afk-afk.js
@@ -1,12 +1,20 @@
-var handler = async (m, {text}) => {
+var handler = async (m, { text }) => {
   const user = global.db.data.users[m.sender];
   user.afk = +new Date();
   user.afkReason = text;
-  var afkinfo = `*DORRAT - AFK*\n\n*👤 USUARIO QUE ESTARA AFK: ${conn.getName(m.sender)}*\n*❍ MOTIVO (𝙰𝙵𝙺)${text ? ": " + text : ""}*\n❍ NO LO ETIQUETEN`;
-  await m.reply(afkinfo)
+  const afkinfo = `
+    *DORRAT - AFK*
+    
+    *👤 USUARIO QUE ESTARA AFK: ${conn.getName(m.sender)}*
+    *❍ MOTIVO (AFK): ${text ? ": " + text : ""}*
+    *❍ NO LO ETIQUETEN*
+  `;
+  await m.reply(afkinfo);
 };
+
 handler.help = ["afk [alasan]"];
 handler.tags = ["main"];
 handler.command = /^afk$/i;
 handler.dolares = 10;
-export default handler;
+
+export default handler
