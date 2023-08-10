456eead9d04f644d8d3637e332319852a55c27ec
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-10 16:16:56 -0400

    Actualizar adult-comandos+18.js

--- comandos/adult-comandos+18.js
+++ comandos/adult-comandos+18.js
@@ -47,10 +47,11 @@ const handler = async (m, {command, conn}) => {
   }
 
   if (command == 'nsfwglass') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfwglass.json`)).data;
-    const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
-  }
+  const gatapendeja = await akaneko.nsfw.glasses();  
+    const caption = `*乂 ⺀ NSFW GLASS ⺀ 乂*` 
+    await conn.sendFile(m.chat, gatapendeja, "zirax.jpg", caption, m); 
+ }  
+
 
   if (command == 'hentai') {
         const akafw = await akaneko.nsfw.hentai();  
