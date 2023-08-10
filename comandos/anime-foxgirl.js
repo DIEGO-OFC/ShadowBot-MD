b2b832b167b5d60b882904df50cf670c43095f0c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-10 12:33:37 -0400

    Update anime-foxgirl.js

--- comandos/anime-foxgirl.js
+++ comandos/anime-foxgirl.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
-let handler = async (m, {usedPrefix, conn}) => {
-  let foxgirl_irius = await axios.get(`https://nekos.life/api/v2/img/fox_girl`);
-  let buttons = [
-    {
-      buttonId: `${usedPrefix}foxgirl`,
-      buttonText: {displayText: "Siguiente 🦊"},
-      type: 1,
-    },
-  ];
-  let buttonMessage = {
-    image: {url: foxgirl_irius.data.url},
+var  handler = async (m, { usedPrefix, conn }) => {
+  const foxgirl_irius = await axios.get(`https://nekos.life/api/v2/img/fox_girl`);
+
+  const buttonMessage = {
+    image: { url: foxgirl_irius.data.url },
     caption: `*乂 ⺀ ANIME - RANDOM ⺀ 乂*`,
     footer: `*🔥 THE Zirax - BOT 🔥*`,
-    buttons: null,
+    buttons: [
+      {
+        buttonId: `${usedPrefix}foxgirl`,
+        buttonText: { displayText: "Siguiente 🦊" },
+        type: 1,
+      },
+    ],
     headerType: 4,
   };
-  conn.sendMessage(m.chat, buttonMessage, {quoted: m});
+
+  await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
 };
 
 handler.command = /^(foxgirl|girlfox)$/i;
+
 export default handler;
