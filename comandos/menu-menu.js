2e97841687dbd9dd9ad5fc0db23f5fcc3608c52d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-11 01:34:34 -0400

    Update menu-menu.js

--- comandos/menu-menu.js
+++ comandos/menu-menu.js
@@ -21,13 +21,7 @@ var handler = async (m, {
    const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
    const username = conn.getName(who); 
    let pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-   const fload = { key : ,{ remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023,
-    status: 1,
-    surface : 1,
-    message: '[ 𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 𝐌𝐄𝐍𝐔... ]', 
-    orderTitle: `▮Menu ▸`,
-    thumbnail: await (await fetch(pp)).buffer(), 
-    sellerJid: '0@s.whatsapp.net' 
+   const fload = { key : { remoteJid: 'status@broadcast', participant : '0@s.whatsapp.net' }, message: { orderMessage: { itemCount : 2023, status: 1, surface : 1, message: '[ 𝐂𝐀𝐑𝐆𝐀𝐍𝐃𝐎 𝐌𝐄𝐍𝐔... ]', orderTitle: `▮Menu ▸`, thumbnail: await (await fetch(pp)).buffer(), sellerJid: '0@s.whatsapp.net' 
     }
     }
     }
