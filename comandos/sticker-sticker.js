d9d6b9ac421f2ae6efcabaaf26c2286f4766f309
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-12 01:00:10 -0400

    Actualizar sticker-sticker.js

--- comandos/sticker-sticker.js
+++ comandos/sticker-sticker.js
@@ -1,7 +1,62 @@
-//import {sticker} from "../lib/sticker.js";
-//import uploadFile from "../lib/uploadFile.js";
-//import uploadImage from "../lib/uploadImage.js";
-//import {webp2png} from "../lib/webp2mp4.js";
+import {sticker} from '../lib/sticker.js'; 
+ import uploadFile from '../lib/uploadFile.js'; 
+ import uploadImage from '../lib/uploadImage.js'; 
+ import {webp2png} from '../lib/webp2mp4.js'; 
+  
+ const handler = async (m, {conn, args, usedPrefix, command}) => { 
+   if (usedPrefix == 'a' || usedPrefix == 'A') return; 
+   let stiker = false; 
+   const user = db.data.users[m.sender]; 
+   try { 
+     const q = m.quoted ? m.quoted : m; 
+     const mime = (q.msg || q).mimetype || q.mediaType || ''; 
+     if (/webp|image|video/g.test(mime)) { 
+       const img = await q.download?.(); 
+       if (!img) throw `*[❌] Responda a una imagen o video*`; 
+       let out; 
+       try { 
+         stiker = await sticker(img, false, global.packname, global.author); 
+       } catch (e) { 
+         console.error(e); 
+       } finally { 
+         if (!stiker) { 
+           if (/webp/g.test(mime)) out = await webp2png(img); 
+           else if (/image/g.test(mime)) out = await uploadImage(img); 
+           else if (/video/g.test(mime)) out = await uploadFile(img); 
+           if (typeof out !== 'string') out = await uploadImage(img); 
+           stiker = await sticker(false, out, global.packname, global.author); 
+         } 
+       } 
+     } else if (args[0]) { 
+       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author); 
+       else return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙽𝙾 𝙴𝚂 𝚅𝙰𝙻𝙸𝙳𝙰, 𝙻𝙰 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝚄𝚁𝙻 / 𝙻𝙸𝙽𝙺 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 .𝚓𝚙𝚐, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix}s https://telegra.ph/file/0dc687c61410765e98de2.jpg*'); 
+     } 
+   } catch (e) { 
+     console.error(e); 
+     if (!stiker) stiker = e; 
+   } finally { 
+     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m); 
+     else throw '*[❌] Responda a una imagen o video*'; 
+   } 
+ }; 
+ handler.help = ['sfull']; 
+ handler.tags = ['sticker']; 
+ handler.command = /^s(tic?ker)?(gif)?(wm)?$/i; 
+  
+ export default handler; 
+  
+ const isUrl = (text) => { 
+   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi')); 
+ };
+
+
+
+
+
+/*import {sticker} from "../lib/sticker.js";
+import uploadFile from "../lib/uploadFile.js";
+import uploadImage from "../lib/uploadImage.js";
+import {webp2png} from "../lib/webp2mp4.js";
 
 let handler = async (m, {conn, args, usedPrefix, command}) => {
   let stiker = false;
