ea0e238b7285ad505aba8d59a44427721f506140
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-11 20:38:12 -0400

    Actualizar downloader-spotify.js

--- comandos/descargas/downloader-spotify.js
+++ comandos/descargas/downloader-spotify.js
@@ -1,19 +1,82 @@
-import fetch from "node-fetch";
-let handler = async (m, {conn, text}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙰 𝙱𝚄𝚂𝙲𝙰𝚁*`;
-  try {
-    let res = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${text}`);
-    let json = await res.json();
-    let {link} = json.result[0];
-    let res2 = await fetch(`https://api.lolhuman.xyz/api/spotify?apikey=${lolkeysapi}&url=${link}`);
-    let json2 = await res2.json();
-    let {thumbnail, title, artists, preview_url} = json2.result;
-    let spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒\n┬\n├‣✨ *𝚃𝙸𝚃𝚄𝙻𝙾:* ${title}\n┴\n┬\n├‣🗣️ *𝙰𝚁𝚃𝙸𝚂𝚃𝙰:* ${artists}\n┴\n┬\n├‣🌐 *𝚄𝚁𝙻*: ${link}\n┴\n┬\n├‣💚 *𝚄𝚁𝙻 𝙳𝙴 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰:* ${preview_url}\n┴`;
-    conn.sendFile(m.chat, thumbnail, "error.jpg", spotifyi, m);
-    await conn.sendFile(m.chat, preview_url, "error.mp3", null, m, false, {mimetype: "audio/mp4"});
-  } catch (e) {
-    throw "*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙻𝙾𝙶𝚁𝙾 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝙻𝙰 𝙿𝙰𝙶𝙸𝙽𝙰 𝙳𝙴 𝙰𝚈𝚄𝙳𝙰 𝙿𝙰𝚁𝙰 𝙱𝚄𝚂𝙲𝙰𝚁 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙴𝚂𝚃𝙰 𝙲𝙰𝙸𝙳𝙰, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝚁𝙽𝚃𝙰𝚁𝙻𝙾 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴*";
-  }
-};
-handler.command = /^(spotify|music)$/i;
-export default handler;
+
+import fetch from 'node-fetch'; 
+ import Spotify from 'spotifydl-x'; 
+ import fs from 'fs'; 
+ import NodeID3 from 'node-id3'; 
+ import axios from 'axios'; 
+  
+ const credentials = { 
+   clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3', 
+   clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009', 
+ }; 
+ const spotify = new Spotify.default(credentials); 
+  
+ var handler = async (m, { conn, text }) => { 
+  if (!text) throw `*[✖️] Escriba el nombre de una cancion.*`; 
+   try { 
+     const resDL = await fetch(`https://api.lolhuman.xyz/api/spotifysearch?apikey=${lolkeysapi}&query=${text}`); 
+     const jsonDL = await resDL.json(); 
+     const linkDL = jsonDL.result[0].link; 
+     const spty = await spotifydl(linkDL); 
+     const getRandom = (ext) => { 
+       return `${Math.floor(Math.random() * 10000)}${ext}`; 
+     }; 
+     const randomName = getRandom('.mp3'); 
+     const filePath = `./tmp/${randomName}`; 
+     const artist = spty.data.artists.join(', ') || '-'; 
+     const tags = { 
+       title: spty.data.name || '-', 
+       artist: artist, 
+       album: spty.data.album_name || '-', 
+       year: spty.data.release_date || '-', 
+       genre: '-', 
+       comment: { 
+         language: 'spa', 
+         text: '🤴🏻 вσтωнαιтα є∂ιт ƒαвяι115© 🤖', 
+       }, 
+       unsynchronisedLyrics: { 
+         language: 'spa', 
+         text: '🤴🏻 вσтωнαιтα є∂ιт ƒαвяι115© 🤖', 
+       }, 
+       image: { 
+         mime: './Menu2.jpg', 
+         type: { 
+           id: 3, 
+           name: 'front cover', 
+         }, 
+         description: 'Spotify Thumbnail', 
+         imageBuffer: await axios.get(spty.data.cover_url, { responseType: "arraybuffer" }).then((response) => Buffer.from(response.data, "binary")), 
+       }, 
+       mimetype: 'image/jpeg', 
+       copyright: 'Copyright Darlyn ©2023', 
+     }; 
+     await fs.promises.writeFile(filePath, spty.audio); 
+     await NodeID3.write(tags, filePath); 
+     const spotifyi = `❒═════❬ 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 ❭═════╾❒\n┬\n├‣🔍 *TÍTOLO:* ${spty.data.name}\n┴\n┬\n├‣🗣️ *ARTISTA:* ${spty.data.artists}\n┴\n┬\n├‣🌐 *𝚄𝚁𝙻*: ${linkDL}\n┴`; 
+     await conn.sendFile(m.chat, spty.data.cover_url, 'error.jpg', spotifyi, m); 
+     await conn.sendMessage(m.chat, { audio: fs.readFileSync(`./tmp/${randomName}`), fileName: `${spty.data.name}.mp3`, mimetype: 'audio/mp4' }, { quoted: m }); 
+   } catch (error) { 
+     console.error(error); 
+     throw '*[❗] Errore.*'; 
+   } 
+ }; 
+ handler.command = /^(spotify|music)$/i; 
+ export default handler; 
+  
+ async function spotifydl(url) { 
+   return new Promise(async (resolve, reject) => { 
+     try { 
+       const res = await spotify.getTrack(url); 
+       const timeoutPromise = new Promise((_, reject) => { 
+         setTimeout(() => { 
+           reject(new Error('Tempo esaurito')); 
+         }, 300000); 
+       }); 
+       const audioPromise = spotify.downloadTrack(url); 
+       const audio = await Promise.race([audioPromise, timeoutPromise]); 
+       resolve({ data: res, audio }); 
+     } catch (error) { 
+       reject(error); 
+     } 
+   }); 
+ }
