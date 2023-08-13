1b5e17f9b4f2ab24c5b4bf1b5fdbfefe53b92b35
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 02:09:41 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -14,7 +14,7 @@ import {sticker} from '../lib/sticker.js';
      const json = await res.json(); 
      const {url} = json; 
      const text2 = `@${m.sender.split("@")[0]}`; Te dio besos ${text}`.trim()
-      await await conn.sendMessage(m.chat, {text: text2, mentions: [_user, m.sender]}, {quoted: m});
+ conn.sendMessage(m.chat, {text: text2, mentions: [_user, m.sender]}, {quoted: m})
 const stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio besos a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`); 
 conn.sendFile(m.chat, stiker, null, {asSticker: true}); 
    } catch (e) { } 
