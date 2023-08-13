4257c661442693ca120fea040b95ee477e62fee3
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 00:49:24 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -1,18 +1,40 @@
-import {sticker} from "../lib/sticker.js";
+import { sticker } from "../lib/sticker.js";
 import fetch from "node-fetch";
 import axios from "axios";
-let handler = async (m, {conn}) => {
-let besoo = await axios.get(`https://nekos.life/api/kiss`);
-let Menssage = {
-    video: {url: besoo.data.url},
+import fs from "fs";
+import ffmpeg from 'ffmpeg-static'
+import exec from 'child_process'
+
+let handler = async (m, { conn }) => {
+  let besoo = await axios.get(`https://nekos.life/api/kiss`);
+  let Menssage = {
+    video: { url: besoo.data.url },
     caption: `😘 +${m.sender.split("@")[0]} le dio besos a ${m.mentionedJid
-        .map((user) => (user === m.sender ? "alguien " : `+${user.split("@")[0]}`))
-        .join(", ")}`,
-    footer: `*🔥 THE DORRAT - BOT 🔥*`,
+      .map((user) => (user === m.sender ? "alguien " : `+${user.split("@")[0]}`))
+      .join(", ")}`,
+    footer: `*🔥 THE DORRAT - BOT 🔥*`,
     headerType: 4,
   };
-  conn.sendMessage(m.chat, Menssage, {quoted: m});
- 
+
+  const attachmentData = await fetch(Menssage.video.url).then((r) => r.buffer());
+  const inputPath = `./input.mp4`;
+  const outputPath = `./output.gif`;
+
+  fs.writeFileSync(inputPath, attachmentData);
+
+  const cmd = `"${ffmpeg}" -i "${inputPath}" -vf "fps=10,scale=320:-1:flags=lanczos" "${outputPath}"`;
+
+  exec(cmd, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const gifData = fs.readFileSync(outputPath);
+
+    conn.sendMessage(m.chat, gifData, { sendMediaAsSticker: true, quoted: m });
+  });
 };
+
 handler.command = /^(kiss|skiss|kis|besos|beso)$/i;
 export default handler;
