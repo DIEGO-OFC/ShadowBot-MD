7f1959203c9f195e56dd7821dd38feb7951c8ad5
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 12:38:45 -0400

    Create random-fish.js

---
+++ comandos/random/random-fish.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+let handler = async (m, {conn}) => {
+  try {
+    let res = await fetch("https://api.sefinek.net/api/v1/animals/fish");
+    let json = await res.json();
+    let img = await json.url;
+    let caption = `
+_©The Zirax - Bot_
+`.trim();
+    conn.sendFile(m.chat, img, "fish.jpg", caption, m);
+  } catch (e) {
+    console.log(e);
+    throw "*Error!*";
+  }
+};
+handler.help = ["fish"];
+handler.tags = ["random"];
+handler.command = ["fish", "pescado"];
+handler.fail = null;
+export default handler;
