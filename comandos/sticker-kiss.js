65b20e5d44036751d9ccd325e9ec20dc8ae677e9
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 01:26:46 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -1,14 +1,15 @@
 import {sticker} from "../lib/sticker.js";
 import fetch from "node-fetch";
 import axios from "axios";
+import ffmpeg from "fluent-ffmpeg";
 
 let handler = async (m, {conn}) => {
   let besoo = await axios.get(`https://nekos.life/api/kiss`);
   let videoUrl = besoo.data.url;
-  let gifUrl = await convertMP4toGIF(videoUrl);
+  let gifData = await convertMP4toGIF(videoUrl);
 
   let Menssage = {
-    video: {url: gifUrl},
+    video: {url: `data:image/gif;base64,${gifData}`},
     caption: `😘 +${m.sender.split("@")[0]} le dio besos a ${m.mentionedJid
         .map((user) => (user === m.sender ? "alguien " : `+${user.split("@")[0]}`))
         .join(", ")}`,
@@ -24,14 +25,8 @@ handler.command = /^(kiss|skiss|kis|besos|beso)$/i;
 export default handler;
 
 async function convertMP4toGIF(videoUrl) {
-  const response = await fetch(videoUrl);
-  const blob = await response.blob();
-  const reader = new FileReader();
-  reader.onload = async () => {
-    const gifData = await reader.result;
-    const base64 = btoa(gifData);
-    const gifUrl = `data:image/gif;base64,${base64}`;
-    return gifUrl;
-  };
-  reader.readAsBinaryString(blob);
+  const ffmpeg = new ffmpeg();
+  const output = await ffmpeg.run(`-i ${videoUrl} -vf "fps=10" -f gif -y output.gif`);
+  const data = await fetch(output).buffer();
+  return data.toString("base64");
 }
