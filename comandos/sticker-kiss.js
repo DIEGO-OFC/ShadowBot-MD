b96f8e7a99a744b4afc215144414bf8d26110773
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 01:54:56 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -8,7 +8,8 @@ import {sticker} from '../lib/sticker.js';
      const res = await fetch('https://nekos.life/api/kiss'); 
      const json = await res.json(); 
      const {url} = json; 
-
+     const text2 = `+${m.sender.split('@')[0]} Te dio besos $(text}`.trim()
+  m.reply(text2, null, {mentions: conn.parseMention(text2)});
      conn.sendFile(m.chat, url, null, {asSticker: true}); 
    } catch (e) { } 
  }; 
