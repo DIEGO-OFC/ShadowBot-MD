632d23acde6a82cbffac457aebb1b0898bfe37d8
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 13:10:40 -0400

    Update random-fish.js

--- comandos/random/random-fish.js
+++ comandos/random/random-fish.js
@@ -1,21 +1,11 @@
-import fetch from "node-fetch";
-import axios from "axios";
+import fetch from 'node-fetch'
 let handler = async (m, {conn}) => {
-  try {
-    let res = await axios.get(`https://api.sefinek.net/api/v1/animals/fish`);
-    let json = await res.json();
-    let img = await json.url;
-    let caption = `
-_©The Zirax - Bot_
-`.trim();
-    conn.sendFile(m.chat, img, "fish.jpg", caption, m);
-  } catch (e) {
-    console.log(e);
-    throw "*Error!*";
-  }
-};
-handler.help = ["fish"];
-handler.tags = ["random"];
-handler.command = ["fish", "pescado"];
-handler.fail = null;
-export default handler;
+let res = await fetch(`https://api.sefinek.net/api/v1/animals/fish`)
+
+let json = await res.json()
+
+conn.sendMessage(m.chat, {image: {url: json.message }}, {quoted: m})
+}
+handler.help = ['fish']
+handler.command = ['pescado', 'fish']
+export default handler 
