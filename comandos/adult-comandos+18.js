de7877a8521dd95efece115d19f8fdca276ba38d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-12 23:46:18 -0400

    Actualizar adult-comandos+18.js

--- comandos/adult-comandos+18.js
+++ comandos/adult-comandos+18.js
@@ -102,9 +102,9 @@ if (command == 'tetas2') {
 }
 
   if (command == 'pornoorgy') {
-    const res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/nsfworgy.json`)).data;
-    const haha = await res[Math.floor(res.length * Math.random())];
-    conn.sendMessage(m.chat, {image: {url: haha}, caption: `_${command}_`.trim()}, {quoted: m});
+const orgy = await akaneko.nsfw.orgy();  
+    const caption = `*乂 ⺀ NSFW ORGY ⺀ 乂*` 
+    await conn.sendFile(m.chat, orgt, "zirax.jpg", caption, m);
   }
 
   if (command == 'tetas') {
