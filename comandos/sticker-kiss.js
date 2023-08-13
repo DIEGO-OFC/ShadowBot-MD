008fa793b68faa78c34bc5ae74e251c391d0b7fe
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 02:03:27 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -3,12 +3,16 @@ import {sticker} from '../lib/sticker.js';
  import MessageType from '@whiskeysockets/baileys'; 
  const handler = async (m, {conn, text}) => { 
    try { 
+  let who; 
+   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender; 
+   else who = m.sender; 
+   let name = conn.getName(who);
      if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender); 
      if (!m.mentionedJid.length) m.mentionedJid.push(m.sender); 
      const res = await fetch('https://nekos.life/api/kiss'); 
      const json = await res.json(); 
      const {url} = json; 
-     const text2 = `+${m.sender.split('@')[0]} Te dio besos ${text}`.trim()
+     const text2 = `${name} Te dio besos ${text}`.trim()
   m.reply(text2, null, {mentions: conn.parseMention(text2)});
 const stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio besos a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`); 
 conn.sendFile(m.chat, stiker, null, {asSticker: true}); 
