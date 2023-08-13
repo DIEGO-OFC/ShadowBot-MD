025e80540f3940974a59e5ed95eae04a910a008b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-13 01:20:30 -0400

    Actualizar sticker-kiss.js

--- comandos/sticker-kiss.js
+++ comandos/sticker-kiss.js
@@ -1,31 +1,37 @@
-import { sticker } from "../lib/sticker.js";
+import {sticker} from "../lib/sticker.js";
 import fetch from "node-fetch";
 import axios from "axios";
-import {readdirSync, statSync, unlinkSync, existsSync, readFileSync, rmSync, watch} from "fs";
-import ffmpeg from 'ffmpeg-static'
-import { exec } from 'child_process'
 
-let handler = async (m, { conn }) => {
+let handler = async (m, {conn}) => {
   let besoo = await axios.get(`https://nekos.life/api/kiss`);
+  let videoUrl = besoo.data.url;
+  let gifUrl = await convertMP4toGIF(videoUrl);
+
   let Menssage = {
-    video: { url: besoo.data.url },
+    video: {url: gifUrl},
     caption: `😘 +${m.sender.split("@")[0]} le dio besos a ${m.mentionedJid
-      .map((user) => (user === m.sender ? "alguien " : `+${user.split("@")[0]}`))
-      .join(", ")}`,
+        .map((user) => (user === m.sender ? "alguien " : `+${user.split("@")[0]}`))
+        .join(", ")}`,
     footer: `*🔥 THE DORRAT - BOT 🔥*`,
     headerType: 4,
   };
 
-  const attachmentData = await fetch(Menssage.video.url).then((r) => r.buffer());
-  const inputPath = `./input.mp4`;
-  const outputPath = `./output.gif`;
-
-  fs.writeFileSync(inputPath, attachmentData);
-
-    const gifData = fs.readFileSync(outputPath);
-
-        conn.sendMessage(m.chat, gifData, {asSticker: true}, {quoted: m});
+  conn.sendMessage(m.chat, Menssage, {quoted: m});
 };
 
 handler.command = /^(kiss|skiss|kis|besos|beso)$/i;
+
 export default handler;
+
+async function convertMP4toGIF(videoUrl) {
+  const response = await fetch(videoUrl);
+  const blob = await response.blob();
+  const reader = new FileReader();
+  reader.onload = async () => {
+    const gifData = await reader.result;
+    const base64 = btoa(gifData);
+    const gifUrl = `data:image/gif;base64,${base64}`;
+    return gifUrl;
+  };
+  reader.readAsBinaryString(blob);
+}
