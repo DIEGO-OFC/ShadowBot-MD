80e3224b368e7bbbe88b4be39fa0002154979413
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-15 01:17:36 -0400

    Actualizar index-inicio.js

--- index-inicio.js
+++ index-inicio.js
@@ -154,3 +154,30 @@ global.reload = async (_ev, filename) => {
 Object.freeze(global.reload);
 watch(comandosFolder, global.reload);
 await global.reloadHandler();
+if (!opts["test"]) {
+  if (global.db)
+    setInterval(async () => {
+      if (global.db.data) await global.db.write();
+      if (opts["autocleartmp"] && (global.support || {}).find)
+        (tmp = [os.tmpdir(), "tmp"]), tmp.forEach((filename) => cp.spawn("find", [filename, "-amin", "3", "-type", "f", "-delete"]));
+    }, 30 * 1000);
+}
+
+if (opts["server"]) (await import("./server.js")).default(global.conn, PORT);
+
+function clearTmp() {
+  const tmp = [tmpdir(), join(__dirname, "./tmp")];
+  const filename = [];
+  tmp.forEach((dirname) => readdirSync(dirname).forEach((file) => filename.push(join(dirname, file))));
+  return filename.map((file) => {
+    const stats = statSync(file);
+    if (stats.isFile() && Date.now() - stats.mtimeMs >= 1000 * 60 * 3) return unlinkSync(file); // 3 minutes
+    return false;
+  });
+}
+setInterval(async () => {
+  var a = await clearTmp();
+  console.log(
+    chalk.cyanBright(`\n▣════════[ 𝐀𝐔𝐓𝐎𝐂𝐋𝐄𝐀𝐑-𝐓𝐌𝐏 ]════════════...\n│\n▣─➢ 𝐁𝐚𝐬𝐮𝐫𝐚 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐝𝐚 ✅\n│\n▣═════════════════════════════════════...\n`)
+  );
+}, 180000);
