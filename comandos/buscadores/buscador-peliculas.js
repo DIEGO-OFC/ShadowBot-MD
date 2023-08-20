02ee6332f7a067c9f61517bbe0ca8ea3e65b2a53
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-20 14:55:46 -0400

    Actualizar buscador-peliculas.js

--- comandos/buscadores/buscador-peliculas.js
+++ comandos/buscadores/buscador-peliculas.js
@@ -21,7 +21,7 @@ let handler = async (m, {text, usedPrefix, command, conn}) => {
 *Grabacion ∙* ${x.Country || "-"}
 
    ❕Pedido por @${m.sender.split("@")[0]}
-*🔥 ${wm2} 🔥*`;
+*🔥 ${wm3} 🔥*`;
 
   conn.sendMessage(m.chat, {image: {url: x.Poster}, caption: mov_txt, mentions: [m.sender]}, {quoted: m});
 };
