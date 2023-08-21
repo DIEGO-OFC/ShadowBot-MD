de300408089e9c246b7323614e66e9e304104ef0
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-21 14:53:11 -0400

    Actualizar buscador-google.js

--- comandos/buscadores/buscador-google.js
+++ comandos/buscadores/buscador-google.js
@@ -1,4 +1,32 @@
-import {googleIt} from "@bochilteam/scraper";
+
+import {googleIt} from '@bochilteam/scraper'; 
+ import google from 'google-it'; 
+ import axios from 'axios'; 
+ var handler = async (m, { conn, command, args, usedPrefix }) => { 
+   const fetch = (await import('node-fetch')).default; 
+   const text = args.join` `; 
+   if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m); 
+ const url = 'https://google.com/search?q=' + encodeURIComponent(text); 
+ google({'query': text}).then(res => { 
+ let teks = `*RESULTADOS DE : _${text}_*\n\n${url}\n\n` 
+ for (let g of res) { 
+ teks += `_*${g.title}*_\n_${g.link}_\n_${g.snippet}_\n\n` 
+ }  
+ const ss = `https://image.thum.io/get/fullpage/${url}` 
+ conn.sendFile(m.chat, ss, 'error.png', teks, m) 
+ //m.reply(teks) 
+ }) 
+ }  
+ handler.help = ['google', 'googlef'].map((v) => v + ' <pencarian>'); 
+ handler.tags = ['internet']; 
+ handler.command = /^googlef?$/i; 
+ export default handler;
+
+
+
+
+
+/*import {googleIt} from "@bochilteam/scraper";
 let handler = async (m, {conn, command, args}) => {
   const fetch = (await import("node-fetch")).default;
   let full = /f$/i.test(command);
@@ -29,4 +57,4 @@ let handler = async (m, {conn, command, args}) => {
 handler.help = ["google", "googlef"].map((v) => v + " <pencarian>");
 handler.tags = ["search"];
 handler.command = /^googlef?$/i;
-export default handler;
+export default handler;*/
