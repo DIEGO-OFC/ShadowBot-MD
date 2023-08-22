ea4aa6ec21f6f00315af080f4aaed52e33f3c34a
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-22 01:53:56 -0400

    Actualizar main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ import {Boom} from '@hapi/boom';
 import {makeWASocket, protoType, serialize} from "./lib/simple.js";
 import {Low, JSONFile} from "lowdb";
 import {mongoDB, mongoDBV2} from "./lib/mongoDB.js";
-import store from "./lib/store.js";
+//import store from "./lib/store.js";
 const {proto} = (await import("@whiskeysockets/baileys")).default;
 const {DisconnectReason, useMultiFileAuthState, MessageRetryMap, fetchLatestBaileysVersion, makeCacheableSignalKeyStore} = await import("@whiskeysockets/baileys");
 const {CONNECTING} = ws;
