e974fda3cc5a852f6eb8c564638a02e4bee6920d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-22 14:00:01 -0400

    Actualizar random-loli.js

--- comandos/random/random-loli.js
+++ comandos/random/random-loli.js
@@ -2,17 +2,17 @@ import Booru from 'booru'
 import fetch from 'node-fetch'
 let sites = ['sb', 'kn', 'kc']
 
-var PTM = async (m, { conn, usedPrefix, command }) => {
+var shadow = async (m, { conn, usedPrefix, command }) => {
 	let res = await Booru.search(sites.getRandom(), ['loli'], { random: true })
 	let url = res[0].fileUrl
 let caption = `${wm3}`
     conn.sendFile(m.chat, await shortUrl(url), "loli", caption, m);
 }
-PTM.help = ['loli']
-PTM.tags = ['random']
-PTM.command = /^(loli)$/i
+shadow.help = ['loli']
+shadow.tags = ['random']
+shadow.command = /^(loli)$/i
 
-export default PTM
+export default shadow
 
 async function shortUrl(url) {
 	return await (await fetch(`https://tinyurl.com/api-create.php?url=${url}`)).text()
