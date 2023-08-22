fd3172050d8b050adb00d32ef703e133ec8b516d
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-22 14:21:45 -0400

    Actualizar random-C. Ronaldo.js

--- comandos/random/random-C. Ronaldo.js
+++ comandos/random/random-C. Ronaldo.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 var shadow = async(m, { conn, usedPrefix, command }) => {
 //let cristiano = (await axios.get(`https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/master/galeria/JSON/CristianoRonaldo.json`)).data 
-let cristiano = (await axios.get(`https://raw.githubusercontent.com/felipecsl/63e16660a58071250b51/raw/b32e55d90ac24504c3499d6f9fd33d3f1f608194/cristiano_ronaldo.json`)).data  
+let cristiano = (await axios.get(`https://gist.githubusercontent.com/felipecsl/63e16660a58071250b51/raw/b32e55d90ac24504c3499d6f9fd33d3f1f608194/cristiano_ronaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
 let caption = `${wm3}\n\nSIUUUU`
 conn.sendFile(m.chat, ronaldo, "cr7.jpg", caption, m);
