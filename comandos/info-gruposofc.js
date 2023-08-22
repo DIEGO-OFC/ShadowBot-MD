11c19df6a20a24381cbbe1a9b1066125e4b09038
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-21 21:32:47 -0400

    Actualizar info-gruposofc.js

--- comandos/info-gruposofc.js
+++ comandos/info-gruposofc.js
@@ -1,6 +1,6 @@
 
 var handler = async (m, {conn}) => {
-const media = './Menu2.jpg';
+const media = fotogodzx 
 const grupos = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝐒𝐡𝐚𝐝𝐨𝐰𝐁𝐨𝐭 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
 ╭━━❍𝐓𝐡𝐞 𝐒𝐡𝐚𝐝𝐨𝐰 𝐁𝐫𝐨𝐜𝐤𝐞𝐫𝐬 - 𝐁𝐨𝐭❍━━╮
