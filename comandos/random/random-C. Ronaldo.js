aae6979683c1fe90d8f0828146ba3a9616adb121
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-22 14:16:44 -0400

    Actualizar random-C. Ronaldo.js

--- comandos/random/random-C. Ronaldo.js
+++ comandos/random/random-C. Ronaldo.js
@@ -1,3 +1,18 @@
+import fetch from 'node-fetch' 
+ let handler = async (m, {conn}) => { 
+ let res = await fetch(`https:\/\/fifa15.content.easports.com\/fifa\/fltOnlineAssets\/8D941B48-51BB-4B87-960A-06A61A62EBC0\/2015\/fut\/items\/images`) 
+  
+ let json = await res.json() 
+  
+ conn.sendMessage(m.chat, {image: {url: json.message }}, {quoted: m}) 
+ } 
+ handler.help = ['fish'] 
+ handler.command = ['cr7', 'cris'] 
+ export default handler 
+
+
+
+
 /*import axios from 'axios'
 var shadow = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/DIEGO-OFC/DORRAT-BOT-MD/master/galeria/JSON/CristianoRonaldo.json`)).data  
