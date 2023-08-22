60cbbcf6497f2156dcf34adf0948afe7c462c596
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-22 11:13:34 -0400

    Actualizar random-loli.js

--- comandos/random/random-loli.js
+++ comandos/random/random-loli.js
@@ -5,7 +5,7 @@ let sites = ['sb', 'kn', 'kc']
 let handler = async (m, { conn, usedPrefix, command }) => {
 	let res = await Booru.search(sites.getRandom(), ['loli'], { random: true })
 	let url = res[0].fileUrl
-	conn.sendButton(m.chat, 'Random Image Loli', await shortUrl(url), url, [['Next', usedPrefix + command]], m)
+    conn.sendFile(m.chat, await shortUrl(url), "lizard.jpg", caption, m);
 }
 handler.help = ['loli']
 handler.tags = ['random']
