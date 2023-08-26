2994e8cb429ca2dd8a2e777600d6d0a40a0ed3e8
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 22:19:57 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -1,30 +1,40 @@
 //hecho por DIEGO-OFC pro
 
+
 import axios from "axios";
 
 const handler = async (m, { text }) => {
-  const bingSearchUrl = "https://www.bing.com/search?q=";
-  const term = encodeURIComponent(text.toLowerCase()); // Codificar el término de búsqueda
-  const options = {
-    params: {
-      q: term,
-    },
-  };
+    const bingSearchUrl = "https://www.bing.com/search?q=";
+    const term = encodeURIComponent(text.toLowerCase());
 
-  try {
-    const response = await axios.get(bingSearchUrl, options);
-    
-    if (response.status === 200) { // Cambiado a 200 en lugar de 400
-      const results = response.data.webPages.value; // Acceder a la propiedad "value" para obtener los resultados
-      const resultTitles = results.map(result => result.name); // Extraer títulos de los resultados
-      m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
-    } else {
-      throw new Error(`Error al buscar "${text}"`);
+    const options = {
+        params: {
+            q: term,
+        },
+    };
+
+    try {
+        const response = await axios.get(bingSearchUrl, options);
+
+        if (response.status === 200) {
+            const webPages = response.data.webPages;
+
+            if (webPages && webPages.value && Array.isArray(webPages.value)) {
+                const results = webPages.value;
+                const resultTitles = results.map(result => result.name);
+                m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
+            } else {
+                throw new Error(`No se encontraron resultados válidos para "${text}"`);
+            }
+        } else {
+            throw new Error(`Error al buscar "${text}"`);
+        }
+    } catch (error) {
+        m.reply(error.message);
     }
-  } catch (error) {
-    m.reply(error.message);
-  }
 };
 
 handler.command = ["bing"];
+
 export default handler;
+
