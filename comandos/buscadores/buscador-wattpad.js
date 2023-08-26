4157682e51a4ac6b3639697626d2aaf80241346c
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 23:16:09 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -1,30 +1,34 @@
 //hecho por DIEGO-OFC pro
 
-import axios from "axios"; 
-  
- const handler = async (m, { text }) => { 
-   const bingSearchUrl = "https://www.bing.com/search"; 
-   const term = encodeURIComponent(text.toLowerCase()); 
-  
-   const options = { 
-     params: { 
-       q: term, 
-     }, 
-   }; 
-  
-   const response = await axios.get(bingSearchUrl, options); 
-  
-   if (response.status === 200) { 
-     const webPages = response.data.webPages; 
-  
-     if (webPages && webPages.value && Array.isArray(webPages.value)) { 
-       const results = webPages.value; 
-       const resultTitles = results.map(result => result.name); 
- m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`); 
-} 
-}      
- }; 
-  
- handler.command = ["bing"]; 
-  
- export default handler;
+import axios from "axios";
+
+const handler = async (m, { text }) => {
+  const bingSearchUrl = "https://www.bing.com/search";
+  const term = encodeURIComponent(text.toLowerCase());
+
+  const options = {
+    params: {
+      q: term,
+    },
+  };
+
+  const response = await axios.get(bingSearchUrl, options);
+
+  if (response.status !== 200) {
+    return;
+  }
+
+  const webPages = response.data.webPages;
+
+  if (!webPages || !webPages.value || !Array.isArray(webPages.value)) {
+    return;
+  }
+
+  const results = webPages.value;
+  const resultTitles = results.map(result => result.name);
+  m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
+};
+
+handler.command = ["bing"];
+
+export default handler;
