b13d00e968a331ba0fcf7133474767345a9bd34e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 21:04:39 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -20,7 +20,7 @@ const handler = async (m, { text, conn }) => {
   }
 
   // Obtén los resultados de la búsqueda
-  const results = await response.json();
+    let resulta = await response.json();
 
   // Imprime los resultados de la búsqueda
   m.reply(results.webPages.map(({ title, url }) => `${title} - ${url}`));
