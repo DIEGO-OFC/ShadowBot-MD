0d8630e7ac585d081ac669ab3459debdf14c3592
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 22:09:32 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -3,10 +3,8 @@
 import axios from "axios";
 
 const handler = async (m, { text }) => {
-const bingSearchUrl = "https://www.bing.com/search?q=";
-
-  const term = text.toLowerCase();
-
+  const bingSearchUrl = "https://www.bing.com/search?q=";
+  const term = encodeURIComponent(text.toLowerCase()); // Codificar el término de búsqueda
   const options = {
     params: {
       q: term,
@@ -15,19 +13,18 @@ const bingSearchUrl = "https://www.bing.com/search?q=";
 
   try {
     const response = await axios.get(bingSearchUrl, options);
-
-    if (response.status === 400) {
-      throw new Error(`Error al buscar "${term}"`);
+    
+    if (response.status === 200) { // Cambiado a 200 en lugar de 400
+      const results = response.data.webPages.value; // Acceder a la propiedad "value" para obtener los resultados
+      const resultTitles = results.map(result => result.name); // Extraer títulos de los resultados
+      m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
+    } else {
+      throw new Error(`Error al buscar "${text}"`);
     }
-
-    const results = response.data.webPages;
-
-    m.reply(`Los resultados de la búsqueda de "${term}" son: ${results}`);
   } catch (error) {
     m.reply(error.message);
   }
 };
 
 handler.command = ["bing"];
-
 export default handler;
