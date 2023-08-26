3504fe2f6a7572977107d00fc7e3e39074485231
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 22:25:50 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const handler = async (m, { text }) => {
-    const bingSearchUrl = "https://www.bing.com/search?q=${text}";
+    const bingSearchUrl = "https://www.bing.com/search";
     const term = encodeURIComponent(text.toLowerCase());
 
     const options = {
