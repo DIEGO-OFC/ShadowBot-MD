c0bea73cdbcc987b58655be34d04f8235c128fb4
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 23:33:22 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -3,32 +3,9 @@
 import axios from "axios";
 
 const handler = async (m, { text }) => {
-    const term = encodeURIComponent(text.toLowerCase());
-const bingSearchUrl = `https://www.bing.com/search?q=${term}`
-
-  const options = {
-    params: {
-      q: term,
-    },
-  };
-
-  const response = await axios.get(bingSearchUrl, options);
-
-  if (response.status !== 200) {
-    return;
-  }
-
-  const webPages = response.data.webPages;
-
-  if (!webPages || !webPages.value || !Array.isArray(webPages.value)) {
-    return;
-  }
-
-  const results = webPages.value;
-  const resultTitles = results.map(result => result.name);
-  m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
-};
-
+    let name = encodeURIComponent(options.getString('text')); 
+     let link = `https://www.bing.com/search?q=${name}`;
+m.reply(`${texto}\n${link}`)
 handler.command = ["bing"];
 
 export default handler;
