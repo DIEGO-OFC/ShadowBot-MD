30bb4bc0ebc549ae6d4c8806b8f8f2185fd51a3b
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 20:49:50 -0400

    Update buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -1,20 +1,7 @@
-
-
-import fetch from 'node-fetch'
-import translate from '@vitalets/google-translate-api'
-
-let handler = async (m, { conn, text }) => {
-if (!text) throw `*Falta texto*`
-let res = await fetch(`https://api.xyroinee.xyz/api/search/wattpad?q=${text}&lc=es&apikey=uwgflzFEh6`)
-let anu = await res.json()
-let translatedResults = await Promise.all(anu.data.map(async (v) => {
-let titulo = await translate(v.title, { to: 'es', autoCorrect: true })
-let descs = await translate(v.desc, { to: 'es', autoCorrect: true })
-return `*Título:* ${titulo.text}\n*Leídos:* ${v.reads}\n*Votos:* ${v.vote}\n*Capítulos:* ${v.chapter}\n*Enlace:* ${v.link}\n*Descripción:* ${descs.text}`
-}))
-let resultText = translatedResults.join('\n\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n')
-await m.reply(resultText)   
+let handler = async (m, {text, conn}) => {
+let name = "término de búsqueda";
+let link = `https://www.bing.com/search?q=${encodeURIComponent(text)}`;
+m.reply(link)
 }
-handler.command = /^(wattpad)$/i
-handler.dolares = 3
-export default handler
+handler.command = ["bing"]
+export default handler;
