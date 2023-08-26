8334e1d58b72b449e6cc6c881af61b015a8252b9
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 22:51:35 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -4,38 +4,28 @@
 import axios from "axios";
 
 const handler = async (m, { text }) => {
-    const bingSearchUrl = "https://www.bing.com/search";
-    const term = encodeURIComponent(text.toLowerCase());
+  const bingSearchUrl = "https://www.bing.com/search";
+  const term = encodeURIComponent(text.toLowerCase());
 
-    const options = {
-        params: {
-            q: term,
-        },
-    };
+  const options = {
+    params: {
+      q: term,
+    },
+  };
 
-   // try {
-        const response = await axios.get(bingSearchUrl, options);
+  const response = await axios.get(bingSearchUrl, options);
 
-        if (response.status === 200) {
-            const webPages = response.data.webPages;
+  if (response.status === 200) {
+    const webPages = response.data.webPages;
 
-            if (webPages && webPages.value && Array.isArray(webPages.value)) {
-                const results = webPages.value;
-                const resultTitles = results.map(result => result.name);
-                m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
-           /* } else {
-                throw new Error(`No se encontraron resultados válidos para "${text}"`);
-            }
-        } else {
-            throw new Error(`Error al buscar "${text}"`);
-        }
-    } catch (error) {
-        m.reply(error.message);
-    }*/
-}
-}
-}
+    if (webPages && webPages.value && Array.isArray(webPages.value)) {
+      const results = webPages.value;
+      const resultTitles = results.map(result => result.name);
+      m.reply(`Los resultados de la búsqueda de "${text}" son: ${resultTitles.join(", ")}`);
+    } 
+  } 
+};
+
 handler.command = ["bing"];
 
 export default handler;
-
