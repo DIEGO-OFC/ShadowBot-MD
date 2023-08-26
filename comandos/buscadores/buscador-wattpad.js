1ad2a9db95f666f29828ef8c864e72e158748c77
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2023-08-25 21:00:33 -0400

    Actualizar buscador-wattpad.js

--- comandos/buscadores/buscador-wattpad.js
+++ comandos/buscadores/buscador-wattpad.js
@@ -1,7 +1,36 @@
-let handler = async (m, {text, conn}) => {
-let name = "término de búsqueda";
-let link = `https://www.bing.com/search?q=${encodeURIComponent(text)}`;
-m.reply(link)
-}
-handler.command = ["bing"]
+
+Claro, aquí tienes el código con node-fetch:
+
+JavaScript
+import fetch from "node-fetch";
+
+// Define el handler
+const handler = async (m, { text, conn }) => {
+  // Obtén el término de búsqueda
+  const term = text.toLowerCase();
+
+  // Realiza la solicitud de búsqueda
+  const response = await fetch("https://www.bing.com/search", {
+    method: "GET",
+    params: {
+      q: term,
+    },
+  });
+
+  // Verifica si hay algún error
+  if (response.status !== 200) {
+    throw new Error(`Error al buscar "${term}"`);
+  }
+
+  // Obtén los resultados de la búsqueda
+  const results = await response.json();
+
+  // Imprime los resultados de la búsqueda
+  m.reply(results.webPages.map(({ title, url }) => `${title} - ${url}`));
+};
+
+// Define los comandos
+handler.command = ["bing"];
+
+// Exporta el handler
 export default handler;
