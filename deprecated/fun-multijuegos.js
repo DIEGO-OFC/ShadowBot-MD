78fbeb39869154ef19d7391430e89d1593783a12
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-05-06 19:53:48 -0400

    Delete deprecated directory

--- deprecated/fun-multijuegos.js
+++
@@ -1,4 +0,0 @@
-//let com = `${usedPrefix}`
-//let juegos = `${pickRandom([`${com}formarareja`,`${com}reto`,`${com}verdad`])}`
-
-//let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
