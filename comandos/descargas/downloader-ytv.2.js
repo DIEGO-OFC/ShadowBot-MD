e279385575d6993ec6b0fe763d3707dbeab8fbf2
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-05-20 15:38:45 -0400

    Add files via upload

---
+++ comandos/descargas/downloader-ytv.2.js
@@ -0,0 +1,29 @@
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper' 
+ import fetch from 'node-fetch' 
+ let handler = async (m, { conn, args }) => { 
+ if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 / 𝙻𝙸𝙽𝙺 𝙳𝙴 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*' 
+ await m.reply(`*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*\n\n*◉ Sɪ Sᴜ ᴠɪᴅᴇᴏ ɴᴏ ᴇs ᴇɴᴠɪᴀᴅᴏ, ᴘʀᴜᴇʙᴇ ᴄᴏɴ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ #playdoc ᴏ #play.2 ᴏ #ytmp4doc ◉*`) 
+ try { 
+ let qu = args[1] || '360' 
+ let q = qu + 'p' 
+ let v = args[0] 
+ const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)) 
+ const dl_url = await yt.video[q].download() 
+ const ttl = await yt.title 
+ const size = await yt.video[q].fileSizeH 
+ let cap = `🔍 *TITULO:* ${ttl}\n📦 *PESO:* ${size}`.trim() 
+ await await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m}) 
+ } catch { 
+ try { 
+ let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${args[0]}`)     
+ let lolh = await lolhuman.json() 
+ let n = lolh.result.title || 'error' 
+ let n2 = lolh.result.link 
+ let n3 = lolh.result.size 
+ let cap2 = `🔍 *TITULO:* ${n}\n📦 *PESO:* ${n3}`.trim() 
+ await conn.sendMessage(m.chat, { document: { url: n2 }, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m}) 
+ } catch { 
+ await conn.reply(m.chat, '*[⚠️] NO SE PUDO DESCARGAR EL VIDEO*', m)} 
+ }} 
+ handler.command = /^ytmp4doc|ytvdoc|ytmp4.2|ytv.2$/i 
+ export default handler
