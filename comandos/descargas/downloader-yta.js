a910aca874299cf128febafd0e844325a3d33b70
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2024-05-27 11:01:34 -0500

    Update downloader-yta.js

--- comandos/descargas/downloader-yta.js
+++ comandos/descargas/downloader-yta.js
@@ -1,24 +1,51 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
-
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`;
-  if (!args[0].match(/youtu/gi)) throw `❎ Verify that it is a YouTube link.`;
-
- 
-  
-        var data = await youtubedl(text)
-        var url = await data.audio['128kbps'].download()
-        var hasil = await getBuffer(url)
-        await conn.sendMessage(from, {
-          document: hasil,
-          mimetype: "audio/mp4",
-          fileName: `${data.title}.mp3`,
-          jpegThumbnail: fs.readFileSync('./sticker/thumb.jpg'),
-        }, { quoted: msg })
-        await sleep(500)
-        await conn.sendMessage(from, { audio: hasil, mimetype: "audio/mp4" }, { quoted: msg })}
-handler.help = ['ytmp3 <url>']
-handler.tags = ['downloader']
-handler.command = ['ytmp3', 'yta'] 
-
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper' 
+import fetch from 'node-fetch'
+import yts from 'yt-search'
+import ytdl from 'ytdl-core'
+let handler = async (m, { text, conn, args, usedPrefix, command }) => {
+if (!args[0]) return conn.reply(m.chat, `⚡ *USAR EL COMANDO DE ESTA FORMA:*\n\n*${usedPrefix + command} https://youtu.be/c5gJRzCi0f0*`, m)
+let youtubeLink = ''
+if (args[0].includes('you')) {
+youtubeLink = args[0]
+} else {
+const index = parseInt(args[0]) - 1
+if (index >= 0) {
+if (Array.isArray(global.videoList) && global.videoList.length > 0) {
+const matchingItem = global.videoList.find(item => item.from === m.sender)
+if (matchingItem) {
+if (index < matchingItem.urls.length) {
+youtubeLink = matchingItem.urls[index]
+} else {
+throw `𝙉𝙊 𝙎𝙀 𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝙊́ 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙋𝘼𝙍𝘼 𝙀𝙎𝙀 𝙉𝙐́𝙈𝙀𝙍𝙊, 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙉𝙐́𝙈𝙀𝙍𝙊 𝙀𝙉𝙏𝙍𝙀 1 𝙔 𝙀𝙇 ${matchingItem.urls.length}*`
+}} else {
+throw `𝙋𝘼𝙍𝘼 𝙋𝙊𝘿𝙀𝙍 𝙐𝙎𝘼𝙍 𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝘿𝙀 𝙀𝙎𝙏𝘼 𝙁𝙊𝙍𝙈𝘼 (${usedPrefix + command} <numero>), 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙍𝙀𝘼𝙇𝙄𝙕𝘼𝙍 𝙇𝘼 𝘽𝙐́𝙎𝙌𝙐𝙀𝘿𝘼 𝘿𝙀 𝙑𝙄́𝘿𝙀𝙊𝙎 𝘾𝙊𝙉 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊\n*${usedPrefix}playlist <texto>*`
+}} else {
+throw `𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙍𝙀𝘼𝙇𝙄𝙕𝘼𝙍 𝙇𝘼 𝘽𝙐́𝙎𝙌𝙐𝙀𝘿𝘼 𝘿𝙀 𝙑𝙄́𝘿𝙀𝙊𝙎 𝘾𝙊𝙉 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊\n*${usedPrefix}playlist <texto>*`
+}}}  
+await conn.reply(m.chat, `𝙎𝙀 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙉𝘿𝙊 𝙎𝙐 𝘼𝙐𝘿𝙄𝙊, 𝙀𝙎𝙋𝙀𝙍𝙀 𝙐𝙉 𝙈𝙊𝙈𝙀𝙉𝙏𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍`, m)
+try {
+let q = '128kbps'
+let v = youtubeLink
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v))
+const dl_url = await yt.audio[q].download()
+const ttl = await yt.title
+const size = await yt.audio[q].fileSizeH
+await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${youtubeLink}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+await conn.sendMessage(m.chat, { audio: { url: lolh.result.link }, fileName: `${n}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })  
+} catch {   
+try {
+let searchh = await yts(youtubeLink)
+let __res = searchh.all.map(v => v).filter(v => v.type == "video")
+let infoo = await ytdl.getInfo('https://youtu.be/' + __res[0].videoId)
+let ress = await ytdl.chooseFormat(infoo.formats, { filter: 'audioonly' })
+conn.sendMessage(m.chat, { audio: { url: ress.url }, fileName: __res[0].title + '.mp3', mimetype: 'audio/mp4' }, { quoted: m })  
+} catch (e) {
+console.log(e)}
+}}}
+handler.command = /^audio|fgmp3|dlmp3|getaud|yt(a|mp3)$/i
 export default handler
