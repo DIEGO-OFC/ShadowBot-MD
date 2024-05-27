3539680ffbaa27ee29a242e75a3309c36a8e4205
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-05-27 08:43:30 -0400

    Actualizar downloader-yta.js

--- comandos/descargas/downloader-yta.js
+++ comandos/descargas/downloader-yta.js
@@ -4,30 +4,19 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
   if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`;
   if (!args[0].match(/youtu/gi)) throw `❎ Verify that it is a YouTube link.`;
 
-  m.react(rwait); 
-
-  try {
-    let q = '128kbps'; 
-    let v = args[0]; 
-    const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)); 
-    const dl_url = await yt.audio[q].download(); 
-    const title = await yt.title; 
-
-    conn.sendFile(
-      m.chat,
-      dl_url,
-      title + '.mp3',
-      null, 
-      m,
-      false,
-      { mimetype: 'audio/mpeg' }
-    );
-
-  } catch {
-    await m.reply(`❎ Error: Could not download the audio.`)
-  }
-};
-
+ 
+  
+        var data = await youtubedl(text)
+        var url = await data.audio['128kbps'].download()
+        var hasil = await getBuffer(url)
+        await conn.sendMessage(from, {
+          document: hasil,
+          mimetype: "audio/mp4",
+          fileName: `${data.title}.mp3`,
+          jpegThumbnail: fs.readFileSync('./sticker/thumb.jpg'),
+        }, { quoted: msg })
+        await sleep(500)
+        await conn.sendMessage(from, { audio: hasil, mimetype: "audio/mp4" }, { quoted: msg })}
 handler.help = ['ytmp3 <url>']
 handler.tags = ['downloader']
 handler.command = ['ytmp3', 'yta'] 
