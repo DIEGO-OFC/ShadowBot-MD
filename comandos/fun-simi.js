9fe8b40fd6a8d8f70bfc0f85c7446146a1b79fa0
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-05-31 18:47:10 -0300

    Update fun-simi.js

--- comandos/fun-simi.js
+++ comandos/fun-simi.js
@@ -1,18 +1,9 @@
 import fetch from "node-fetch";
 let handler = async (m, {text, usedPrefix, command}) => {
-  if (!text) {
-    conn.reply(
-      m.chat,
-      `Hola *@${m.sender.split`@`[0]}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n*_📌 Ejemplo :_* *${
-        usedPrefix + command
-      }* Hola bot`,
-      m,
-      {mentions: [m.sender]}
-    );
-    throw false;
-  }
-   let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/simi?text=${text}`)
-let res = await gpt.json()
+  if (!text) return conn.reply(m.chat, `Hola *@${m.sender.split`@`[0]}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n*_📌 Ejemplo :_* *${usedPrefix + command}* Hola bot`, m, {mentions: [m.sender]});
+await conn.sendPresenceUpdate('composing', m.chat)
+let simi = await fetch(`https://delirius-api-oficial.vercel.app/api/simi?text=${text}`)
+let res = await simi.json()
 await m.reply(res.data.message)
 };
 handler.help = ["simi", "bot"].map((v) => v + " <teks>");
