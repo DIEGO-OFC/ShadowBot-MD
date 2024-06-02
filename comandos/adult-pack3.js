9b37dc5d4b1311ea0fa25c43704ad088b44aaa20
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-06-02 17:21:33 -0300

    Update adult-pack3.js

--- comandos/adult-pack3.js
+++ comandos/adult-pack3.js
@@ -2,7 +2,7 @@ let handler = async (m, {conn, command}) => {
   if (!db.data.chats[m.chat].modohorny && m.isGroup)
     throw "*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 +𝟷𝟾 𝙴𝚂𝚃𝙰𝙽 𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝙳𝙾𝚂 𝙴𝙽 𝙴𝚂𝚃𝙴 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙸 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝚈 𝙳𝙴𝚂𝙴𝙰 𝙰𝙲𝚃𝙸𝚅𝙰𝚁𝙻𝙾𝚂 𝚄𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 #enable modohorny*";
   let url = packmen[Math.floor(Math.random() * packmen.length)];
-  conn.sendButton(m.chat, `_🥵 Pack 3 🥵_`, author, url, [["🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄", `/${command}`]], m);
+conn.sendButton(m.chat, `_🥵 Pack 3 🥵_`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], null, null, m)
 };
 handler.help = ["pack3"];
 handler.tags = ["internet"];
