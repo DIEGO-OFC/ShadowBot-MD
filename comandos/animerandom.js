c7eded3c28cb7221d566210ee2c475beba470967
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-06-02 19:02:03 -0400

    Actualizar animerandom.js

--- comandos/animerandom.js
+++ comandos/animerandom.js
@@ -1,4 +1,4 @@
-import animeNames from '../lib/anime_names.json';
+//import animeNames from '../lib/anime_names.json';
 
 var handler = async (m, { conn }) => {
   const randomAnime = animeNames[Math.floor(Math.random() * animeNames.length)];
@@ -8,3 +8,27 @@ handler.help = ["anime"];
 handler.tags = ["random"];
 handler.command = ["randoma", "animr"];
 export default handler;
+
+global.animeNames = 
+[
+    "Naruto",
+    "Dragon Ball",
+    "One Piece",
+    "Attack on Titan",
+    "Death Note",
+    "My Hero Academia",
+    "Tokyo Ghoul",
+    "Sword Art Online",
+    "Fullmetal Alchemist",
+    "Demon Slayer",
+    "Hunter x Hunter",
+    "One Punch Man",
+    "Fairy Tail",
+    "Bleach",
+    "Naruto Shippuden",
+    "JoJo's Bizarre Adventure",
+    "Black Clover",
+    "Neon Genesis Evangelion",
+    "Code Geass",
+    "Steins;Gate"
+]
