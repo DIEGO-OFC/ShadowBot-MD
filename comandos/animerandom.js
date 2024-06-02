64e95deb701d3dc35f8a57f5a93fa302f73ccb0d
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-02 19:20:32 -0300

    Actualizar comandos

---
+++ comandos/animerandom.js
@@ -0,0 +1,10 @@
+import animeNames from './anime_names.json';
+
+var handler = async (m, { conn }) => {
+  const randomAnime = animeNames[Math.floor(Math.random() * animeNames.length)];
+  conn.reply(m.chat, `🎬 Nombre de anime aleatorio: *${randomAnime}*`, m);
+};
+handler.help = ["anime"];
+handler.tags = ["random"];
+handler.command = /^(anime)$/i;
+export default handler;
