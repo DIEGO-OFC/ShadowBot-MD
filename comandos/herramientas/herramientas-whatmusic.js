0f36c8db7dd4341c43e798c9e3ef6fa29c533d0e
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-06-02 18:36:23 -0400

    Add files via upload

---
+++ comandos/herramientas/herramientas-whatmusic.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+import acrcloud from "acrcloud";
+let acr = new acrcloud({
+  host: "identify-eu-west-1.acrcloud.com",
+  access_key: "c33c767d683f78bd17d4bd4991955d81",
+  access_secret: "bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu",
+});
+
+let handler = async (m) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || "";
+  if (/audio|video/.test(mime)) {
+    let media = await q.download();
+    let ext = mime.split("/")[1];
+    fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media);
+    let res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`));
+    let {code, msg} = res.status;
+    if (code !== 0) throw msg;
+    let {title, artists, album, genres, release_date, url} = res.metadata.music[0];
+    let txt = `
+🔰 RESULTADOS 🔰
+• 📌 TITULO: ${title}
+• 👨‍🎤 ARTISTA: ${artists !== undefined ? artists.map((v) => v.name).join(", ") : "No encontrado"}
+• 💾 ALBUM: ${album.name || "No encontrado"}
+• 🌐 GENERO: ${genres !== undefined ? genres.map((v) => v.name).join(", ") : "No encontrado"}
+• 📆 FECHA: ${release_date || "No encontrado"}
+${url}
+`.trim();
+    fs.unlinkSync(`./tmp/${m.sender}.${ext}`);
+    m.reply(txt);
+  } else throw "*RESPONDA A UN VIDEO*";
+};
+handler.command = /^quemusica|quemusicaes|whatmusic$/i;
+export default handler;
