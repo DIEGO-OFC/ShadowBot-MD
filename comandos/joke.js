d1728cfb0b3868313a1b106dd93d3be2bf0b10fc
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-06-02 19:26:59 -0300

    Update joke.js

--- comandos/joke.js
+++ comandos/joke.js
@@ -4,20 +4,22 @@ import translate from '@vitalets/google-translate-api';
 let handler = async (m, { usedPrefix, command }) => {
   try {
     let res = await fetch('https://official-joke-api.appspot.com/random_joke');
+    if (!res.ok) throw new Error('Error');
+    
     let json = await res.json();
-
-    if (json.setup && json.punchline) {     
+    if (json.setup && json.punchline) {
       let translatedSetup = await translate(json.setup, { to: 'es' });
       let translatedPunchline = await translate(json.punchline, { to: 'es' });
+      
       m.reply(`╔═══════════════════
 ║*😂 𝐂𝐇𝐈𝐒𝐓𝐄 𝐀𝐋𝐄𝐀𝐓𝐎𝐑𝐈𝐎 😂*
 ║-----------------------
-║ *𝙲𝙷𝙸𝚂𝚃𝙴:* ${translatedSetup}
+║ *𝙲𝙷𝙸𝚂𝚃𝙴:* ${translatedSetup.text}
 ║-----------------------
 ║ *𝙿𝚄𝙽𝙲𝙷𝙻𝙸𝙽𝙴:* ${translatedPunchline.text}
 ╚═══════════════════`);
     } else {
-      throw json;
+      throw new Error('Invalid');
     }
   } catch (e) {
     console.error(e);
