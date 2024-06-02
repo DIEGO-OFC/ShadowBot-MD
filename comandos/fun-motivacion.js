e1e7e5a8c457048c9ccdae7714122e628cd87b46
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-06-02 20:11:40 -0300

    Update and rename motivacion.js to fun-motivacion.js

--- comandos/fun-motivacion.js
+++ comandos/fun-motivacion.js
@@ -1,17 +1,21 @@
 import fetch from 'node-fetch';
+import translate from '@vitalets/google-translate-api';
 
 let handler = async (m, { usedPrefix, command }) => {
   let res = await fetch('https://api.quotable.io/random?tags=motivational');
   let json = await res.json();
-  if (json.content)
+  
+  if (json.content) {
+    let translated = await translate(json.content, { to: 'es' });
     m.reply(`╔═══════════════════
 ║≡ *🌟 𝐅𝐑𝐀𝐒𝐄 𝐌𝐎𝐓𝐈𝐕𝐀𝐂𝐈𝐎𝐍𝐀𝐋 🌟*
 ║-----------------------
-║➢ *𝙵𝚁𝙰𝚂𝙴:* "${json.content}"
-║-----------------------
+║➢ *𝙵𝚁𝙰𝚂𝙴:* ` + `${translated.text}\n` + `║-----------------------
 ║➢ *𝙰𝚄𝚃𝙾𝚁:* ${json.author}
 ╚═══════════════════`);
-  else throw json;
+  } else {
+    throw json;
+  }
 };
 
 handler.help = ["frase"];
