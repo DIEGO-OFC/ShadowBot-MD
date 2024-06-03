3cd5f0fb2548022e7ec31df1ad8727f99839b098
DIEGO-OFC <107585310+DIEGO-OFC@users.noreply.github.com>
2024-06-03 00:18:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -483,7 +483,7 @@ global.reloadHandler = async function (restatConn) {
       global.conn.ws.close();
     } catch {}
     conn.ev.removeAllListeners();
-    global.conn = makeWaSocket(connectionOptions, {chats: oldChats});
+    global.conn = makeWASocket(connectionOptions, {chats: oldChats});
     isInit = true;
   }
   if (!isInit) {
