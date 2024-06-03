67ec825c374909ab0144a725266236e043cb48d7
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-03 14:21:16 -0300

    Actualizar fun-chiste.js

--- comandos/fun-chiste.js
+++ comandos/fun-chiste.js
@@ -3,20 +3,26 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 
-let handler = async (m, { usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   try {
+    // Obtener chiste
     let res = await fetch('https://official-joke-api.appspot.com/random_joke');
     if (!res.ok) throw new Error('Error');
     
     let json = await res.json();
     if (json.setup && json.punchline) {
+      // Traducir chiste
       let translatedSetup = await translate(json.setup, { to: 'es' });
       let translatedPunchline = await translate(json.punchline, { to: 'es' });
 
-      let chiste = `${translatedSetup.text}\n${translatedPunchline.text}`;
-      let imageUrl = `https://quickchart.io/chart?c={type:'bar',data:{labels:['Chiste'],datasets:[{label:'',data:[],backgroundColor:'white',borderColor:'black',borderWidth:1}]},options:{scales:{yAxes:[{ticks:{display:false},gridLines:{display:false}}],xAxes:[{ticks:{display:false},gridLines:{display:false}}]},plugins:{datalabels:{align:'start',anchor:'end',color:'black',backgroundColor:'white',borderRadius:5,borderWidth:1,borderColor:'black',font:{size:16},formatter:function(){return '${chiste.replace(/\n/g, "\\n")}';}}},layout:{padding:{top:30,right:30,bottom:30,left:30}}}}`;
+      // Obtener imagen aleatoria
+      let imageUrl = 'https://picsum.photos/500/300';
 
-      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: '*Chiste aleatorio:*' });
+      // Formatear mensaje
+      let caption = `*Chiste aleatorio:*\n\n${translatedSetup.text}\n${translatedPunchline.text}`;
+
+      // Enviar imagen con chiste
+      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: caption });
     } else {
       throw new Error('Invalid');
     }
