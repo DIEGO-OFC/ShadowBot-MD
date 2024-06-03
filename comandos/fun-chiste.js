39ba24bcf0197465168c2933be748ef035e2643f
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-03 14:32:23 -0300

    Actualizar fun-chiste.js

--- comandos/fun-chiste.js
+++ comandos/fun-chiste.js
@@ -1,19 +1,34 @@
-//Créditos a https://github.com/matias-crypto 
+// Créditos a https://github.com/matias-crypto 
 
-import translate from '@vitalets/google-translate-api'
-import fetch from 'node-fetch'
+import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
 
-let handler = async (m, { usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
   try {
+    // Obtener chiste
     let res = await fetch('https://official-joke-api.appspot.com/random_joke');
     if (!res.ok) throw new Error('Error');
     
     let json = await res.json();
     if (json.setup && json.punchline) {
+      // Traducir chiste
       let translatedSetup = await translate(json.setup, { to: 'es' });
       let translatedPunchline = await translate(json.punchline, { to: 'es' });
+
+      // Obtener imagen aleatoria
+      let imageUrl = 'https://picsum.photos/500/300';
       
-      m.reply(`*Chiste aleatorio:*\n\n${translatedSetup.text}\n` + `${translatedPunchline.text}`);
+      // Formatear mensaje
+      let caption = `*Chiste aleatorio:*\n\n${translatedSetup.text}\n${translatedPunchline.text}`;
+
+      // Esperar a que la imagen esté lista
+      let imgRes = await fetch(imageUrl);
+      if (!imgRes.ok) throw new Error('No se pudo obtener la imagen');
+
+      let buffer = await imgRes.buffer();
+
+      // Enviar imagen con chiste
+      await conn.sendMessage(m.chat, { image: buffer, caption: caption });
     } else {
       throw new Error('Invalid');
     }
