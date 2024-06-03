5ad1eebb07a99420f3baacea851f7a11ef58ff4b
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2024-06-02 20:49:53 -0400

    Update downloader-playlist.js

--- comandos/descargas/downloader-playlist.js
+++ comandos/descargas/downloader-playlist.js
@@ -42,13 +42,13 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP3',
-                  id: `${usedPrefix}play.1 ${V.url}`
+                  id: `${prefijo}play.1 ${video.url}`
                 },
                 {
                   header: video.title,
                   title: video.author.name,
                   description: 'Descargar MP4',
-                  id: `${usedPrefix}play.2 ${V.url}`
+                  id: `${prefijo}play.2 ${video.url}`
                 }
               ]
             }))
