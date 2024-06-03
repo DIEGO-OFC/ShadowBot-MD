0fcb335d86100aa9f800d12894a48352b8bae8c5
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-02 22:49:37 -0300

    Actualizar animerandom.js

--- comandos/animerandom.js
+++ comandos/animerandom.js
@@ -9,7 +9,7 @@ var handler = async (m, { conn }) => {
 handler.help = ["anime"];
 handler.tags = ["random"];
 handler.command = ["randoma", "animr"];
-handler.dolares = 2
+
 
 export default handler;
 
