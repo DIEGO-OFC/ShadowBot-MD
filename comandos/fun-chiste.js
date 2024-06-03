89a7f2a485c72285693a8faa30273e93776e641c
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-03 14:19:36 -0300

    Actualizar fun-chiste.js

--- comandos/fun-chiste.js
+++ comandos/fun-chiste.js
@@ -1,7 +1,7 @@
-//Código hecho por Mario y matias-crypto 
+// Créditos a https://github.com/matias-crypto 
 
-import fetch from 'node-fetch';
 import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch';
 
 let handler = async (m, { usedPrefix, command }) => {
   try {
@@ -12,8 +12,11 @@ let handler = async (m, { usedPrefix, command }) => {
     if (json.setup && json.punchline) {
       let translatedSetup = await translate(json.setup, { to: 'es' });
       let translatedPunchline = await translate(json.punchline, { to: 'es' });
-      
-      m.reply(`${translatedSetup.text}\n` + `${translatedPunchline.text}`);
+
+      let chiste = `${translatedSetup.text}\n${translatedPunchline.text}`;
+      let imageUrl = `https://quickchart.io/chart?c={type:'bar',data:{labels:['Chiste'],datasets:[{label:'',data:[],backgroundColor:'white',borderColor:'black',borderWidth:1}]},options:{scales:{yAxes:[{ticks:{display:false},gridLines:{display:false}}],xAxes:[{ticks:{display:false},gridLines:{display:false}}]},plugins:{datalabels:{align:'start',anchor:'end',color:'black',backgroundColor:'white',borderRadius:5,borderWidth:1,borderColor:'black',font:{size:16},formatter:function(){return '${chiste.replace(/\n/g, "\\n")}';}}},layout:{padding:{top:30,right:30,bottom:30,left:30}}}}`;
+
+      await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: '*Chiste aleatorio:*' });
     } else {
       throw new Error('Invalid');
     }
